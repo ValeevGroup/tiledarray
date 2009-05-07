add264daefda42930b1925b975f52cfcd3d8d7f5
justusc <justusc@vt.edu>
2009-05-07 21:51:37 +0000

    --HG-- extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40106

--- src/array.h
+++ src/array.h
@@ -4,78 +4,14 @@
 #include <cassert>
 #include <map>
 #include <boost/shared_ptr.hpp>
-#include <boost/multi_array.hpp>
-#include <boost/iterator/iterator_facade.hpp>
+#include <boost/noncopyable.hpp>
 #include <coordinates.h>
 #include <permutation.h>
 #include <range.h>
 #include <shape.h>
+#include <tile.h>
 
 namespace TiledArray {
-
-  template<typename T, unsigned int DIM, typename INDEX, typename CS = CoordinateSystem<DIM> >
-  class Tile
-  {
-  public:
-    typedef T value_type;
-    typedef INDEX index_type;
-    typedef typename index_type::Array size_array;
-    typedef CS coordinate_system;
-    typedef size_t ordinal_type;
-
-    static const unsigned int dim() { return DIM; }
-
-    Tile(const size_array& size, const value_type val = 0) {
-      size_ = size;
-
-      init_();
-      data_ = new value_type[n_];
-
-      for(unsigned int i = 0; i < n_; ++i)
-        data_[i] = val;
-    }
-
-    ~Tile() {
-      delete[] data_;
-    }
-
-  private:
-
-    void init_() {
-
-
-      // Get dim ordering iterator
-      const detail::DimensionOrder<DIM>& dimorder = coordinate_system::ordering();
-      typename detail::DimensionOrder<DIM>::const_iterator d;
-
-      ordinal_type weight = 1;
-      for(d = dimorder.begin(); d != dimorder.end(); ++d) {
-        // Init ordinal weights.
-        weights_[*d] = weight;
-        weight *= size_[*d];
-      }
-      n_ = weight;
-
-    }
-
-    bool includes_(const index_type& i) const{
-      index_type origin(0);
-      return (i >= origin) && (i.data() < size_);
-    }
-
-    /// computes an ordinal index for a given index_type
-    ordinal_type ordinal_(const index_type& i) const {
-      assert(includes_(i));
-      ordinal_type result = dot_product(i.data(), weights_);
-      return result;
-    }
-
-    ordinal_type n_;
-	size_array weights_;
-	size_array size_;
-	size_array origin_;
-    value_type* data_;
-  };
 
   /// The main player: Array
   /// Serves as base to various implementations (local, replicated, distributed)
@@ -83,7 +19,7 @@ namespace TiledArray {
   template <typename T, unsigned int DIM, typename CS = CoordinateSystem<DIM> >
   class Array : private boost::noncopyable // we don't allow copy constructors, et al. May need in the future.
   {
-   public:
+  public:
     typedef Array<T,DIM,CS> Array_;
     typedef Range<DIM, CS> range_type;
     typedef typename range_type::tile_iterator range_iterator;
@@ -100,50 +36,8 @@ namespace TiledArray {
     /// array layout must match that given by CoordinateSystem (i.e. both C, or both Fortran)
     typedef Tile<value_type, DIM, element_index, coordinate_system> tile;
 
-  public:
-
-    class iterator : public boost::iterator_facade<iterator, tile, std::output_iterator_tag > {
-
-      /// Default construction not allowed. Access to the array is required for proper operation.
-      iterator();
-
-    public:
-
-      /// Standard constructor used by Array class to create new iterators.
-      iterator(const boost::shared_ptr<shape_iterator>& it, const Array_& a) :
-        current_index_(it), array_ref_(a)
-      {}
-
-      /// copy constructor for iterator.
-      iterator(const iterator& other) :
-        current_index_(other.current_index_), array_ref_(other.array_ref_)
-      {}
-
-      /// Assignment operator.
-      iterator& operator =(const iterator& other) {
-        current_index_ = other.current_index_;
-        array_ref_ = other.array_ref_;
-        return *this;
-      }
-
-    private:
-      friend class boost::iterator_core_access;
-
-      /// Increment the iterator.
-      void increment() { ++(*current_index_); }
-
-      /// Compare with another iterator for equality.
-      bool equal(iterator const& other) const {
-        return (*current_index_ == * other.current_index_) && (array_ref_ == other.array_ref_);
-      }
-
-      /// Dereference tile to an iterator.
-      value_type& dereference() const { return array_ref_.at( *current_index_ ); }
-
-      boost::shared_ptr<shape_iterator> current_index_;
-      const Array& array_ref_;
-
-    };
+	typedef detail::ElementIterator<std::pair<const shape_iterator&, value_type&>, Array_ > iterator;
+	typedef detail::ElementIterator<std::pair<const shape_iterator&, value_type const&>, Array_ > const_iterator;
 
     iterator begin() const {
       return iterator( boost::shared_ptr<shape_iterator>( shape_->begin() ) );
