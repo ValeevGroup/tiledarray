944579bff5aa5c35c7e1d9b9e58d1b41db70f3cc
evaleev <eduard@valeyev.net>
2009-09-28 19:52:04 +0000

    Added tests for NestedForLoop.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40181

--- Tests/algorithmtest.cpp
+++ Tests/algorithmtest.cpp
@@ -6,7 +6,16 @@
 using namespace TiledArray;
 
 struct AlgorithmFixture {
-  AlgorithmFixture() : n(119), v(n, 1) {}
+  AlgorithmFixture() : zero(ndim, 0), unit(ndim, 1) {
+    std::size_t nn[ndim] = {19, 23, 29, 31, 37};
+    n.resize(ndim);
+    std::copy(nn, nn+ndim, n.begin());
+
+    std::size_t size = 1;
+    for(int d=0; d<ndim; ++d) size *= n[d];
+    v.resize(size);
+    std::fill(v.begin(), v.end(), 1);
+  }
   ~AlgorithmFixture() {}
 
   template <typename RandIter> struct Counter {
@@ -16,28 +25,80 @@ struct AlgorithmFixture {
     void operator()(const RandIter& it) { i_ += *it; }
   };
 
+  const static std::size_t ndim = 5;
+
   typedef std::vector<int>::const_iterator iter;
   typedef detail::ForLoop<Counter<iter>, iter> ForLoop1;
+  typedef detail::NestedForLoop<2, Counter<iter>, iter> ForLoop2;
+  typedef detail::NestedForLoop<ndim, Counter<iter>, iter> ForLoopN;
 
-  std::size_t n;
+  std::vector<std::size_t> zero;
+  std::vector<std::size_t> unit;
+  std::vector<std::size_t> n;
   std::vector<int> v;
 };
 
 BOOST_FIXTURE_TEST_SUITE( algorithm_suite, AlgorithmFixture )
 
-BOOST_AUTO_TEST_CASE( constructor )
+BOOST_AUTO_TEST_CASE( constructor_loop1 )
 {
   int c = 0;
   Counter<iter> functor(c);
-  BOOST_REQUIRE_NO_THROW( ForLoop1 l0 (functor, (ForLoop1::diff_t)n) );
+  BOOST_REQUIRE_NO_THROW( ForLoop1 l0 (functor, (ForLoop1::diff_t)n[0]) );
 }
 
-BOOST_AUTO_TEST_CASE( evaluate )
+BOOST_AUTO_TEST_CASE( constructor_loop2 )
 {
   int c = 0;
   Counter<iter> functor(c);
-  ForLoop1 l0 (functor, (ForLoop1::diff_t)n); l0(v.begin());
-  BOOST_CHECK_EQUAL(c, n);
+  BOOST_REQUIRE_NO_THROW( ForLoop2 l0(functor, n.begin(), n.begin() + 2, unit.begin(), unit.begin() + 2) );
+}
+
+BOOST_AUTO_TEST_CASE( constructor_loopN )
+{
+  int c = 0;
+  Counter<iter> functor(c);
+  BOOST_REQUIRE_NO_THROW( ForLoopN l0(functor, n.begin(), n.end(), unit.begin(), unit.end()) );
+}
+
+BOOST_AUTO_TEST_CASE( evaluate_loop1 )
+{
+  int c = 0;
+  Counter<iter> functor(c);
+  ForLoop1 l0 (functor, (ForLoop1::diff_t)n[0]); l0(v.begin());
+  BOOST_CHECK_EQUAL(c, n[0]);
+}
+
+BOOST_AUTO_TEST_CASE( evaluate_loop2 )
+{
+  { // outer dim = 0, inner dim = 1
+    int c = 0;
+    Counter<iter> functor(c);
+    ForLoop2 l0(functor, n.begin(), n.begin() + 2, unit.begin(), unit.begin() + 2); l0(v.begin());
+    BOOST_CHECK_EQUAL(c, n[0]*n[1]);
+  }
+  { // outer dim = 4, inner dim = 3
+    int c = 0;
+    Counter<iter> functor(c);
+    ForLoop2 l0(functor, n.rbegin(), n.rbegin() + 2, unit.rbegin(), unit.rbegin() + 2); l0(v.begin());
+    BOOST_CHECK_EQUAL(c, n[4]*n[3]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( evaluate_loopN )
+{
+  { // outer dim = 0, inner dim = 4
+    int c = 0;
+    Counter<iter> functor(c);
+    ForLoopN l0(functor, n.begin(), n.end(), unit.begin(), unit.end()); l0(v.begin());
+    BOOST_CHECK_EQUAL(c, v.size());
+  }
+  { // outer dim = 4, inner dim = 0
+    int c = 0;
+    Counter<iter> functor(c);
+    ForLoopN l0(functor, n.rbegin(), n.rend(), unit.rbegin(), unit.rend()); l0(v.begin());
+    BOOST_CHECK_EQUAL(c, v.size());
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
