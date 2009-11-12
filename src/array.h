e5678eb260a6b89955c350e0686efda5b13dfc0e
justusc <justusc@vt.edu>
2009-11-12 23:09:04 +0000

    Added an array base class that provides functionality for use with annotated array.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40212

--- src/array.h
+++ src/array.h
@@ -5,6 +5,7 @@
 #include <tiled_range.h>
 #include <tile.h>
 #include <annotated_array.h>
+#include <array_util.h>
 
 #include <cassert>
 #include <boost/shared_ptr.hpp>
@@ -17,14 +18,44 @@ namespace TiledArray {
   class TiledRange;
   template<typename T, unsigned int DIM, typename CS>
   class Tile;
+  template<typename T>
+  class BaseArray;
   template <typename T, unsigned int DIM, typename CS>
   class Array;
   template<typename T, unsigned int DIM, typename CS>
   void swap(Array<T, DIM, CS>&, Array<T, DIM, CS>&);
 
+  namespace expressions {
+    template<typename T>
+    class AnnotatedArray;
+  } // namespace expressions
+
+  /// Array interface class.
+
+  /// Provides a common interface for math operations on array objects.
+  template<typename T>
+  class BaseArray : public madness::WorldObject< BaseArray<T> > {
+  private:
+    BaseArray();
+    BaseArray(const BaseArray<T>&);
+    BaseArray<T>& operator=(const BaseArray<T>&);
+  public:
+    virtual void clear() = 0;
+  protected:
+    BaseArray(madness::World& world) : madness::WorldObject<BaseArray<T> >(world) { }
+    virtual ~BaseArray() { }
+    virtual void insert(const std::size_t, const T* first, const T* last) = 0;
+    virtual void erase(const std::size_t) = 0;
+    virtual bool is_local(const std::size_t) const = 0;
+    virtual bool includes(const std::size_t) const = 0;
+    virtual std::pair<const T*, const T*> data(const std::size_t) const = 0;
+
+    friend class expressions::AnnotatedArray<T>;
+  }; // class BaseArray
+
   /// Tiled Array with data distributed across many nodes.
   template <typename T, unsigned int DIM, typename CS = CoordinateSystem<DIM> >
-  class Array : public madness::WorldObject< Array<T,DIM,CS> > {
+  class Array : public BaseArray<T> {
   public:
     typedef Array<T, DIM, CS> Array_;
     typedef CS coordinate_system;
@@ -57,10 +88,36 @@ namespace TiledArray {
     /// creates an array living in world and described by shape. Optional
     /// val specifies the default value of every element
     Array(madness::World& world, const tiled_range_type& rng) :
-        madness::WorldObject<Array_>(world), range_(rng), tiles_(world, rng.tiles().size())
+        BaseArray<T>(world), range_(rng), tiles_(world, rng.tiles().size())
     {
       this->process_pending();
     }
+
+    /// AnnotatedArray copy constructor
+    template<typename U>
+    Array(const expressions::AnnotatedArray<U>& aarray)
+    {
+      // TODO: Implement this function
+      TA_ASSERT(false, std::runtime_error, "Not yet implemented.");
+      TA_ASSERT((aarray.dim() == DIM), std::runtime_error,
+          "The dimensions of the annotated tile do not match the dimensions of the tile.");
+    }
+
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+    /// AnnotatedArray copy constructor
+    template<typename U>
+    Array(expressions::AnnotatedArray<U>&& aarray)
+    {
+      // TODO: Implement this function.
+      TA_ASSERT(false, std::runtime_error, "Not yet implemented.");
+      TA_ASSERT((aarray.dim() == DIM), std::runtime_error,
+          "The dimensions of the annotated array do not match the dimensions of the tile.");
+
+    }
+#endif // __GXX_EXPERIMENTAL_CXX0X__
+
+    /// Destructor function
+    virtual ~Array() {}
 
     /// Copy the content of the other array into this array.
 
@@ -129,7 +186,7 @@ namespace TiledArray {
     }
 
     /// Removes all tiles from the array.
-    void clear() {
+    virtual void clear() {
       tiles_.clear();
     }
 
@@ -168,7 +225,7 @@ namespace TiledArray {
 
     /// Returns true if the element specified by tile index i is stored locally.
     bool is_local(const tile_index_type& i) const {
-      return range_.elements().includes(i) && is_local(get_tile_index(i) - start());
+      return is_local(get_tile_index_(i) - start());
     }
 
     /// Returns the index of the lower tile boundary.
@@ -242,6 +299,15 @@ namespace TiledArray {
       return tiles_.find(acc, i - start());
     }
 
+    expressions::AnnotatedArray<T> operator ()(const std::string& v) {
+      expressions::AnnotatedArray<T> result(*this, expressions::VariableList(v));
+      return result;
+    }
+
+    expressions::AnnotatedArray<const T> operator ()(const std::string& v) const {
+      return expressions::AnnotatedArray<const T>(*this, expressions::VariableList(v));
+    }
+
     /// Returns a reference to the tile range object.
     const typename tiled_range_type::range_type& tiles() const {
       return range_.tiles();
@@ -257,11 +323,51 @@ namespace TiledArray {
       return range_.tile(i);
     }
 
+  protected:
+    /// Inserts a tile at the give ordinal index with the given data values.
+    virtual void insert(const std::size_t i, const T* first, const T* last) {
+      insert(get_index_(i), first, last);
+    }
+
+    /// Erases a tile at the given ordinal index.
+    virtual void erase(const std::size_t i) {
+      erase(get_index_(i));
+    }
+
+    /// Returns true if the ordinal index is stored locally.
+    virtual bool is_local(const std::size_t i) const {
+      return tiles_.is_local(get_index_(i));
+    }
+
+    /// Returns true if the ordinal index is included in the array.
+    virtual bool includes(const std::size_t i) const {
+      return i < tiles_.volume();
+    }
+
+    /// Returns a pair of pointers that point to the indicated tile's data.
+    virtual std::pair<const T*, const T*> data(const std::size_t i) const {
+      index_type index = get_index_(i);
+      const_accessor acc;
+      find(acc, index);
+      const T* p = acc->second.data();
+      acc.release();
+      return std::make_pair<const T*, const T*>(p, p + range_.tile(index).volume());
+    }
+
   private:
 
     /// Returns the tile index that contains the element index e_idx.
-    index_type get_tile_index(const tile_index_type& i) const {
+    index_type get_tile_index_(const tile_index_type& i) const {
       return * range_.find(i);
+    }
+
+    /// Converts an ordinal into an index
+    index_type get_index_(const std::size_t i) const {
+      index_type result;
+      detail::calc_index(i, coordinate_system::rbegin(tiles_.weight()),
+          coordinate_system::rend(tiles_.weight()),
+          coordinate_system::rbegin(result));
+      return result;
     }
 
     friend void swap<>(Array_&, Array_&);
