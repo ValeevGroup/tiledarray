dfe0703961c67c9446831986697ac586787ca9b4
evaleev <eduard@valeyev.net>
2009-11-19 17:37:44 +0000

    Added tests for Annotation.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40220

--- Tests/annotationtest.cpp
+++ Tests/annotationtest.cpp
@@ -5,3 +5,132 @@
  *      Author: justus
  */
 
+#include "annotation.h"
+#include "tiled_range1.h"
+#include "coordinates.h"
+#include "permutation.h"
+#include <iostream>
+#include <math.h>
+#include <utility>
+#include <boost/test/unit_test.hpp>
+
+using namespace TiledArray;
+using TiledArray::expressions::Annotation;
+using TiledArray::expressions::VariableList;
+
+struct AnnotationFixture {
+  static const size_t ndim = 3;
+
+  typedef Annotation::size_array size_array;
+  typedef Annotation::ordinal_type ordinal_type;
+  typedef ArrayCoordinate<double,ndim,LevelTag<0>, CoordinateSystem<ndim> > index_type;
+
+  AnnotationFixture() : v("a,b,c"), s(ndim) {
+    const size_t primes[] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    for(size_t i=0; i<ndim; ++i) s[i] = primes[i];
+    Annotation* aa = new Annotation(s.begin(), s.end(), v);
+    a = *aa;
+    t = *aa;
+    delete aa;
+  }
+  ~AnnotationFixture() { }
+
+  VariableList v;
+  size_array s;
+  Annotation a, t;
+};
+
+BOOST_FIXTURE_TEST_SUITE( annotation_suite , AnnotationFixture )
+
+BOOST_AUTO_TEST_CASE( accessor )
+{
+  BOOST_CHECK_EQUAL_COLLECTIONS(a.size().begin(), a.size().end(),
+                                t.size().begin(), t.size().end());    // check size accessor
+  BOOST_CHECK_EQUAL_COLLECTIONS(a.weight().begin(), a.weight().end(),
+                                t.weight().begin(), t.weight().end()); // check weight accessor
+  BOOST_CHECK_EQUAL(a.vars(), v); // check variable list accessor
+  BOOST_CHECK_EQUAL(a.dim(), t.dim()); // check dimension accessor
+  BOOST_CHECK_EQUAL(a.volume(), t.volume());// check volume accessor
+  BOOST_CHECK_EQUAL(a.order(), t.order());// check order accessor
+}
+
+BOOST_AUTO_TEST_CASE( include )
+{
+  BOOST_CHECK(a.includes(index_type(0,0,0)));
+  BOOST_CHECK(a.includes(index_type(2,4,4)));
+  BOOST_CHECK(! a.includes(index_type(4,4,5)));
+}
+
+BOOST_AUTO_TEST_CASE( constructor )
+{
+  BOOST_REQUIRE_NO_THROW(Annotation a0); // check default constructor
+  Annotation a0;
+  BOOST_CHECK_EQUAL(a0.volume(), 0u);
+  BOOST_REQUIRE_THROW(a0.includes(index_type(0,0,0)), std::runtime_error);
+
+  BOOST_REQUIRE_NO_THROW(Annotation ac(a)); // check copy constructor
+  Annotation ac(a);
+  BOOST_CHECK_EQUAL_COLLECTIONS(ac.size().begin(), ac.size().end(),
+                                a.size().begin(), a.size().end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(ac.weight().begin(), ac.weight().end(),
+                                a.weight().begin(), a.weight().end());
+  BOOST_CHECK_EQUAL(ac.vars(), a.vars());
+  BOOST_CHECK_EQUAL(ac.dim(), a.dim());
+  BOOST_CHECK_EQUAL(ac.volume(), a.volume());
+  BOOST_CHECK_EQUAL(ac.order(), a.order());
+
+  BOOST_REQUIRE_NO_THROW(Annotation a1(s.begin(), s.end(), v)); // check construction from sizes and annotation
+  Annotation a1(s.begin(), s.end(), v);
+  BOOST_CHECK_EQUAL_COLLECTIONS(a1.size().begin(), a1.size().end(),
+                                a.size().begin(), a.size().end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(a1.weight().begin(), a1.weight().end(),
+                                a.weight().begin(), a.weight().end());
+  BOOST_CHECK_EQUAL(a1.vars(), a.vars());
+  BOOST_CHECK_EQUAL(a1.dim(), a.dim());
+  BOOST_CHECK_EQUAL(a1.volume(), a.volume());
+  BOOST_CHECK_EQUAL(a1.order(), a.order());
+
+  BOOST_REQUIRE_NO_THROW(Annotation a2(s.begin(), s.end(), a.weight().begin(), a.weight().end(), a.volume(), v)); // check construction from sizes, weights, size, and annotation
+  Annotation a2(s.begin(), s.end(), a.weight().begin(), a.weight().end(), a.volume(), v);
+  BOOST_CHECK_EQUAL_COLLECTIONS(a2.size().begin(), a2.size().end(),
+                                a.size().begin(), a.size().end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(a2.weight().begin(), a2.weight().end(),
+                                a.weight().begin(), a.weight().end());
+  BOOST_CHECK_EQUAL(a2.vars(), a.vars());
+  BOOST_CHECK_EQUAL(a2.dim(), a.dim());
+  BOOST_CHECK_EQUAL(a2.volume(), a.volume());
+  BOOST_CHECK_EQUAL(a2.order(), a.order());
+}
+
+BOOST_AUTO_TEST_CASE( assignment )
+{
+  Annotation ac;  ac = a;
+  BOOST_CHECK_EQUAL_COLLECTIONS(ac.size().begin(), ac.size().end(),
+                                a.size().begin(), a.size().end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(ac.weight().begin(), ac.weight().end(),
+                                a.weight().begin(), a.weight().end());
+  BOOST_CHECK_EQUAL(ac.vars(), a.vars());
+  BOOST_CHECK_EQUAL(ac.dim(), a.dim());
+  BOOST_CHECK_EQUAL(ac.volume(), a.volume());
+  BOOST_CHECK_EQUAL(ac.order(), a.order());
+}
+
+BOOST_AUTO_TEST_CASE( permutation )
+{
+  Permutation<ndim> p(1,2,0);
+  size_array s1 = p ^ s;
+  VariableList v1 = p ^ v;
+  Annotation a1(s1.begin(), s1.end(), v1);
+  Annotation a2 = p ^ a;
+  BOOST_CHECK_EQUAL_COLLECTIONS(a2.size().begin(), a2.size().end(),
+                                a1.size().begin(), a1.size().end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(a2.weight().begin(), a2.weight().end(),
+                                a1.weight().begin(), a1.weight().end());
+  BOOST_CHECK_EQUAL(a2.vars(), a1.vars());
+  BOOST_CHECK_EQUAL(a2.dim(), a1.dim());
+  BOOST_CHECK_EQUAL(a2.volume(), a1.volume());
+  BOOST_CHECK_EQUAL(a2.order(), a1.order());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
--- src/annotation.h
+++ src/annotation.h
@@ -30,14 +30,12 @@ namespace TiledArray {
 
       /// Default constructor
 
-      /// Creates an annotated tile with no size or dimensions.
+      /// Creates an empty Annotation
       Annotation() : size_(), weight_(), n_(0), var_(),
           order_(detail::decreasing_dimension_order)
       { }
 
-      /// Create an annotation.
-
-      /// The annotation will be set to the size.
+      /// Create an Annotation.
       /// \var \c [size_first, \c size_last) is the size of each dimension.
       /// \var \c var is the variable annotation.
       /// \var \c o is the dimension order (optional).
@@ -51,7 +49,12 @@ namespace TiledArray {
           var_(var), order_(o)
       { }
 
-      /// Create an annotation.
+      /// Create an Annotation.
+      /// \var \c [size_first, \c size_last) is the size of each dimension.
+      /// \var \c [weight_first, \c weight_last) is the weight of each dimension.
+      /// \var \c n is the number of elements in the array
+      /// \var \c var is the variable annotation.
+      /// \var \c o is the dimension order (optional).
       template<typename SizeInIter, typename WeightInIter>
       Annotation(SizeInIter size_first, SizeInIter size_last, WeightInIter weight_first,
           WeightInIter weight_last, std::size_t n, const VariableList& var,
@@ -76,10 +79,15 @@ namespace TiledArray {
 
       ~Annotation() { }
 
-      /// Annotated tile assignment operator.
+      /// Annotation assignment operator.
       Annotation& operator =(const Annotation& other) {
+        // TODO Justus, should VariableList be also copied?
+#if 0
         TA_ASSERT(var_ == other.var_, std::runtime_error,
             "The variable lists do not match.");
+#else
+        var_ = other.var_;
+#endif
 
         size_ = other.size_;
         weight_ = other.weight_;
@@ -90,7 +98,7 @@ namespace TiledArray {
       }
 
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
-      /// Annotated tile move assignment operator.
+      /// Annotation move assignment operator.
       Annotation& operator =(Annotation&& other) {
         if(this != &other)
           swap(other);
@@ -103,11 +111,11 @@ namespace TiledArray {
       const size_array& size() const { return size_; }
       /// Returns a constant reference to a vector with the dimension weights.
       const size_array& weight() const { return weight_; }
-      /// Returns the number of elements contained by the tile.
+      /// Returns the number of elements contained by the array.
       volume_type volume() const { return n_; }
       /// Returns a constant reference to variable list (the annotation).
       const VariableList& vars() const { return var_; }
-      /// Returns the number of dimensions of the tile.
+      /// Returns the number of dimensions of the array.
       unsigned int dim() const { return var_.dim(); }
       /// Return the array storage order
       detail::DimensionOrderType order() const { return order_; }
@@ -116,9 +124,9 @@ namespace TiledArray {
       template<typename I, unsigned int DIM, typename Tag, TiledArray::detail::DimensionOrderType O>
       bool includes(const ArrayCoordinate<I,DIM,Tag, CoordinateSystem<DIM,O> >& i) const {
         TA_ASSERT(dim() == DIM, std::runtime_error,
-            "Coordinate dimension is not equal to tile dimension.");
+            "Coordinate dimension is not equal to array dimension.");
         TA_ASSERT(order() == O, std::runtime_error,
-            "Coordinate order does not match tile order.");
+            "Coordinate order does not match array dimension order.");
         for(unsigned int d = 0; d < dim(); ++d)
           if(size_[d] <= i[d])
             return false;
@@ -126,7 +134,7 @@ namespace TiledArray {
         return true;
       }
 
-      /// Returns true if the ordinal index is included by the tile.
+      /// Returns true if the ordinal index is included by this array.
       bool includes(const ordinal_type& i) const {
         return (i < n_);
       }
@@ -189,7 +197,8 @@ namespace TiledArray {
     Annotation operator ^(const Permutation<DIM>& p, const Annotation& t) {
       TA_ASSERT((t.dim() == DIM), std::runtime_error,
           "The permutation dimension is not equal to the tile dimensions.");
-      return Annotation(p ^ t.size(), p ^ t.vars());
+      Annotation::size_array new_size = p ^ t.size();
+      return Annotation(new_size.begin(), new_size.end(), p ^ t.vars());
     }
 
   } // namespace expressions
