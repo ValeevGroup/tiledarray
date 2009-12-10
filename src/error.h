ea9598a48af79189f437952c4ec124a460fd2334
justusc <justusc@vt.edu>
2009-12-10 18:30:21 +0000

    Added another macro that throws an exception.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40243

--- src/error.h
+++ src/error.h
@@ -8,22 +8,28 @@
 #endif
 #include <stdexcept>
 #define TA_STRINGIZE( s ) #s
-#define TA_EXCEPTION_MESSAGE( file , line , func , mess ) \
-  "TiledArray Assertion failure in " TA_STRINGIZE( func ) ":" \
+#define TA_EXCEPTION_MESSAGE( file , line , func , type ,  mess ) \
+  type TA_STRINGIZE( func ) ":" \
   TA_STRINGIZE( file ) "(" TA_STRINGIZE( line ) "): " mess
 #define TA_ASSERT( a , e , m )  \
   if(! ( a ) ) \
-    { throw e ( TA_EXCEPTION_MESSAGE( __FILE__ , __LINE__ , __FUNCTION__ , m ) ) ; }
-#endif
+    { throw e ( TA_EXCEPTION_MESSAGE( __FILE__ , __LINE__ , __FUNCTION__ , "TiledArray Assertion failure in " , m ) ) ; }
+
+#define TA_EXCEPTION( e , t ,  m ) \
+    throw e ( TA_EXCEPTION_MESSAGE( __FILE__ , __LINE__ , __FUNCTION__ , t , m ) )
+
+#else
 
 #ifdef TA_ASSERT_ERROR
 #include <cassert>
 #define TA_ASSERT( a , e , m ) assert( a )
-#endif
+#define TA_EXCEPTION( e , m ) exit()
 
-#ifndef TA_ASSERT
+#else
 #define TA_ASSERT( a , e , m ) { ; }
-#endif
+#define TA_EXCEPTION( e , m ) { ; }
+#endif // TA_ASSERT_ERROR
 
+#endif //TA_EXCEPTION_ERROR
 
 #endif // ERROR_H__INCLUDED
