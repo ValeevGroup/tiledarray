32dcc95d0adb76a42a5ef526b88ed7f5c2b6418b
justusc <justusc@vt.edu>
2010-02-10 19:01:56 +0000

    Removed unneeded header file.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40266

--- src/TiledArray/config.h
+++ src/TiledArray/config.h
@@ -1,7 +1,6 @@
 #ifndef TILEDARRAY_CONFIGURE_H__INCULDED
 #define TILEDARRAY_CONFIGURE_H__INCULDED
 
-#include <TiledArray/package.h>
 /* Defines the default error checking behavior. none = 0, throw = 1, assert = 2 */
 #define TA_DEFAULT_ERROR 1
 
