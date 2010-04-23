5d7676daf5d841e113dd6b71045f502bae07769e
justusc <justusc@vt.edu>
2010-04-23 14:26:01 +0000

    Modified the function of binary ops. Removed TaskOp's. Added initial code for contraction ops.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40292

--- src/TiledArray/array_math.h
+++ src/TiledArray/array_math.h
@@ -40,29 +40,144 @@ namespace TiledArray {
       class ProbeOp {
       public:
 
-        ProbeOp(BinaryArrayOp_::result_type& r, tile_op o) :
-            result_(r), op_(o)
-        { }
+        /// Copies of this object are passed to the madness task queue.
 
-        madness::Void left_op(const typename Arg1::index_type i, const typename Arg1::tile_type& t1, const typename Arg2::tile_type& t2) const {
-          if(t2.initialized())
-            result_.insert(i, op_(t1, t2));
-          else
-            result_.insert(i, op_(t1, 0));
+        /// This object handles decision making which operations should be run
+        /// based on the existence or non-existence of tiles.
+        class ProbeOp {
+          ProbeOp();
+        public:
+          ProbeOp(tile_op o) : op_(o) { }
 
-          return madness::None;
+          /// Executes operations for local left hand tile operands.
+
+          /// This function checks for the existence of the right hand operand.
+          /// If it exists, the binary operation is performed and the result is
+          ///
+          typename result_type::tile_type left_op(const typename Arg1::tile_type& t1, const typename Arg2::tile_type& t2) const {
+            if(t2.initialized())
+              return op_(t1, t2);
+
+            return op_(t1, 0);
+          }
+
+          typename result_type::tile_type right_op(bool p1, typename Arg2::tile_type t2) const {
+            if(!p1)
+              return op_(0, t2);
+
+            return result_type::tile_type();
+          }
+
+        private:
+          tile_op op_; ///< Tile operation to be executed.
+        }; // class ProbeOp
+
+    public:
+      /// operation constructor
+      /// \arg \c w is a reference to the world object where tasks will be spawned.
+      /// \arg \c o is the functor or function that will be used in tile operations.
+      /// \arg \c a is the task attributes that will be used when spawning tile, task operations.
+      BinaryArrayOp(madness::World& w, tile_op o = tile_op(), madness::TaskAttributes a = madness::TaskAttributes()) :
+          world_(w), op_(o), attr_(a)
+      { }
+
+      /// Constructs a series of tasks for the given arrays.
+      result_type operator ()(first_argument_type a1, second_argument_type a2) {
+        // Here we assume that the array tiles have compatible sizes because it
+        // is checked in the expression generation functions (if error checking
+        // is enabled.
+
+        // This loop will generate the appropriate tasks for the cases where the
+        // left and right tile exist and only the left tile exists.
+        result_type result(world_, a1.range(), a1.vars(), a1.order());
+        ProbeOp probe(result, op_);
+        for(typename Arg1::const_iterator it = a1.begin(); it != a1.end(); ++it) {
+          const typename Arg1::index_type i = it->first;
+          madness::Future<typename Arg1::tile_type> f1 = it->second;
+          madness::Future<typename Arg2::tile_type> f2 = a2.find(i)->second;
+
+          madness::Future<typename result_type::tile_type> fr =
+              world_.taskq.add(probe, &ProbeOp::left_op, i, f1, f2, attr_);
+
+          result.insert(i, fr);
         }
 
-        madness::Void right_op(const typename Arg1::index_type i, bool p1, typename Arg2::tile_type t2) const {
-          if(!p1)
-            result_.insert(i, op_(0, t2));
+        // This loop will generate the appropriate tasks for the cases where
+        // only the right tile exists.
+        for(typename Arg2::const_iterator it = a2.begin(); it != a2.end(); ++it) {
+          const typename Arg2::index_type i = it->first;
+          madness::Future<bool> f1 = a1.probe(i);
+          madness::Future<typename Arg2::tile_type> f2 = it->second;
 
-          return madness::None;
+          madness::Future<typename result_type::tile_type> fr =
+              world_.taskq.add(probe, &ProbeOp::right_op, i, f1, f2, attr_);
+
+          result.insert(i, fr);
+        }
+
+        // No tasks are generated where neither the left nor the right tiles exits.
+
+        return result;
+      }
+
+    private:
+      madness::World& world_;
+      tile_op op_;
+      madness::TaskAttributes attr_;
+    }; // struct BinaryArrayOp
+
+
+    /// Array operation
+
+    /// Performs an element wise binary operation (e.g. std::plus<T>,
+    /// std::minus<T>) on two annotated tiles. The value type of the different
+    /// tiles may be different, but the value types of expression one and two
+    /// must be implicitly convertible to the result value type.
+    template<typename Arg1, typename Arg2, typename Res>
+    struct BinaryArrayOp<Arg1, Arg2, Res, std::multiplies> {
+    private:
+      BinaryArrayOp();
+      typedef BinaryArrayOp<Arg1, Arg2, Res, std::multiplies> BinaryArrayOp_;
+
+    public:
+      typedef const Arg1& first_argument_type;  ///< first array argument type.
+      typedef const Arg2& second_argument_type; ///< second array argument type.
+      typedef Res result_type;                  ///< result array type.
+      typedef BinaryTileOp<typename Arg1::tile_type, typename Arg2::tile_type,
+          typename result_type::tile_type, std::multiplies> tile_op; ///< Binary tile operation
+
+    private:
+
+      /// Copies of this object are passed to the madness task queue.
+
+      /// This object handles decision making which operations should be run
+      /// based on the existence or non-existence of tiles.
+      class ProbeOp {
+        ProbeOp();
+      public:
+        ProbeOp(tile_op o) : op_(o) { }
+
+        /// Executes operations for local left hand tile operands.
+
+        /// This function checks for the existence of the right hand operand.
+        /// If it exists, the binary operation is performed and the result is
+        ///
+        typename result_type::tile_type left_op(const typename Arg1::tile_type& t1, const typename Arg2::tile_type& t2) const {
+          if(t2.initialized())
+            return op_(t1, t2);
+
+          return op_(t1, 0);
+        }
+
+        typename result_type::tile_type right_op(bool p1, typename Arg2::tile_type t2) const {
+          if(!p1)
+            return op_(0, t2);
+
+          return result_type::tile_type();
         }
 
       private:
-        result_type& result_;
-        tile_op op_;
+        tile_op op_; ///< Tile operation to be executed.
       }; // class ProbeOp
 
     public:
@@ -86,7 +201,10 @@ namespace TiledArray {
           madness::Future<typename Arg1::tile_type> f1 = it->second;
           madness::Future<typename Arg2::tile_type> f2 = a2.find(i)->second;
 
-          world_.taskq.add(probe, &ProbeOp::left_op, i, f1, f2, attr_);
+          madness::Future<typename result_type::tile_type> fr =
+              world_.taskq.add(probe, &ProbeOp::left_op, i, f1, f2, attr_);
+
+          result.insert(i, fr);
         }
 
         for(typename Arg2::const_iterator it = a2.begin(); it != a2.end(); ++it) {
@@ -94,7 +212,10 @@ namespace TiledArray {
           madness::Future<bool> f1 = a1.probe(i);
           madness::Future<typename Arg2::tile_type> f2 = it->second;
 
-          world_.taskq.add(probe, &ProbeOp::right_op, i, f1, f2, attr_);
+          madness::Future<typename result_type::tile_type> fr =
+              world_.taskq.add(probe, &ProbeOp::right_op, i, f1, f2, attr_);
+
+          result.insert(i, fr);
         }
         return result;
       }
@@ -104,8 +225,6 @@ namespace TiledArray {
       tile_op op_;
       madness::TaskAttributes attr_;
     }; // struct BinaryArrayOp
-
-    // Todo: Add BinaryArrayOp specialization for contractions.
 
     /// Unary tile operation
 
