84463956cc21f5b10e86d687c911bda71da91acb
justusc <justusc@vt.edu>
2010-07-27 13:16:21 +0000

    Implemented the Fixture for range math tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40340

--- Tests/range_math.cpp
+++ Tests/range_math.cpp
@@ -1,7 +1,56 @@
-/*
- * range_math.cpp
- *
- *  Created on: Jul 12, 2010
- *      Author: justus
- */
+#include "TiledArray/range_math.h"
+#include "TiledArray/annotated_array.h"
+#include "unit_test_config.h"
+#include "array_fixtures.h"
 
+using TiledArray::Range;
+using TiledArray::expressions::AnnotatedArray;
+
+struct RangeMathFixture {
+  typedef FakeArray<int, GlobalFixture::coordinate_system> array_type;
+  typedef AnnotatedArray<array_type > fake_annotation;
+  typedef fake_annotation::range_type range_type;
+  typedef array_type::index index;
+
+  static const range_type r;
+  static const array_type a;
+  static const VariableList& vars;
+  static const fake_annotation fa;
+
+  RangeMathFixture() : result() { }
+
+  range_type result;
+}; // RangeMathFixture
+
+const RangeMathFixture::range_type RangeMathFixture::r;
+const RangeMathFixture::array_type RangeMathFixture::a(r);
+const VariableList& RangeMathFixture::vars(AnnotatedArrayFixture::vars);
+const RangeMathFixture::fake_annotation RangeMathFixture::fa(a, RangeMathFixture::vars);
+
+BOOST_FIXTURE_TEST_SUITE( range_math_suite, RangeMathFixture )
+
+BOOST_AUTO_TEST_CASE( construct )
+{
+  // Construct a binary range operation object
+
+  // Construct a unary range operation object
+
+  // Construct a binary range operation object
+}
+
+BOOST_AUTO_TEST_CASE( binary )
+{
+  // Test binary range operation
+}
+
+BOOST_AUTO_TEST_CASE( unary )
+{
+  // Test unary range operation
+}
+
+BOOST_AUTO_TEST_CASE( binary_contract )
+{
+  // Test contraction range operation
+}
+
+BOOST_AUTO_TEST_SUITE_END()
