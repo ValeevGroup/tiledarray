862d0a69f61d36ccb6634ea92a18c124f897dd74
justusc <justusc@vt.edu>
2010-07-27 13:22:44 +0000

    Fixed header file includes for this set of tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40342

--- Tests/variable_list.cpp
+++ Tests/variable_list.cpp
@@ -1,7 +1,6 @@
 #include "TiledArray/variable_list.h"
 #include "TiledArray/permutation.h"
-#include <boost/test/unit_test.hpp>
-#include <boost/test/output_test_stream.hpp>
+#include "unit_test_config.h"
 
 //using namespace TiledArray;
 using TiledArray::Permutation;
