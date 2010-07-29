1bc37a3c8119a3cc72321c730c4f7547e3d19077
justusc <justusc@vt.edu>
2010-07-29 18:32:35 +0000

    Modified the iterator type tests so it is more consistent with boost type traits.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40346

--- src/TiledArray/type_traits.h
+++ src/TiledArray/type_traits.h
@@ -2,6 +2,8 @@
 #define TILEDARRAY_TYPE_TRAITS_H__INCLUDED
 
 #include <iterator>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/or.hpp>
 #include <boost/type_traits.hpp>
 
 namespace TiledArray {
@@ -44,42 +46,57 @@ namespace TiledArray {
 
     /// The static member value is true if the type T is a random access iterator
     template<typename T>
-    struct is_random_iterator {
-      static const bool value = boost::is_same<std::random_access_iterator_tag,
-          typename std::iterator_traits<T>::iterator_category>::value;
-    }; // struct is_random_iterator
+    struct is_random_iterator : public boost::is_same<std::random_access_iterator_tag,
+        typename std::iterator_traits<T>::iterator_category>
+    { }; // struct is_random_iterator
+
+    template<typename T>
+    struct is_random_iterator<T*> : public boost::true_type
+    { }; // struct is_random_iterator
 
     /// The static member value is true if the type T is a bidirectional iterator
     template<typename T>
-    struct is_bidirectional_iterator {
-      static const bool value = is_random_iterator<T>::value ||
-          boost::is_same<std::bidirectional_iterator_tag,
-          typename std::iterator_traits<T>::iterator_category>::value;
-    }; // struct is_bidirectional_iterator
+    struct is_bidirectional_iterator : public
+        boost::mpl::if_<
+          boost::mpl::or_<
+            is_random_iterator<T>,
+            boost::is_same<std::bidirectional_iterator_tag, typename std::iterator_traits<T>::iterator_category>
+          >, boost::true_type, boost::false_type
+        >::type
+    { }; // struct is_bidirectional_iterator
 
     /// The static member value is true if the type T is a forward iterator
     template<typename T>
-    struct is_forward_iterator {
-      static const bool value = is_bidirectional_iterator<T>::value ||
-          boost::is_same<std::forward_iterator_tag,
-          typename std::iterator_traits<T>::iterator_category>::value;
-    }; // struct is_forward_iterator
+    struct is_forward_iterator : public
+        boost::mpl::if_<
+          boost::mpl::or_<
+            is_bidirectional_iterator<T>,
+            boost::is_same<std::forward_iterator_tag, typename std::iterator_traits<T>::iterator_category>
+          >, boost::true_type, boost::false_type
+        >::type
+    { }; // struct is_forward_iterator
 
     /// The static member value is true if the type T is an output iterator
     template<typename T>
-    struct is_output_iterator {
-      static const bool value = is_forward_iterator<T>::value ||
-          boost::is_same<std::output_iterator_tag,
-          typename std::iterator_traits<T>::iterator_category>::value;
-    }; // struct is_output_iterator
+    struct is_output_iterator : public
+        boost::mpl::if_<
+          boost::mpl::or_<
+            is_forward_iterator<T>,
+            boost::is_same<std::output_iterator_tag, typename std::iterator_traits<T>::iterator_category>
+          >, boost::true_type, boost::false_type
+        >::type
+    { }; // struct is_output_iterator
 
     /// The static member value is true if the type T is an input iterator
     template<typename T>
-    struct is_input_iterator {
-      static const bool value = is_forward_iterator<T>::value ||
-          boost::is_same<std::input_iterator_tag,
-          typename std::iterator_traits<T>::iterator_category>::value;
-    }; // struct is_input_iterator
+    struct is_input_iterator : public
+        boost::mpl::if_<
+          boost::mpl::or_<
+            is_forward_iterator<T>,
+            boost::is_same<std::input_iterator_tag, typename std::iterator_traits<T>::iterator_category>
+          >, boost::true_type, boost::false_type
+        >::type
+    { }; // struct is_input_iterator
 
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
     /// value is equal to the number of template parameters.
