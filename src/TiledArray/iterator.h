5033e0fe0afbca999c335d316c11991ca8ca50fe
justusc <justusc@vt.edu>
2010-07-30 21:18:17 +0000

    Removed ElementIterator because it was never used and will never be used.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40354

--- src/TiledArray/iterator.h
+++ src/TiledArray/iterator.h
@@ -50,65 +50,6 @@ namespace TiledArray {
       Value current_;
     }; // class IndexIterator
 
-    /// Element Iterator used to iterate over elements of dense arrays.
-    template <typename Value, typename IndexIt, typename Container, typename Reference = Value& >
-    class ElementIterator : public boost::iterator_facade<
-        ElementIterator<Value, IndexIt, Container>, Value, std::output_iterator_tag, Reference>
-    {
-    public:
-      typedef ElementIterator<Value, IndexIt, Container> ElementIterator_;
-      typedef typename IndexIt::value_type index_type;
-      typedef typename boost::remove_const<Container>::type container_type;
-
-      /// Primary constructor
-      ElementIterator(const IndexIt& it, container_type * const container) :
-        container_(container), current_(it)
-      {}
-
-      /// Copy constructor
-      ElementIterator(const ElementIterator_& other) :
-        container_(other.container_), current_(other.current_)
-      {}
-
-      /// Copy constructor for iterators of other types (i.e. const_iterator to iterator).
-      template<typename OtherValue>
-      ElementIterator(const ElementIterator<OtherValue, IndexIt, Container>& other) :
-        container_(other.container_), current_(other.current_)
-      {}
-
-      ~ElementIterator() {}
-
-      const index_type & index() const {
-        return *current_;
-      }
-
-    private:
-      friend class boost::iterator_core_access;
-
-      ElementIterator();
-
-      bool equal(const ElementIterator_ & other) const {
-        return current_ == other.current_ && container_ == other.container_;
-      }
-
-      void increment() {
-        ++current_;
-      }
-
-      Reference dereference() const {
-        return container_->operator[](*current_);
-      }
-
-      template<typename OtherValue>
-      bool equal(const ElementIterator<OtherValue, IndexIt, Container>& other) {
-        return current_ == other.current_ && container_ == other.container_;
-      }
-
-      container_type * container_;
-      IndexIt current_;
-
-    }; // class ElementIterator
-
   } // namespace detail
 
 } // namespace TiledArray
