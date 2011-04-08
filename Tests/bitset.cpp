506218796e49b1d520b1c8c3437b8006d7e824b7
justusc <justusc@vt.edu>
2011-04-08 20:54:13 +0000

    Added bitset.cpp outline.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40432

---
+++ Tests/bitset.cpp
@@ -0,0 +1,23 @@
+#include "unit_test_config.h"
+#include "global_fixture.h"
+#include "TiledArray/bitset.h"
+
+using namespace TiledArray;
+
+struct BitsetFixture {
+
+};
+
+// =============================================================================
+// PredicatedShape Test Suite
+
+
+BOOST_FIXTURE_TEST_SUITE( bitset_suite, BitsetFixture )
+
+BOOST_AUTO_TEST_CASE( constructor )
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
