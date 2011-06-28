a818ad4c569aa1e179aa100f916e47ceb0474ac4
justusc <justusc@vt.edu>
2011-06-28 01:08:13 +0000

    Add a file for reduce task tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40552

---
+++ Tests/reduce_task.cpp
@@ -0,0 +1,16 @@
+#include "TiledArray/reduce_task.h"
+#include "unit_test_config.h"
+
+struct ReduceTaskFixture {
+
+};
+
+BOOST_FIXTURE_TEST_SUITE( reduce_task_suite, ReduceTaskFixture )
+
+BOOST_AUTO_TEST_CASE( constructor )
+{
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
