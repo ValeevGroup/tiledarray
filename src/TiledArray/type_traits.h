6f1b10e003729e2b0514d5f3c0c243bcae79a687
justusc <justusc@vt.edu>
2011-07-18 02:11:45 +0000

    Fixed compile bug.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40580

--- src/TiledArray/type_traits.h
+++ src/TiledArray/type_traits.h
@@ -125,7 +125,7 @@ namespace TiledArray {
       typedef typename C::reference               reference;
       typedef typename C::const_reference         const_reference;
       typedef typename C::size_type               size_type;
-      typedef typename C::difference_type         difference_type
+      typedef typename C::difference_type         difference_type;
     }; // ContainerTraits
 
   } // namespace detail
