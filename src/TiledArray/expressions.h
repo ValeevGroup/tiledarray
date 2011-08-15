48d3c152c634e55cdbc1f09d4f7771fc8a201bed
justusc <justusc@vt.edu>
2011-08-15 20:12:30 +0000

    Changed the expressions to require readable tensor types.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40623

--- src/TiledArray/expressions.h
+++ src/TiledArray/expressions.h
@@ -12,79 +12,72 @@ namespace TiledArray {
   namespace expressions {
 
     template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<!(std::is_scalar<LeftExp>::value || std::is_scalar<RightExp>::value),
-        BinaryTensor<LeftExp, RightExp, std::plus<typename LeftExp::value_type> > >::type
-    operator+(const LeftExp& left, const RightExp& right) {
-      return BinaryTensor<LeftExp, RightExp, std::plus<typename LeftExp::value_type> >(left,
-          right, std::plus<typename LeftExp::value_type>());
+    BinaryTensor<LeftExp, RightExp, std::plus<typename LeftExp::value_type> >
+    operator+(const ReadableTensor<LeftExp>& left, const ReadableTensor<RightExp>& right) {
+      return BinaryTensor<LeftExp, RightExp, std::plus<typename LeftExp::value_type> >(left.derived(),
+          right.derived(), std::plus<typename LeftExp::value_type>());
     }
 
-    template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<std::is_scalar<LeftExp>::value && (! std::is_scalar<RightExp>::value),
-        UnaryTensor<RightExp, std::binder1st<std::plus<typename RightExp::value_type> > > >::type
-    operator+(const LeftExp& left, const RightExp& right) {
-      return UnaryTensor<RightExp, std::binder1st<std::plus<typename RightExp::value_type> > >(right,
+    template <typename RightExp>
+    UnaryTensor<RightExp, std::binder1st<std::plus<typename RightExp::value_type> > >
+    operator+(const typename ReadableTensor<RightExp>::value_type& left, const ReadableTensor<RightExp>& right) {
+      return UnaryTensor<RightExp, std::binder1st<std::plus<typename RightExp::value_type> > >(right.derived(),
           std::bind1st(std::plus<typename RightExp::value_type>(), left));
     }
 
-    template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<(! std::is_scalar<LeftExp>::value) && std::is_scalar<RightExp>::value,
-        UnaryTensor<LeftExp, std::binder2nd<std::plus<typename LeftExp::value_type> > > >::type
-    operator+(const LeftExp& left, const RightExp& right) {
-      return UnaryTensor<LeftExp, std::binder2nd<std::plus<typename LeftExp::value_type> > >(left,
+    template <typename LeftExp>
+    UnaryTensor<LeftExp, std::binder2nd<std::plus<typename LeftExp::value_type> > >
+    operator+(const ReadableTensor<LeftExp>& left, const typename ReadableTensor<LeftExp>::value_type& right) {
+      return UnaryTensor<LeftExp, std::binder2nd<std::plus<typename LeftExp::value_type> > >(left.derived(),
           std::bind2nd(std::plus<typename LeftExp::value_type>(), right));
     }
 
     template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<!(std::is_scalar<LeftExp>::value || std::is_scalar<RightExp>::value),
-        BinaryTensor<LeftExp, RightExp, std::minus<typename LeftExp::value_type> > >::type
-    operator-(const LeftExp& left, const RightExp& right) {
-      return BinaryTensor<LeftExp, RightExp, std::minus<typename LeftExp::value_type> >(left,
-          right, std::minus<typename LeftExp::value_type>());
+    BinaryTensor<LeftExp, RightExp, std::minus<typename LeftExp::value_type> >
+    operator-(const ReadableTensor<LeftExp>& left, const ReadableTensor<RightExp>& right) {
+      return BinaryTensor<LeftExp, RightExp, std::minus<typename LeftExp::value_type> >(left.derived(),
+          right.derived(), std::minus<typename LeftExp::value_type>());
     }
 
-    template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<std::is_scalar<LeftExp>::value && (! std::is_scalar<RightExp>::value),
-        UnaryTensor<RightExp, std::binder1st<std::minus<typename RightExp::value_type> > > >::type
-    operator-(const LeftExp& left, const RightExp& right) {
-      return UnaryTensor<RightExp, std::binder1st<std::minus<typename RightExp::value_type> > >(right,
+    template <typename RightExp>
+    UnaryTensor<RightExp, std::binder1st<std::minus<typename RightExp::value_type> > >
+    operator-(const typename ReadableTensor<RightExp>::value_type& left, const ReadableTensor<RightExp>& right) {
+      return UnaryTensor<RightExp, std::binder1st<std::minus<typename RightExp::value_type> > >(right.derived(),
           std::bind1st(std::minus<typename RightExp::value_type>(), left));
     }
 
-    template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<(! std::is_scalar<LeftExp>::value) && std::is_scalar<RightExp>::value,
-        UnaryTensor<LeftExp, std::binder2nd<std::minus<typename LeftExp::value_type> > > >::type
-    operator-(const LeftExp& left, const RightExp& right) {
-      return UnaryTensor<LeftExp, std::binder2nd<std::minus<typename LeftExp::value_type> > >(left,
+    template <typename LeftExp>
+    UnaryTensor<LeftExp, std::binder2nd<std::minus<typename LeftExp::value_type> > >
+    operator-(const ReadableTensor<LeftExp>& left, const typename ReadableTensor<LeftExp>::value_type& right) {
+      return UnaryTensor<LeftExp, std::binder2nd<std::minus<typename LeftExp::value_type> > >(left.derived(),
           std::bind2nd(std::minus<typename LeftExp::value_type>(), right));
     }
 
-    template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<std::is_scalar<LeftExp>::value && (! std::is_scalar<RightExp>::value),
-        UnaryTensor<RightExp, std::binder1st<std::multiplies<typename RightExp::value_type> > > >::type
-    operator*(const LeftExp& left, const RightExp& right) {
-      return UnaryTensor<RightExp, std::binder1st<std::multiplies<typename RightExp::value_type> > >(right,
+    template <typename RightExp>
+    UnaryTensor<RightExp, std::binder1st<std::multiplies<typename RightExp::value_type> > >
+    operator*(const typename ReadableTensor<RightExp>::value_type& left, const ReadableTensor<RightExp>& right) {
+      return UnaryTensor<RightExp, std::binder1st<std::multiplies<typename RightExp::value_type> > >(right.derived(),
           std::bind1st(std::multiplies<typename RightExp::value_type>(), left));
     }
 
-    template <typename LeftExp, typename RightExp>
-    typename madness::enable_if_c<(! std::is_scalar<LeftExp>::value) && std::is_scalar<RightExp>::value,
-        UnaryTensor<LeftExp, std::binder2nd<std::multiplies<typename LeftExp::value_type> > > >::type
-    operator*(const LeftExp& left, const RightExp& right) {
-      return UnaryTensor<LeftExp, std::binder2nd<std::multiplies<typename LeftExp::value_type> > >(left,
+    template <typename LeftExp>
+    UnaryTensor<LeftExp, std::binder2nd<std::multiplies<typename LeftExp::value_type> > >
+    operator*(const ReadableTensor<LeftExp>& left, const typename ReadableTensor<LeftExp>::value_type& right) {
+      return UnaryTensor<LeftExp, std::binder2nd<std::multiplies<typename LeftExp::value_type> > >(left.derived(),
           std::bind2nd(std::multiplies<typename LeftExp::value_type>(), right));
     }
 
     template <typename ArgExp>
     UnaryTensor<ArgExp, std::negate<typename ArgExp::value_type> >
-    operator-(const ArgExp& arg) {
-      return UnaryTensor<ArgExp, std::negate<typename ArgExp::value_type> >(arg, std::negate<typename ArgExp::value_type>());
+    operator-(const ReadableTensor<ArgExp>& arg) {
+      return UnaryTensor<ArgExp, std::negate<typename ArgExp::value_type> >(arg.derived(),
+          std::negate<typename ArgExp::value_type>());
     }
 
     template <unsigned int DIM, typename ArgExp>
     PermuteTensor<ArgExp, DIM>
-    operator^(const Permutation<DIM>& p, const ArgExp& arg) {
-      return PermuteTensor<ArgExp, DIM>(arg, p);
+    operator^(const Permutation<DIM>& p, const ReadableTensor<ArgExp>& arg) {
+      return PermuteTensor<ArgExp, DIM>(arg.derived(), p);
     }
 
   } // namespace expressions
