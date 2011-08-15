3eb2931aed6b41b30281e074c17aef92832ae50d
justusc <justusc@vt.edu>
2011-08-15 22:38:05 +0000

    Fixed permutation expressions test.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40629

--- Tests/expressions.cpp
+++ Tests/expressions.cpp
@@ -12,44 +12,76 @@ using namespace TiledArray;
 
 struct ExpressionFixture {
   typedef TiledArray::expressions::Tile<int, GlobalFixture::element_coordinate_system> TileN;
+  typedef TileN::value_type value_type;
   typedef TileN::index index;
   typedef TileN::volume_type volume_type;
   typedef TileN::size_array size_array;
-  typedef TileN::range_type RangeN;
+  typedef TileN::range_type range_type;
+  typedef Permutation<GlobalFixture::coordinate_system::dim> PermN;
 
-  static const RangeN r;
+  static const range_type r;
 
   ExpressionFixture() : t(r, 1) {
   }
 
   ~ExpressionFixture() { }
 
+
+  // get a unique value for the given index
+  static value_type get_value(const index i) {
+    index::value_type x = 1;
+    value_type result = 0;
+    for(index::const_iterator it = i.begin(); it != i.end(); ++it, x *= 10)
+      result += *it * x;
+
+    return result;
+  }
+
+  // make a tile to be permuted
+  static TileN make_tile() {
+    index start(0);
+    index finish(0);
+    index::value_type i = 3;
+    for(index::iterator it = finish.begin(); it != finish.end(); ++it, ++i)
+      *it = i;
+
+    range_type r(start, finish);
+    TileN result(r);
+    for(range_type::const_iterator it = r.begin(); it != r.end(); ++it)
+      result[*it] = get_value(*it);
+
+    return result;
+  }
+
+  // make permutation definition object
+  static PermN make_perm() {
+    std::array<std::size_t, GlobalFixture::coordinate_system::dim> temp;
+    for(std::size_t i = 0; i < temp.size(); ++i)
+      temp[i] = i + 1;
+
+    temp.back() = 0;
+
+    return PermN(temp.begin());
+  }
+
   TileN t;
 };
 
-const ExpressionFixture::RangeN ExpressionFixture::r = ExpressionFixture::RangeN(index(0), index(5));
+const ExpressionFixture::range_type ExpressionFixture::r = ExpressionFixture::range_type(index(0), index(5));
 
 BOOST_FIXTURE_TEST_SUITE( expressions_suite , ExpressionFixture )
 
 BOOST_AUTO_TEST_CASE( permutation )
 {
-  typedef TiledArray::CoordinateSystem<3, 0> cs3;
-  Permutation<3> p(1,2,0);
-  Range<cs3> r1(Range<cs3>::index(0,0,0), Range<cs3>::index(2,3,4));
-  Range<cs3> r3(r1);
-  std::array<double, 24> val =  {{0,  1,  2,  3, 10, 11, 12, 13, 20, 21, 22, 23,100,101,102,103,110,111,112,113,120,121,122,123}};
-  //         destination       {{0,100,200,300,  1,101,201,301,  2,102,202,302, 10,110,210,310, 11,111,211,311, 12,112,212,312}}
-  //         permuted index    {{0,  1,  2, 10, 11, 12,100,101,102,110,111,112,200,201,202,210,211,212,300,301,302,310,311,312}}
-  std::array<double, 24> pval = {{0, 10, 20,100,110,120,  1, 11, 21,101,111,121,  2, 12, 22,102,112,122,  3, 13, 23,103,113,123}};
-  expressions::Tile<int, cs3> t1(r1, val.begin());
-  expressions::Tile<int, cs3> t2 = (p ^ t1);
-  BOOST_CHECK_EQUAL(t2.range(), p ^ t1.range()); // check that the dimensions were correctly permuted.
-  BOOST_CHECK_EQUAL_COLLECTIONS(t2.begin(), t2.end(), pval.begin(), pval.end()); // check that the values were correctly permuted.
+  index i;
+  TileN pt = t;
+  Permutation<GlobalFixture::coordinate_system::dim> p = make_perm();
+  pt ^= p;
 
-  expressions::Tile<int, cs3> t3(r3, val.begin());
-  t3 ^= p;
-  BOOST_CHECK_EQUAL(t3.range(), p ^ t1.range()); // check that the dimensions were correctly permuted.
-  BOOST_CHECK_EQUAL_COLLECTIONS(t3.begin(), t3.end(), pval.begin(), pval.end()); // check that the values were correctly permuted.
+  for(range_type::const_iterator it = t.range().begin(); it != t.range().end(); ++it) {
+    // Check that each element is correct
+    BOOST_CHECK_EQUAL(pt[p ^ *it], t[*it]);
+  }
 }
 
 BOOST_AUTO_TEST_CASE( addition )
@@ -66,7 +98,7 @@ BOOST_AUTO_TEST_CASE( addition )
   for(TileN::const_iterator it = t.begin(); it != t.end(); ++it)
     BOOST_CHECK_EQUAL(*it, 1);
 
-  t.resize(RangeN());
+  t.resize(range_type());
   t = TileN() + t1;
   for(TileN::const_iterator it = t.begin(); it != t.end(); ++it)
     BOOST_CHECK_EQUAL(*it, 1);
@@ -89,7 +121,7 @@ BOOST_AUTO_TEST_CASE( subtraction )
   for(TileN::const_iterator it = t.begin(); it != t.end(); ++it)
     BOOST_CHECK_EQUAL(*it, 1);
 
-  t.resize(RangeN());
+  t.resize(range_type());
   t = TileN() - t1;
   for(TileN::const_iterator it = t.begin(); it != t.end(); ++it)
     BOOST_CHECK_EQUAL(*it, -1);
