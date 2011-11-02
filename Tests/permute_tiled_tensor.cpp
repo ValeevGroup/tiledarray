e9550023f3ec8b7dfc51e91fdd1a92052a6ffe75
justusc <justusc@vt.edu>
2011-11-02 19:25:24 +0000

    Implemented some tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40795

--- Tests/permute_tiled_tensor.cpp
+++ Tests/permute_tiled_tensor.cpp
@@ -10,12 +10,13 @@ using namespace TiledArray::expressions;
 struct PermuteTiledTensorFixture : public AnnotatedArrayFixture {
   typedef PermuteTensorFixture::PermN PermN;
 
-  PermuteTiledTensorFixture() {
-
-  }
+  PermuteTiledTensorFixture() : ptt(aa, p) { }
 
 
   static const PermN p;
+
+
+  PermuteTiledTensor<array_annotation, GlobalFixture::coordinate_system::dim> ptt;
 };
 
 
@@ -23,9 +24,39 @@ const PermuteTiledTensorFixture::PermN PermuteTiledTensorFixture::p(PermuteTenso
 
 BOOST_FIXTURE_TEST_SUITE( permute_tiled_tensor_suite, PermuteTiledTensorFixture )
 
-BOOST_AUTO_TEST_CASE( constructors )
+BOOST_AUTO_TEST_CASE( range )
 {
-  PermuteTiledTensor<array_annotation, GlobalFixture::coordinate_system::dim> ptt(aa, p);
+  BOOST_CHECK_EQUAL(ptt.range(), p ^ a.range());
+  BOOST_CHECK_EQUAL(ptt.size(), a.size());
+  BOOST_CHECK_EQUAL(ptt.trange(), p ^ a.trange());
+}
+
+BOOST_AUTO_TEST_CASE( vars )
+{
+  BOOST_CHECK_EQUAL(ptt.vars(), p ^ aa.vars());
+}
+
+BOOST_AUTO_TEST_CASE( shape )
+{
+  BOOST_CHECK_EQUAL(ptt.is_dense(), a.is_dense());
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(ptt.get_shape(), TiledArray::Exception);
+#endif
+}
+
+BOOST_AUTO_TEST_CASE( location )
+{
+  BOOST_CHECK((& ptt.get_world()) == (& a.get_world()));
+  BOOST_CHECK(ptt.get_pmap() == a.get_pmap());
+  for(std::size_t i = 0; i < ptt.size(); ++i) {
+    BOOST_CHECK(! ptt.is_zero(i));
+    BOOST_CHECK_EQUAL(ptt.owner(i), a.owner(i));
+    BOOST_CHECK_EQUAL(ptt.is_local(i), a.is_local(i));
+  }
+
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(ptt.get_shape(), TiledArray::Exception);
+#endif
 }
 
 
