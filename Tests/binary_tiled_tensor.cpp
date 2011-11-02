8badbd42a86d128cb9c810fa51488dc8ce8b9133
justusc <justusc@vt.edu>
2011-11-02 19:49:43 +0000

    Added some tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40799

--- Tests/binary_tiled_tensor.cpp
+++ Tests/binary_tiled_tensor.cpp
@@ -8,20 +8,50 @@ using namespace TiledArray::expressions;
 
 struct BinaryTiledTensorFixture : public AnnotatedArrayFixture {
 
-  BinaryTiledTensorFixture() {
+  BinaryTiledTensorFixture() : btt(aa, aa, std::plus<int>()) {
 
   }
 
+  BinaryTiledTensor<array_annotation, array_annotation, std::plus<int> > btt;
 };
 
 
 
 BOOST_FIXTURE_TEST_SUITE( binary_tiled_tensor_suite, BinaryTiledTensorFixture )
 
-BOOST_AUTO_TEST_CASE( constructors )
+BOOST_AUTO_TEST_CASE( range )
 {
-  BinaryTiledTensor<array_annotation, array_annotation, std::plus<int> > btt(aa, aa, std::plus<int>());
+  BOOST_CHECK_EQUAL(btt.range(), a.range());
+  BOOST_CHECK_EQUAL(btt.size(), a.size());
+  BOOST_CHECK_EQUAL(btt.trange(), a.trange());
 }
 
+BOOST_AUTO_TEST_CASE( vars )
+{
+  BOOST_CHECK_EQUAL(btt.vars(), aa.vars());
+}
+
+BOOST_AUTO_TEST_CASE( shape )
+{
+  BOOST_CHECK_EQUAL(btt.is_dense(), a.is_dense());
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(btt.get_shape(), TiledArray::Exception);
+#endif
+}
+
+BOOST_AUTO_TEST_CASE( location )
+{
+  BOOST_CHECK((& btt.get_world()) == (& a.get_world()));
+  BOOST_CHECK(btt.get_pmap() == a.get_pmap());
+  for(std::size_t i = 0; i < btt.size(); ++i) {
+    BOOST_CHECK(! btt.is_zero(i));
+    BOOST_CHECK_EQUAL(btt.owner(i), a.owner(i));
+    BOOST_CHECK_EQUAL(btt.is_local(i), a.is_local(i));
+  }
+
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(btt.get_shape(), TiledArray::Exception);
+#endif
+}
 
 BOOST_AUTO_TEST_SUITE_END()
