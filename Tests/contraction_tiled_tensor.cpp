39035a9b10cb155407981448a59a77d4ba6501a0
justusc <justusc@vt.edu>
2011-11-03 15:33:17 +0000

    Implemented tests and fixes for tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40803

--- Tests/contraction_tiled_tensor.cpp
+++ Tests/contraction_tiled_tensor.cpp
@@ -8,7 +8,7 @@ using namespace TiledArray::expressions;
 
 struct ContractionTiledTensorFixture : public AnnotatedArrayFixture {
 
-  ContractionTiledTensorFixture() {
+  ContractionTiledTensorFixture() : aar(a, right_var), ctt(aa, aar, cont) {
 
   }
 
@@ -17,6 +17,8 @@ struct ContractionTiledTensorFixture : public AnnotatedArrayFixture {
   static const std::shared_ptr<math::Contraction> cont;
 
 
+  array_annotation aar;
+  ContractionTiledTensor<array_annotation, array_annotation> ctt;
 };
 
 const VariableList ContractionTiledTensorFixture::left_var(
@@ -31,11 +33,41 @@ const std::shared_ptr<math::Contraction> ContractionTiledTensorFixture::cont(new
 
 BOOST_FIXTURE_TEST_SUITE( contraction_tiled_tensor_suite, ContractionTiledTensorFixture )
 
-BOOST_AUTO_TEST_CASE( constructors )
+BOOST_AUTO_TEST_CASE( range )
 {
-  array_annotation aar(a, right_var);
-  ContractionTiledTensor<array_annotation, array_annotation> ctt(aa, aar, cont);
+  DynamicTiledRange dtr = cont->contract_trange(aa.trange(), aar.trange());
+  BOOST_CHECK_EQUAL(ctt.range(), dtr.tiles());
+  BOOST_CHECK_EQUAL(ctt.size(), dtr.tiles().volume());
+  BOOST_CHECK_EQUAL(ctt.trange(), dtr);
 }
 
+BOOST_AUTO_TEST_CASE( vars )
+{
+  VariableList var2(aa.vars().data().front() + "," + aar.vars().data().back());
+  BOOST_CHECK_EQUAL(ctt.vars(), var2);
+}
+
+BOOST_AUTO_TEST_CASE( shape )
+{
+  BOOST_CHECK_EQUAL(ctt.is_dense(), aa.is_dense() && aar.is_dense());
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(ctt.get_shape(), TiledArray::Exception);
+#endif
+}
+
+BOOST_AUTO_TEST_CASE( location )
+{
+  BOOST_CHECK((& ctt.get_world()) == (& a.get_world()));
+  BOOST_CHECK(ctt.get_pmap() == a.get_pmap());
+  for(std::size_t i = 0; i < ctt.size(); ++i) {
+    BOOST_CHECK(! ctt.is_zero(i));
+    BOOST_CHECK_EQUAL(ctt.owner(i), a.owner(i));
+    BOOST_CHECK_EQUAL(ctt.is_local(i), a.is_local(i));
+  }
+
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(ctt.get_shape(), TiledArray::Exception);
+#endif
+}
 
 BOOST_AUTO_TEST_SUITE_END()
