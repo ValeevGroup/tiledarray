fe1e83c9b4db28aca43b26e8737e0af5151abab3
justusc <justusc@vt.edu>
2011-11-07 15:55:12 +0000

    Updated the user header.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40818

--- src/tiled_array.h
+++ src/tiled_array.h
@@ -1,35 +1,7 @@
 #ifndef TILED_ARRAY_H__INCLUDED
 #define TILED_ARRAY_H__INCLUDED
 
-//#include <TiledArray/error.h>
-//#include <TiledArray/type_traits.h>
-#include <TiledArray/utility.h>
-//#include <TiledArray/array_util.h>
-//#include <TiledArray/coordinate_system.h>
-//#include <TiledArray/madness_runtime.h>
-
-//#include <TiledArray/permutation.h>
-//#include <TiledArray/coordinates.h>
-//#include <TiledArray/iterator.h>
-
-//#include <TiledArray/range.h>
-//#include <TiledArray/variable_list.h>
-//#include <TiledArray/annotation.h>
-
-//#include <TiledArray/array_storage.h>
-//#include <TiledArray/tiled_range1.h>
-//#include <TiledArray/annotated_tile.h>
-//#include <TiledArray/annoated_array.h>
-//#include <TiledArray/packed_tile.h>
-//#include <TiledArray/tile_slice.h>
-//#include <TiledArray/tile_math.h>
-
-#include <TiledArray/tiled_range.h>
-#include <TiledArray/tile.h>
-
 #include <TiledArray/array.h>
+#include <TiledArray/expressions.h>
 
-// Include madness
-#define WORLD_INSTANTIATE_STATIC_TEMPLATES
-#include <world/world.h>
 #endif // TILED_ARRAY_H__INCLUDED
