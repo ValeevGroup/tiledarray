86e06073530e1407035d2ba4a8992e33495fa436
justusc <justusc@vt.edu>
2011-11-11 21:22:42 +0000

    Added Binder1st and Binder2nd that are compatible with madness tasks.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40832

---
+++ src/TiledArray/functional.h
@@ -0,0 +1,53 @@
+#ifndef TILEDARRAY_FUNCTIONAL_H__INCLUDED
+#define TILEDARRAY_FUNCTIONAL_H__INCLUDED
+
+#include <boost/functional.hpp>
+
+namespace TiledArray {
+  namespace detail {
+
+    template <class Op>
+    class Binder1st : public boost::binder1st<Op> {
+    public:
+      typedef typename boost::binder1st<Op>::argument_type argument_type;
+      typedef typename boost::binder1st<Op>::result_type result_type;
+    public:
+      Binder1st() : boost::binder1st<Op>(Op(), typename boost::binary_traits<Op>::first_argument_type()) { }
+      Binder1st(typename boost::binary_traits<Op>::param_type x,
+                typename boost::call_traits<typename boost::binary_traits<Op>::first_argument_type>::param_type y)
+          :
+            boost::binder1st<Op>(x,y)
+      {}
+
+      using boost::binder1st<Op>::operator();
+
+      template <typename Archive>
+      void serialize(Archive& ar) const {
+        ar & boost::binder1st<Op>::op & boost::binder1st<Op>::value;
+      }
+    };
+
+    template <class Op>
+    class Binder2nd : public boost::binder2nd<Op> {
+    public:
+      using boost::binder1st<Op>::argument_type;
+      using boost::binder1st<Op>::result_type;
+    public:
+      Binder2nd() : boost::binder2nd<Op>(Op(), typename boost::binary_traits<Op>::second_argument_type()) { }
+      Binder2nd(typename boost::binary_traits<Op>::param_type x,
+                typename boost::call_traits<typename boost::binary_traits<Op>::second_argument_type>::param_type y) :
+          boost::binder2nd<Op>(x,y)
+      { }
+
+      using boost::binder1st<Op>::operator();
+
+      template <typename Archive>
+      void serialize(Archive& ar) const {
+        ar & boost::binder1st<Op>::op & boost::binder1st<Op>::value;
+      }
+    };
+
+  }  // namespace detail
+}  // namespace TiledArray
+
+#endif // TILEDARRAY_FUNCTIONAL_H__INCLUDED
