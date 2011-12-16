edbb6d9df865f62d7af08480332f73b719fb24b4
justusc <justusc@vt.edu>
2011-12-16 22:05:30 +0000

    removed some tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40869

--- Tests/reduce_task.cpp
+++ Tests/reduce_task.cpp
@@ -18,86 +18,6 @@ struct ReduceTaskFixture {
 
 BOOST_FIXTURE_TEST_SUITE( reduce_task_suite, ReduceTaskFixture )
 
-BOOST_AUTO_TEST_CASE( constructors )
-{
-  BOOST_REQUIRE_NO_THROW( (ReduceTaskImpl<int, std::plus<int> >(world, std::plus<int>())) );
-  BOOST_REQUIRE_NO_THROW( (ReduceTask<int, std::plus<int> >(world, std::plus<int>())) );
-  BOOST_REQUIRE_NO_THROW( (ReduceTask<int, std::plus<int> >(rt)) );
-}
-
-BOOST_AUTO_TEST_CASE( impl_reduce_op )
-{
-  ReduceTaskImpl<int, std::plus<int> > impl(world, std::plus<int>());
-
-  // Check that reduce operation works correctly
-  madness::Future<int> v1(5);
-  madness::Future<int> v2(7);
-
-  BOOST_CHECK_EQUAL(impl.reduce(v1,v2), 12);
-}
-
-BOOST_AUTO_TEST_CASE( reduce_op )
-{
-  // Check that reduce operation works correctly
-  BOOST_CHECK_EQUAL(rt(2,3), 5);
-
-  // Check that reduce operation works correctly for futures
-  madness::Future<int> v1(5);
-  madness::Future<int> v2(7);
-
-  BOOST_CHECK_EQUAL(rt(v1,v2), 12);
-}
-
-BOOST_AUTO_TEST_CASE( impl_add_future )
-{
-  ReduceTaskImpl<int, std::plus<int> > impl(world, std::plus<int>());
-
-  BOOST_CHECK_EQUAL(impl.size(), 0ul);
-
-  for(unsigned int i = 1; i < 10; ++i) {
-    madness::Future<int> f;
-    impl.add(f);
-
-    BOOST_CHECK_EQUAL(impl.size(), i);
-  }
-
-  // Check the range
-  madness::Range<ReduceTaskImpl<int, std::plus<int> >::container_type::iterator>
-    range = impl.range(8);
-
-  // Check the range size
-  BOOST_CHECK_EQUAL(range.size(), 9ul);
-  BOOST_CHECK_EQUAL(std::distance(range.begin(), range.end()), 9);
-
-  // Check that none of the elements in the range have been set.
-  for(madness::Range<ReduceTaskImpl<int, std::plus<int> >::container_type::iterator>::iterator it = range.begin(); it != range.end(); ++it)
-    BOOST_CHECK(!(it->probe()));
-}
-
-BOOST_AUTO_TEST_CASE( add_value )
-{
-  BOOST_CHECK_EQUAL(rt.size(), 0ul);
-
-  for(unsigned int i = 1; i < 10; ++i) {
-    rt.add(i);
-
-    BOOST_CHECK_EQUAL(rt.size(), i);
-  }
-}
-
-BOOST_AUTO_TEST_CASE( add_future )
-{
-  BOOST_CHECK_EQUAL(rt.size(), 0ul);
-
-  for(unsigned int i = 1; i < 10; ++i) {
-    madness::Future<int> f;
-    rt.add(f);
-
-    BOOST_CHECK_EQUAL(rt.size(), i);
-  }
-}
-
-
 BOOST_AUTO_TEST_CASE( reduce_value )
 {
   int sum = 0;
@@ -106,7 +26,7 @@ BOOST_AUTO_TEST_CASE( reduce_value )
     rt.add(i);
   }
 
-  madness::Future<int> result = rt();
+  madness::Future<int> result = rt.submit();
 
   BOOST_CHECK_EQUAL(result.get(), sum);
 }
@@ -121,7 +41,7 @@ BOOST_AUTO_TEST_CASE( reduce_future )
     rt.add(f);
   }
 
-  madness::Future<int> result = rt();
+  madness::Future<int> result = rt.submit();
 
   BOOST_CHECK(!(result.probe()));
 
