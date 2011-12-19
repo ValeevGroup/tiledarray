9295d11d615949d230bb0be3a569f260246fc4a5
justusc <justusc@vt.edu>
2011-12-19 19:34:50 +0000

    Added more tests.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40883

--- Tests/distributed_storage.cpp
+++ Tests/distributed_storage.cpp
@@ -48,4 +48,73 @@ BOOST_AUTO_TEST_CASE( get_pmap )
   BOOST_CHECK_EQUAL(t->get_pmap(), pmap);
 }
 
+BOOST_AUTO_TEST_CASE( insert )
+{
+  // Make sure the element is only inserted once when every node inserts it.
+  t->insert(0);
+
+  world.gop.fence();
+  int n = t->size();
+  world.gop.sum(n);
+
+  BOOST_CHECK_EQUAL(n, 1);
+
+  for(std::size_t i = 0; i < t->max_size(); ++i)
+    t->insert(i);
+
+
+  world.gop.fence();
+  n = t->size();
+  world.gop.sum(n);
+
+  BOOST_CHECK_EQUAL(n, t->max_size());
+
+
+  // Check throw for an out-of-range insert.
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(t->insert(t->max_size()), TiledArray::Exception);
+  BOOST_CHECK_THROW(t->insert(t->max_size() + 2), TiledArray::Exception);
+#endif // NDEBUG
+}
+
+BOOST_AUTO_TEST_CASE( set_value )
+{
+  // Check that we can set all elements
+  for(std::size_t i = 0; i < t->max_size(); ++i)
+    if(t->is_local(i))
+      t->set(i, world.rank());
+
+  world.gop.fence();
+  int n = t->size();
+  world.gop.sum(n);
+
+  BOOST_CHECK_EQUAL(n, t->max_size());
+
+  // Check throw for an out-of-range set.
+#ifndef NDEBUG
+  BOOST_CHECK_THROW(t->set(t->max_size(), 1), TiledArray::Exception);
+  BOOST_CHECK_THROW(t->set(t->max_size() + 2, 1), TiledArray::Exception);
+#endif // NDEBUG
+}
+
+BOOST_AUTO_TEST_CASE( array_operator )
+{
+  // Check that elements are inserted properly for access requests.
+  for(std::size_t i = 0; i < t->max_size(); ++i)
+    (*t)[i].probe();
+
+  world.gop.fence();
+  int n = t->size();
+  world.gop.sum(n);
+
+  BOOST_CHECK_EQUAL(n, t->max_size());
+
+  // Check throw for an out-of-range set.
+#ifndef NDEBUG
+  BOOST_CHECK_THROW((*t)[t->max_size()], TiledArray::Exception);
+  BOOST_CHECK_THROW((*t)[t->max_size() + 2], TiledArray::Exception);
+#endif // NDEBUG
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
