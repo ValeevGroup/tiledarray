b1befdb684f7ed495486d44dc827bd88c45ed520
justusc <justusc@vt.edu>
2012-01-25 20:04:50 +0000

    Finished implementing the CCD equations and algorithms.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%40963

--- examples/ccd/ccd.cpp
+++ examples/ccd/ccd.cpp
@@ -27,11 +27,14 @@ int main(int argc, char** argv) {
 
     // Read input data.
 
-    std::cout << "Reading input...";
+    if(world.rank() == 0)
+      std::cout << "Reading input...";
+
     InputData data(input);
     input.close();
 
-    std::cout << " done.\nConstructing Fock tensors...";
+    if(world.rank() == 0)
+      std::cout << " done.\nConstructing Fock tensors...";
 
     // Construct Fock tensor
     Array<double, CoordinateSystem<2> > f_a_oo = data.make_f(world, alpha, occ, occ);
@@ -42,33 +45,43 @@ int main(int argc, char** argv) {
 
     // Fence to make sure Fock tensors are initialized on all nodes
     world.gop.fence();
-    std::cout << " done.\nConstructing v_ab tensors...";
+
+    if(world.rank() == 0)
+      std::cout << " done.\nConstructing v_ab tensors...";
 
     // Construct the integral tensors
     Array<double, CoordinateSystem<4> > v_ab_oooo = data.make_v_ab(world, occ, occ, occ, occ);
     Array<double, CoordinateSystem<4> > v_ab_vvoo = data.make_v_ab(world, vir, vir, occ, occ);
+    Array<double, CoordinateSystem<4> > v_ab_oovv = data.make_v_ab(world, occ, occ, vir, vir);
     Array<double, CoordinateSystem<4> > v_ab_vovo = data.make_v_ab(world, vir, occ, vir, occ);
+    Array<double, CoordinateSystem<4> > v_ab_ovov = data.make_v_ab(world, occ, vir, occ, vir);
     Array<double, CoordinateSystem<4> > v_ab_voov = data.make_v_ab(world, vir, occ, occ, vir);
+    Array<double, CoordinateSystem<4> > v_ab_ovvo = data.make_v_ab(world, occ, vir, vir, occ);
     Array<double, CoordinateSystem<4> > v_ab_vvvv = data.make_v_ab(world, vir, vir, vir, vir);
 
     // Fence to make sure data on all nodes has been initialized
     world.gop.fence();
-    std::cout << " done.\nConstructing v_aa and v_bb tensors...";
+
+    if(world.rank() == 0)
+      std::cout << " done.\nConstructing v_aa and v_bb tensors...";
 
     Array<double, CoordinateSystem<4> > v_aa_oooo = v_ab_oooo("i,j,k,l") - v_ab_oooo("i,j,l,k");
     Array<double, CoordinateSystem<4> > v_aa_vvoo = v_ab_vvoo("a,b,i,j") - v_ab_vvoo("a,b,j,i");
     Array<double, CoordinateSystem<4> > v_aa_vovo = v_ab_vovo("a,i,b,j") - v_ab_voov("a,i,j,b");
+    Array<double, CoordinateSystem<4> > v_aa_oovv = v_ab_oovv("i,j,a,b") - v_ab_oovv("i,j,b,a");
     Array<double, CoordinateSystem<4> > v_aa_vvvv = v_ab_vvvv("a,b,c,d") - v_ab_vvvv("a,b,d,c");
     // Just make references to the data since the input is closed shell.
     Array<double, CoordinateSystem<4> >& v_bb_oooo = v_aa_oooo;
     Array<double, CoordinateSystem<4> >& v_bb_vvoo = v_aa_vvoo;
     Array<double, CoordinateSystem<4> >& v_bb_vovo = v_aa_vovo;
+    Array<double, CoordinateSystem<4> >& v_bb_oovv = v_aa_oovv;
     Array<double, CoordinateSystem<4> >& v_bb_vvvv = v_aa_vvvv;
 
     // Fence again to make sure data all the integral tensors have been initialized
     world.gop.fence();
-    std::cout << " done.\n";
 
+    if(world.rank() == 0)
+      std::cout << " done.\n";
 
     Array<double, CoordinateSystem<4> > t_aa_vvoo(world, v_aa_vvoo.trange(), v_aa_vvoo.get_shape());
     for(Array<double, CoordinateSystem<4> >::range_type::const_iterator it = t_aa_vvoo.range().begin(); it != t_aa_vvoo.range().end(); ++it)
@@ -95,51 +108,158 @@ int main(int argc, char** argv) {
 
     data.clear();
 
-    Array<double, CoordinateSystem<4> >r_aa_vvoo =
-         v_aa_vvoo("p1a,p2a,h1a,h2a")
-        -f_a_vv("p1a,p3a")*t_aa_vvoo("p2a,p3a,h1a,h2a")
-        +f_a_vv("p2a,p3a")*t_aa_vvoo("p1a,p3a,h1a,h2a")
-        +f_a_oo("h3a,h1a")*t_aa_vvoo("p1a,p2a,h2a,h3a")
-        -f_a_oo("h3a,h2a")*t_aa_vvoo("p1a,p2a,h1a,h3a")
-        ;
+    if(world.rank() == 0)
+      std::cout << "Calculating t amplitudes...\n";
 
-    Array<double, CoordinateSystem<4> >r_ab_vvoo =
-         v_ab_vvoo("p1a,p2b,h1a,h2b")
-        +f_a_vv("p1a,p3a")*t_ab_vvoo("p3a,p2b,h1a,h2b")
-        +f_b_vv("p2b,p3b")*t_ab_vvoo("p1a,p3b,h1a,h2b")
-        -f_a_oo("h3a,h1a")*t_ab_vvoo("p1a,p2b,h3a,h2b")
-        -f_b_oo("h3b,h2b")*t_ab_vvoo("p1a,p2b,h1a,h3b");
 
-    Array<double, CoordinateSystem<4> >r_bb_vvoo =
-         v_bb_vvoo("p1b,p2b,h1b,h2b")
-        -f_b_vv("p1b,p3b")*t_bb_vvoo("p2b,p3b,h1b,h2b")
-        +f_b_vv("p2b,p3b")*t_bb_vvoo("p1b,p3b,h1b,h2b")
-        +f_b_oo("h3b,h1b")*t_bb_vvoo("p1b,p2b,h2b,h3b")
-        -f_b_oo("h3b,h2b")*t_bb_vvoo("p1b,p2b,h1b,h3b");
+    double energy = 0.0;
 
-    t_aa_vvoo("a,b,i,j") =
-        make_binary_tiled_tensor(D_vvoo("a,b,i,j"),
-        r_aa_vvoo("a,b,i,j"), std::multiplies<double>())
-        + t_aa_vvoo("a,b,i,j");
+    for(unsigned int i = 0ul; i < 100; ++i) {
 
-    t_ab_vvoo("a,b,i,j") =
-        make_binary_tiled_tensor(D_vvoo("a,b,i,j"),
-        r_ab_vvoo("a,b,i,j"), std::multiplies<double>())
-        + t_ab_vvoo("a,b,i,j");
+      if(world.rank() == 0)
+        std::cout << "Iteration " << i << "\n";
 
-    t_bb_vvoo("a,b,i,j") =
-        make_binary_tiled_tensor(D_vvoo("a,b,i,j"),
-        r_bb_vvoo("a,b,i,j"), std::multiplies<double>())
-        + t_bb_vvoo("a,b,i,j");
+      Array<double, CoordinateSystem<4> > r_aa_vvoo =
+          v_aa_vvoo("p1a,p2a,h1a,h2a")
+          -f_a_vv("p1a,p3a")*t_aa_vvoo("p2a,p3a,h1a,h2a")
+          +f_a_vv("p2a,p3a")*t_aa_vvoo("p1a,p3a,h1a,h2a")
+          +f_a_oo("h3a,h1a")*t_aa_vvoo("p1a,p2a,h2a,h3a")
+          -f_a_oo("h3a,h2a")*t_aa_vvoo("p1a,p2a,h1a,h3a")
+          +0.5*t_aa_vvoo("p3a,p4a,h1a,h2a")*v_aa_vvvv("p1a,p2a,p3a,p4a")
+          +v_ab_voov("p1a,h3b,h1a,p3b")*t_ab_vvoo("p2a,p3b,h2a,h3b")
+          -v_aa_vovo("p1a,h3a,p3a,h1a")*t_aa_vvoo("p2a,p3a,h2a,h3a")
+          -v_ab_voov("p1a,h3b,h2a,p3b")*t_ab_vvoo("p2a,p3b,h1a,h3b")
+          +v_aa_vovo("p1a,h3a,p3a,h2a")*t_aa_vvoo("p2a,p3a,h1a,h3a")
+          -v_ab_voov("p2a,h3b,h1a,p3b")*t_ab_vvoo("p1a,p3b,h2a,h3b")
+          +v_aa_vovo("p2a,h3a,p3a,h1a")*t_aa_vvoo("p1a,p3a,h2a,h3a")
+          +v_ab_voov("p2a,h3b,h2a,p3b")*t_ab_vvoo("p1a,p3b,h1a,h3b")
+          -v_aa_vovo("p2a,h3a,p3a,h2a")*t_aa_vvoo("p1a,p3a,h1a,h3a")
+          +0.5*v_aa_oooo("h3a,h4a,h1a,h2a")*t_aa_vvoo("p1a,p2a,h3a,h4a")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p2a,p4b,h3a,h4b")*t_aa_vvoo("p1a,p3a,h1a,h2a")
+          -0.5*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p2a,p4a,h3a,h4a")*t_aa_vvoo("p1a,p3a,h1a,h2a")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p1a,p4b,h3a,h4b")*t_aa_vvoo("p2a,p3a,h1a,h2a")
+          +0.5*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p1a,p4a,h3a,h4a")*t_aa_vvoo("p2a,p3a,h1a,h2a")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h2a,h4b")*t_aa_vvoo("p1a,p2a,h1a,h3a")
+          -0.5*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p3a,p4a,h2a,h4a")*t_aa_vvoo("p1a,p2a,h1a,h3a")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h1a,h4b")*t_aa_vvoo("p1a,p2a,h2a,h3a")
+          -0.5*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p3a,p4a,h1a,h3a")*t_aa_vvoo("p1a,p2a,h2a,h4a")
+          +0.25*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p3a,p4a,h1a,h2a")*t_aa_vvoo("p1a,p2a,h3a,h4a")
+          +v_bb_oovv("h3b,h4b,p3b,p4b")*t_ab_vvoo("p1a,p3b,h1a,h3b")*t_ab_vvoo("p2a,p4b,h2a,h4b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p1a,p4b,h1a,h4b")*t_aa_vvoo("p2a,p3a,h2a,h3a")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_aa_vvoo("p1a,p3a,h1a,h3a")*t_ab_vvoo("p2a,p4b,h2a,h4b")
+          +v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p1a,p3a,h1a,h3a")*t_aa_vvoo("p2a,p4a,h2a,h4a")
+          -v_bb_oovv("h3b,h4b,p3b,p4b")*t_ab_vvoo("p2a,p3b,h1a,h3b")*t_ab_vvoo("p1a,p4b,h2a,h4b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p2a,p4b,h1a,h4b")*t_aa_vvoo("p1a,p3a,h2a,h3a")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_aa_vvoo("p2a,p3a,h1a,h3a")*t_ab_vvoo("p1a,p4b,h2a,h4b")
+          -v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p2a,p3a,h1a,h3a")*t_aa_vvoo("p1a,p4a,h2a,h4a");
 
-    double energy =
-         0.25 * (  dot(t_aa_vvoo("a,b,i,j"), v_aa_vvoo("a,b,i,j"))
-                 + dot(t_bb_vvoo("a,b,i,j"), v_bb_vvoo("a,b,i,j"))
-                )
-        + dot(t_ab_vvoo("a,b,i,j"), v_ab_vvoo("a,b,i,j"))
-    ;
+      world.gop.fence();
 
-    std::cout << "MP2 energy = " << std::setprecision(12) << energy << "\n";
+      Array<double, CoordinateSystem<4> > r_ab_vvoo =
+          v_ab_vvoo("p1a,p2b,h1a,h2b")
+          +f_a_vv("p1a,p3a")*t_ab_vvoo("p3a,p2b,h1a,h2b")
+          +f_b_vv("p2b,p3b")*t_ab_vvoo("p1a,p3b,h1a,h2b")
+          -f_a_oo("h3a,h1a")*t_ab_vvoo("p1a,p2b,h3a,h2b")
+          -f_b_oo("h3b,h2b")*t_ab_vvoo("p1a,p2b,h1a,h3b")
+          +t_ab_vvoo("p3a,p4b,h1a,h2b")*v_ab_vvvv("p1a,p2b,p3a,p4b")
+          +v_ab_voov("p1a,h3b,h1a,p3b")*t_bb_vvoo("p2b,p3b,h2b,h3b")
+          -v_aa_vovo("p1a,h3a,p3a,h1a")*t_ab_vvoo("p3a,p2b,h3a,h2b")
+          -v_ab_vovo("p1a,h3b,p3a,h2b")*t_ab_vvoo("p3a,p2b,h1a,h3b")
+          -v_ab_ovov("h3a,p2b,h1a,p3b")*t_ab_vvoo("p1a,p3b,h3a,h2b")
+          -v_bb_vovo("p2b,h3b,p3b,h2b")*t_ab_vvoo("p1a,p3b,h1a,h3b")
+          +v_ab_ovvo("h3a,p2b,p3a,h2b")*t_aa_vvoo("p1a,p3a,h1a,h3a")
+          +v_ab_oooo("h3a,h4b,h1a,h2b")*t_ab_vvoo("p1a,p2b,h3a,h4b")
+          -0.5*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p2b,p4b,h3b,h4b")*t_ab_vvoo("p1a,p3b,h1a,h2b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p2b,h3a,h4b")*t_ab_vvoo("p1a,p4b,h1a,h2b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p1a,p4b,h3a,h4b")*t_ab_vvoo("p3a,p2b,h1a,h2b")
+          -0.5*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p1a,p4a,h3a,h4a")*t_ab_vvoo("p3a,p2b,h1a,h2b")
+          -0.5*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p3b,p4b,h2b,h4b")*t_ab_vvoo("p1a,p2b,h1a,h3b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h3a,h2b")*t_ab_vvoo("p1a,p2b,h1a,h4b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h1a,h4b")*t_ab_vvoo("p1a,p2b,h3a,h2b")
+          +0.5*v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p3a,p4a,h1a,h3a")*t_ab_vvoo("p1a,p2b,h4a,h2b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h1a,h2b")*t_ab_vvoo("p1a,p2b,h3a,h4b")
+          +v_bb_oovv("h3b,h4b,p3b,p4b")*t_ab_vvoo("p1a,p3b,h1a,h3b")*t_bb_vvoo("p2b,p4b,h2b,h4b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p1a,p4b,h1a,h4b")*t_ab_vvoo("p3a,p2b,h3a,h2b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_aa_vvoo("p1a,p3a,h1a,h3a")*t_bb_vvoo("p2b,p4b,h2b,h4b")
+          +v_aa_oovv("h3a,h4a,p3a,p4a")*t_aa_vvoo("p1a,p3a,h1a,h3a")*t_ab_vvoo("p4a,p2b,h4a,h2b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p2b,h1a,h4b")*t_ab_vvoo("p1a,p4b,h3a,h2b");
+
+      world.gop.fence();
+
+      Array<double, CoordinateSystem<4> > r_bb_vvoo =
+          v_bb_vvoo("p1b,p2b,h1b,h2b")
+          -f_b_vv("p1b,p3b")*t_bb_vvoo("p2b,p3b,h1b,h2b")
+          +f_b_vv("p2b,p3b")*t_bb_vvoo("p1b,p3b,h1b,h2b")
+          +f_b_oo("h3b,h1b")*t_bb_vvoo("p1b,p2b,h2b,h3b")
+          -f_b_oo("h3b,h2b")*t_bb_vvoo("p1b,p2b,h1b,h3b")
+          +0.5*t_bb_vvoo("p3b,p4b,h1b,h2b")*v_bb_vvvv("p1b,p2b,p3b,p4b")
+          -v_bb_vovo("p1b,h3b,p3b,h1b")*t_bb_vvoo("p2b,p3b,h2b,h3b")
+          +v_ab_ovvo("h3a,p1b,p3a,h1b")*t_ab_vvoo("p3a,p2b,h3a,h2b")
+          +v_bb_vovo("p1b,h3b,p3b,h2b")*t_bb_vvoo("p2b,p3b,h1b,h3b")
+          -v_ab_ovvo("h3a,p1b,p3a,h2b")*t_ab_vvoo("p3a,p2b,h3a,h1b")
+          +v_bb_vovo("p2b,h3b,p3b,h1b")*t_bb_vvoo("p1b,p3b,h2b,h3b")
+          -v_ab_ovvo("h3a,p2b,p3a,h1b")*t_ab_vvoo("p3a,p1b,h3a,h2b")
+          -v_bb_vovo("p2b,h3b,p3b,h2b")*t_bb_vvoo("p1b,p3b,h1b,h3b")
+          +v_ab_ovvo("h3a,p2b,p3a,h2b")*t_ab_vvoo("p3a,p1b,h3a,h1b")
+          +0.5*v_bb_oooo("h3b,h4b,h1b,h2b")*t_bb_vvoo("p1b,p2b,h3b,h4b")
+          -0.5*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p2b,p4b,h3b,h4b")*t_bb_vvoo("p1b,p3b,h1b,h2b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p2b,h3a,h4b")*t_bb_vvoo("p1b,p4b,h1b,h2b")
+          +0.5*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p1b,p4b,h3b,h4b")*t_bb_vvoo("p2b,p3b,h1b,h2b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p1b,h3a,h4b")*t_bb_vvoo("p2b,p4b,h1b,h2b")
+          -0.5*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p3b,p4b,h2b,h4b")*t_bb_vvoo("p1b,p2b,h1b,h3b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h3a,h2b")*t_bb_vvoo("p1b,p2b,h1b,h4b")
+          -0.5*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p3b,p4b,h1b,h3b")*t_bb_vvoo("p1b,p2b,h2b,h4b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p4b,h3a,h1b")*t_bb_vvoo("p1b,p2b,h2b,h4b")
+          +0.25*v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p3b,p4b,h1b,h2b")*t_bb_vvoo("p1b,p2b,h3b,h4b")
+          +v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p1b,p3b,h1b,h3b")*t_bb_vvoo("p2b,p4b,h2b,h4b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p2b,h3a,h2b")*t_bb_vvoo("p1b,p4b,h1b,h4b")
+          +v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p1b,h3a,h1b")*t_bb_vvoo("p2b,p4b,h2b,h4b")
+          +v_aa_oovv("h3a,h4a,p3a,p4a")*t_ab_vvoo("p3a,p1b,h3a,h1b")*t_ab_vvoo("p4a,p2b,h4a,h2b")
+          -v_bb_oovv("h3b,h4b,p3b,p4b")*t_bb_vvoo("p2b,p3b,h1b,h3b")*t_bb_vvoo("p1b,p4b,h2b,h4b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p1b,h3a,h2b")*t_bb_vvoo("p2b,p4b,h1b,h4b")
+          -v_ab_oovv("h3a,h4b,p3a,p4b")*t_ab_vvoo("p3a,p2b,h3a,h1b")*t_bb_vvoo("p1b,p4b,h2b,h4b")
+          -v_aa_oovv("h3a,h4a,p3a,p4a")*t_ab_vvoo("p3a,p2b,h3a,h1b")*t_ab_vvoo("p4a,p1b,h4a,h2b");
+
+
+      world.gop.fence();
+
+      t_aa_vvoo("a,b,i,j") =
+          make_binary_tiled_tensor(D_vvoo("a,b,i,j"),
+          r_aa_vvoo("a,b,i,j"), std::multiplies<double>())
+          + t_aa_vvoo("a,b,i,j");
+
+      t_ab_vvoo("a,b,i,j") =
+          make_binary_tiled_tensor(D_vvoo("a,b,i,j"),
+          r_ab_vvoo("a,b,i,j"), std::multiplies<double>())
+          + t_ab_vvoo("a,b,i,j");
+
+      t_bb_vvoo("a,b,i,j") =
+          make_binary_tiled_tensor(D_vvoo("a,b,i,j"),
+          r_bb_vvoo("a,b,i,j"), std::multiplies<double>())
+          + t_bb_vvoo("a,b,i,j");
+
+      const double error = TiledArray::expressions::norm2(r_aa_vvoo("a,b,i,j")
+          + r_ab_vvoo("a,b,i,j") + r_bb_vvoo("a,b,i,j"));
+
+      energy =
+           0.25 * (  dot(t_aa_vvoo("a,b,i,j"), v_aa_vvoo("a,b,i,j"))
+                   + dot(t_bb_vvoo("a,b,i,j"), v_bb_vvoo("a,b,i,j"))
+                  )
+          + dot(t_ab_vvoo("a,b,i,j"), v_ab_vvoo("a,b,i,j"));
+
+      world.gop.fence();
+
+      if(world.rank() == 0)
+        std::cout << " error  = "  << std::setprecision(12) << error << "\n"
+                  << " energy = " << std::setprecision(12) << energy << "\n";
+
+      if(error < 1.0e-10)
+        break;
+    }
+
+
+
+    std::cout << "CCD energy = " << std::setprecision(12) << energy << "\n";
     std::cout << "Done!\n";
 
   } else  {
