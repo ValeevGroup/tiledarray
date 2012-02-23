e35f5525db50da9e0e9384d4600b4bc8d9db400d
justusc <justusc@vt.edu>
2012-02-23 17:23:09 +0000

    More goto blas updates.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401017

--- src/TiledArray/mxmt.h
+++ src/TiledArray/mxmt.h
@@ -52,13 +52,15 @@ namespace TiledArray {
     inline void mxmT(const long m, const long n, const long k, const std::complex<double>* a, const std::complex<double>* b, std::complex<double>* c) {
       std::complex<double> one(1.0);
       cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, &one,
-          reinterpret_cast<double*>(a), k, reinterpret_cast<double*>(b), k, &one, c, n);
+          const_cast<double*>(reinterpret_cast<double*>(a)), k,
+          const_cast<double*>(reinterpret_cast<double*>(b)), k, &one, c, n);
     }
 
     inline void mxmT(const long m, const long n, const long k, const std::complex<float>* a, const std::complex<float>* b, std::complex<float>* c) {
       std::complex<float> one(1.0);
       cblas_cgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, &one,
-          reinterpret_cast<float*>(a), k, reinterpret_cast<float*>(b), k, &one, c, n);
+          const_cast<float*>(reinterpret_cast<const float*>(a)), k,
+          const_cast<float*>(reinterpret_cast<const float*>(b)), k, &one, c, n);
     }
 
 #endif // TILEDARRAY_HAS_CBLAS
