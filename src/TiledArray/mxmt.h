a11a976e6f1a2a1b140634950dbf0546285f1770
justusc <justusc@vt.edu>
2012-02-23 17:10:04 +0000

    Fixed a const issue with Goto BLAS.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401015

--- src/TiledArray/mxmt.h
+++ src/TiledArray/mxmt.h
@@ -39,22 +39,26 @@ namespace TiledArray {
 
     inline void mxmT(const long m, const long n, const long k, const double* a, const double* b, double* c) {
       const double one = 1.0;
-      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, one, a, k, b, k, one, c, n);
+      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, one,
+          const_cast<double*>(a), k, const_cast<double*>(b), k, one, c, n);
     }
 
     inline void mxmT(const long m, const long n, const long k, const float* a, const float* b, float* c) {
-      const double one = 1.0;
-      cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, one, a, k, b, k, one, c, n);
+      const float one = 1.0;
+      cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, one,
+          const_cast<float*>(a), k, const_cast<float*>(b), k, one, c, n);
     }
 
     inline void mxmT(const long m, const long n, const long k, const std::complex<double>* a, const std::complex<double>* b, std::complex<double>* c) {
       std::complex<double> one(1.0);
-      cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, &one, a, k, b, k, &one, c, n);
+      cblas_zgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, &one,
+          const_cast<std::complex<double>*>(a), k, const_cast<std::complex<double>*>(b), k, &one, c, n);
     }
 
     inline void mxmT(const long m, const long n, const long k, const std::complex<float>* a, const std::complex<float>* b, std::complex<float>* c) {
       std::complex<float> one(1.0);
-      cblas_cgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, &one, a, k, b, k, &one, c, n);
+      cblas_cgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, n, k, &one,
+          const_cast<std::complex<float>*>(a), k, const_cast<std::complex<float>*>(b), k, &one, c, n);
     }
 
 #endif // TILEDARRAY_HAS_CBLAS
