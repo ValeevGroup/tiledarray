a8b9e406667915a98ce935b4ae162a8726b6248a
justusc <justusc@vt.edu>
2012-02-27 17:16:37 +0000

    --HG-- extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401025

---
+++ examples/mpi_tests/mpi_bandwidth.c
@@ -0,0 +1,157 @@
+/****************************************************************************
+ * FILE: mpi_bandwidth.c
+ * DESCRIPTION:
+ *   Provides point-to-point communications timings for any even
+ *   number of MPI tasks.
+ * AUTHOR: Blaise Barney
+ * LAST REVISED: 04/13/05
+ ****************************************************************************/
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define MAXTASKS      8192
+/* Change the next four parameters to suit your case */
+#define STARTSIZE     100000
+#define ENDSIZE       1000000
+#define INCREMENT     100000
+#define ROUNDTRIPS    100
+
+int main (int argc, char *argv[])
+{
+  int     numtasks, rank, n, i, j, rndtrps, nbytes, start, end, incr,
+  src, dest, rc, tag=1, taskpairs[MAXTASKS], namelength;
+  double  thistime, bw, bestbw, worstbw, totalbw, avgbw,
+  bestall, avgall, worstall,
+  timings[MAXTASKS/2][3], tmptimes[3],
+  resolution, t1, t2;
+  char    msgbuf[ENDSIZE], host[MPI_MAX_PROCESSOR_NAME],
+  hostmap[MAXTASKS][MPI_MAX_PROCESSOR_NAME];
+  struct  timeval tv1, tv2;
+  MPI_Status status;
+
+  /* Some initializations and error checking */
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  if (numtasks % 2 != 0) {
+    printf("ERROR: Must be an even number of tasks!  Quitting...\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+    exit(0);
+  }
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  start = STARTSIZE;
+  end = ENDSIZE;
+  incr = INCREMENT;
+  rndtrps = ROUNDTRIPS;
+  for (i=0; i<end; i++)
+    msgbuf[i] = 'x';
+
+  /* All tasks send their host name to task 0 */
+  MPI_Get_processor_name(host, &namelength);
+  MPI_Gather(&host, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, &hostmap,
+      MPI_MAX_PROCESSOR_NAME, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+  /* Determine who my send/receive partner is and tell task 0 */
+  if (rank < numtasks/2)
+    dest = src = numtasks/2 + rank;
+  if (rank >= numtasks/2)
+    dest = src = rank - numtasks/2;
+  MPI_Gather(&dest, 1, MPI_INT, &taskpairs, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    resolution = MPI_Wtick();
+    printf("\n******************** MPI Bandwidth Test ********************\n");
+    printf("Message start size= %d bytes\n",start);
+    printf("Message finish size= %d bytes\n",end);
+    printf("Incremented by %d bytes per iteration\n",incr);
+    printf("Roundtrips per iteration= %d\n",rndtrps);
+    printf("MPI_Wtick resolution = %e\n",resolution);
+    printf("************************************************************\n");
+    for (i=0; i<numtasks; i++)
+      printf("task %4d is on %s partner=%4d\n",i,hostmap[i],taskpairs[i]);
+    printf("************************************************************\n");
+  }
+
+
+  /*************************** first half of tasks *****************************/
+  /* These tasks send/receive messages with their partner task, and then do a  */
+  /* few bandwidth calculations based upon message size and timings.           */
+
+  if (rank < numtasks/2) {
+    for (n=start; n<=end; n=n+incr) {
+      bestbw = 0.0;
+      worstbw = .99E+99;
+      totalbw = 0.0;
+      nbytes =  sizeof(char) * n;
+      for (i=1; i<=rndtrps; i++){
+        t1 = MPI_Wtime();
+        MPI_Send(&msgbuf, n, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        MPI_Recv(&msgbuf, n, MPI_CHAR, src, tag, MPI_COMM_WORLD, &status);
+        t2 = MPI_Wtime();
+        thistime = t2 - t1;
+        bw = ((double)nbytes * 2) / thistime;
+        totalbw = totalbw + bw;
+        if (bw > bestbw ) bestbw = bw;
+        if (bw < worstbw ) worstbw = bw;
+      }
+      /* Convert to megabytes per second */
+      bestbw = bestbw/1000000.0;
+      avgbw = (totalbw/1000000.0)/(double)rndtrps;
+      worstbw = worstbw/1000000.0;
+
+      /* Task 0 collects timings from all relevant tasks */
+      if (rank == 0) {
+        /* Keep track of my own timings first */
+        timings[0][0] = bestbw;
+        timings[0][1] = avgbw;
+        timings[0][2] = worstbw;
+        /* Initialize overall averages */
+        bestall = 0.0;
+        avgall = 0.0;
+        worstall = 0.0;
+        /* Now receive timings from other tasks and print results. Note */
+        /* that this loop will be appropriately skipped if there are    */
+        /* only two tasks. */
+        for (j=1; j<numtasks/2; j++)
+          MPI_Recv(&timings[j], 3, MPI_DOUBLE, j, tag, MPI_COMM_WORLD, &status);
+        printf("***Message size: %8d *** best  /  avg  / worst (MB/sec)\n",n);
+        for (j=0; j<numtasks/2; j++) {
+          printf("   task pair: %4d - %4d:    %4.2f / %4.2f / %4.2f \n",
+              j, taskpairs[j], timings[j][0], timings[j][1], timings[j][2]);
+          bestall += timings[j][0];
+          avgall += timings[j][1];
+          worstall += timings[j][2];
+        }
+        printf("   OVERALL AVERAGES:          %4.2f / %4.2f / %4.2f \n\n",
+            bestall/(numtasks/2), avgall/(numtasks/2), worstall/(numtasks/2));
+      }
+      else {
+        /* Other tasks send their timings to task 0 */
+        tmptimes[0] = bestbw;
+        tmptimes[1] = avgbw;
+        tmptimes[2] = worstbw;
+        MPI_Send(tmptimes, 3, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+      }
+    }
+  }
+
+
+
+  /**************************** second half of tasks ***************************/
+  /* These tasks do nothing more than send and receive with their partner task */
+
+  if (rank >= numtasks/2) {
+    for (n=start; n<=end; n=n+incr) {
+      for (i=1; i<=rndtrps; i++){
+        MPI_Recv(&msgbuf, n, MPI_CHAR, src, tag, MPI_COMM_WORLD, &status);
+        MPI_Send(&msgbuf, n, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+      }
+    }
+  }
+
+
+  MPI_Finalize();
+
+}  /* end of main */
---
+++ examples/mpi_tests/mpi_latency.c
@@ -0,0 +1,112 @@
+/******************************************************************************
+ * FILE: mpi_latency.c
+ * DESCRIPTION:
+ *   MPI Latency Timing Program - C Version
+ *   In this example code, a MPI communication timing test is performed.
+ *   MPI task 0 will send "reps" number of 1 byte messages to MPI task 1,
+ *   waiting for a reply between each rep. Before and after timings are made
+ *   for each rep and an average calculated when completed.
+ * AUTHOR: Blaise Barney
+ * LAST REVISED: 04/13/05
+ ******************************************************************************/
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#define NUMBER_REPS 1000
+
+int main (int argc, char *argv[])
+{
+  int reps,                   /* number of samples per test */
+  tag,                    /* MPI message tag parameter */
+  numtasks,               /* number of MPI tasks */
+  rank,                   /* my MPI task number */
+  dest, source,           /* send/receive task designators */
+  avgT,                   /* average time per rep in microseconds */
+  rc,                     /* return code */
+  n;
+  double T1, T2,              /* start/end times per rep */
+  sumT,                   /* sum of all reps times */
+  deltaT;                 /* time for one rep */
+  char msg;                   /* buffer containing 1 byte message */
+  MPI_Status status;          /* MPI receive routine parameter */
+
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  if (rank == 0 && numtasks != 2) {
+    printf("Number of tasks = %d\n",numtasks);
+    printf("Only need 2 tasks - extra will be ignored...\n");
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  sumT = 0;
+  msg = 'x';
+  tag = 1;
+  reps = NUMBER_REPS;
+
+  if (rank == 0) {
+    /* round-trip latency timing test */
+    printf("task %d has started...\n", rank);
+    printf("Beginning latency timing test. Number of reps = %d.\n", reps);
+    printf("***************************************************\n");
+    printf("Rep#       T1               T2            deltaT\n");
+    dest = 1;
+    source = 1;
+    for (n = 1; n <= reps; n++) {
+      T1 = MPI_Wtime();     /* start time */
+      /* send message to worker - message tag set to 1.  */
+      /* If return code indicates error quit */
+      rc = MPI_Send(&msg, 1, MPI_BYTE, dest, tag, MPI_COMM_WORLD);
+      if (rc != MPI_SUCCESS) {
+        printf("Send error in task 0!\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+        exit(1);
+      }
+      /* Now wait to receive the echo reply from the worker  */
+      /* If return code indicates error quit */
+      rc = MPI_Recv(&msg, 1, MPI_BYTE, source, tag, MPI_COMM_WORLD,
+          &status);
+      if (rc != MPI_SUCCESS) {
+        printf("Receive error in task 0!\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+        exit(1);
+      }
+      T2 = MPI_Wtime();     /* end time */
+
+      /* calculate round trip time and print */
+      deltaT = T2 - T1;
+      printf("%4d  %8.8f  %8.8f  %2.8f\n", n, T1, T2, deltaT);
+      sumT += deltaT;
+    }
+    avgT = (sumT*1000000)/reps;
+    printf("***************************************************\n");
+    printf("\n*** Avg round trip time = %d microseconds\n", avgT);
+    printf("*** Avg one way latency = %d microseconds\n", avgT/2);
+  }
+
+  else if (rank == 1) {
+    printf("task %d has started...\n", rank);
+    dest = 0;
+    source = 0;
+    for (n = 1; n <= reps; n++) {
+      rc = MPI_Recv(&msg, 1, MPI_BYTE, source, tag, MPI_COMM_WORLD,
+          &status);
+      if (rc != MPI_SUCCESS) {
+        printf("Receive error in task 1!\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+        exit(1);
+      }
+      rc = MPI_Send(&msg, 1, MPI_BYTE, dest, tag, MPI_COMM_WORLD);
+      if (rc != MPI_SUCCESS) {
+        printf("Send error in task 1!\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+        exit(1);
+      }
+    }
+  }
+
+  MPI_Finalize();
+  exit(0);
+}
