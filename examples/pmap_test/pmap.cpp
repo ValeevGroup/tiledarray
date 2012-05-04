c1a2ed08bb599b35ffdcaeaddc85786d8d4f6785
justusc <justusc@vt.edu>
2012-05-04 13:40:17 +0000

    Only do work on node 0.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401074

--- examples/pmap_test/pmap.cpp
+++ examples/pmap_test/pmap.cpp
@@ -12,22 +12,40 @@ std::vector<ProcessID> make_map(std::size_t m, std::size_t n, std::shared_ptr<Ti
   return map;
 }
 
+void print(std::size_t m, std::size_t n, const std::vector<ProcessID>& map) {
+  for(std::size_t i = 0ul; i < m; ++i) {
+    for(std::size_t j = 0ul; j < n; ++j)
+      std::cout << map[i * n + j] << " ";
+    std::cout << "\n";
+  }
+}
+
 int main(int argc, char** argv) {
   madness::initialize(argc,argv);
   {
     madness::World world(MPI::COMM_WORLD);
 
-    std::size_t m = 20;
-    std::size_t n = 20;
+    if(world.rank() == 0) {
+      std::size_t m = 20;
+      std::size_t n = 20;
 
-    std::shared_ptr<TiledArray::Pmap<std::size_t> > blockd_pmap(new TiledArray::detail::BlockedPmap(world, m * n));
+      std::shared_ptr<TiledArray::Pmap<std::size_t> > blocked_pmap(new TiledArray::detail::BlockedPmap(world, m * n));
+      std::vector<ProcessID> blocked_map = make_map(m, n, blocked_pmap);
+      std::cout << "Block\n";
+      print(m, n, blocked_map);
 
 
-    std::shared_ptr<TiledArray::Pmap<std::size_t> > cyclic_pmap(new TiledArray::detail::CyclicPmap(world, m, n));
 
+      std::shared_ptr<TiledArray::Pmap<std::size_t> > cyclic_pmap(new TiledArray::detail::CyclicPmap(world, m, n));
+      std::vector<ProcessID> cyclic_map = make_map(m, n, cyclic_pmap);
+      std::cout << "\n\nCyclic\n";
+      print(m, n, cyclic_map);
 
-    std::shared_ptr<TiledArray::Pmap<std::size_t> > hash_pmap(new TiledArray::detail::HashPmap(world, m * n));
-
+      std::shared_ptr<TiledArray::Pmap<std::size_t> > hash_pmap(new TiledArray::detail::HashPmap(world, m * n));
+      std::vector<ProcessID> hash_map = make_map(m, n, hash_pmap);
+      std::cout << "\n\nHash\n";
+      print(m, n, hash_map);
+    }
   }
   madness::finalize();
 
