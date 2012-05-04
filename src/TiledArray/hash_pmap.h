4890fa3fb01af0330973f1b1187a965b4df3b4c7
justusc <justusc@vt.edu>
2012-05-04 13:02:23 +0000

    Add hashed process map.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401071

---
+++ src/TiledArray/hash_pmap.h
@@ -0,0 +1,99 @@
+#ifndef TILEDARRAY_HASH_PMAP_H__INCLUDED
+#define TILEDARRAY_HASH_PMAP_H__INCLUDED
+
+#include <world/worldtypes.h>
+#include <world/worldhash.h>
+#include <vector>
+#include <world/shared_ptr.h>
+
+namespace TiledArray {
+
+  /// Process map base class
+  template <typename Key>
+  class HashPmap : public Pmap<std::size_t> {
+  public:
+    typedef Pmap<std::size_t>::key_type key_type;
+    typedef Pmap<std::size_t>::const_iterator const_iterator;
+
+    HashPmap(madness::World& world, std::size_t size) :
+        rank_(world.rank()),
+        procs_(world.size()),
+        size_(size),
+        seed_(0ul),
+        local_()
+    { }
+
+  private:
+
+    HashPmap(const HashPmap& other) :
+        rank_(other.rank_),
+        procs_(other.procs_),
+        size_(other.size),
+        seed_(0ul),
+        local_()
+    { }
+
+  public:
+
+    virtual ~HashPmap() { }
+
+    /// Initialize the hashing seed and local iterator
+    virtual void set_seed(madness::hashT seed) {
+      seed_ = seed;
+
+      for(key_type i = 0ul; i < size_; ++i) {
+        if(HashPmap::owner(i) == rank_)
+          local_.push_back(i);
+      }
+    }
+
+    /// Create a copy of this pmap
+
+    /// \return A shared pointer to the new object
+    virtual std::shared_ptr<Pmap<key_type> > clone() const {
+      return std::shared_ptr<Pmap<key_type> >(new HashPmap(*this));
+    }
+
+    /// Key owner
+
+    /// \return The \c ProcessID of the process that owns \c key .
+    virtual ProcessID owner(const key_type& key) const {
+      madness::hashT seed = seed_;
+      madness::hash_combine(seed, key);
+      return seed % procs_;
+    }
+
+
+    /// Local size accessor
+
+    /// \return The number of local elements
+    virtual std::size_t local_size() const { return local_.size(); }
+
+    /// Local elements
+
+    /// \return \c true when there are no local elements, otherwise \c false .
+    virtual bool empty() const { return local_.empty(); }
+
+    /// Begin local element iterator
+
+    /// \return An iterator that points to the beginning of the local element set
+    virtual const_iterator begin() const { return local_.begin(); }
+
+
+    /// End local element iterator
+
+    /// \return An iterator that points to the beginning of the local element set
+    virtual const_iterator end() const { return local_.end(); }
+
+  private:
+    const std::size_t rank_;
+    const std::size_t procs_;
+    const std::size_t size_;
+    std::size_t seed_;
+    std::vector<key_type> local_;
+  }; // class HashPmap
+
+}  // namespace TiledArray
+
+
+#endif // TILEDARRAY_HASH_PMAP_H__INCLUDED
