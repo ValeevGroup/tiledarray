a2e56beb1522fd35237ce7abab2561e89d92ca22
justusc <justusc@vt.edu>
2012-05-04 14:25:30 +0000

    Added HashPmap test.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401076

---
+++ tests/hash_pmap.cpp
@@ -0,0 +1,83 @@
+#include "TiledArray/hash_pmap.h"
+#include "unit_test_config.h"
+#include "global_fixture.h"
+
+using namespace TiledArray;
+
+struct HashPmapFixture {
+
+  HashPmapFixture() : pmap(* GlobalFixture::world, 50ul) {
+    pmap.set_seed();
+  }
+
+  detail::HashPmap pmap;
+};
+
+
+// =============================================================================
+// BlockdPmap Test Suite
+
+
+BOOST_FIXTURE_TEST_SUITE( hash_pmap_suite, HashPmapFixture )
+
+BOOST_AUTO_TEST_CASE( constructor )
+{
+  BOOST_REQUIRE_NO_THROW(TiledArray::detail::HashPmap pmap(* GlobalFixture::world, 50ul));
+}
+
+BOOST_AUTO_TEST_CASE( owner )
+{
+  const std::size_t rank = GlobalFixture::world->rank();
+  const std::size_t size = GlobalFixture::world->size();
+
+  ProcessID* p_owner = new ProcessID[size];
+
+  for(std::size_t i = 0ul; i < 50ul; ++i) {
+    std::fill_n(p_owner, size, 0);
+    p_owner[rank] = pmap.owner(i);
+
+    // check that the value is in range
+    BOOST_CHECK_LT(p_owner[rank], size);
+    GlobalFixture::world->gop.sum(p_owner, size);
+
+    // Make sure everyone agrees on who owns what.
+    for(std::size_t p = 0ul; p < size; ++p)
+      BOOST_CHECK_EQUAL(p_owner[p], p_owner[rank]);
+  }
+
+  delete [] p_owner;
+}
+
+BOOST_AUTO_TEST_CASE( local_size )
+{
+  std::size_t total_size = pmap.local_size();
+  GlobalFixture::world->gop.sum(total_size);
+
+  BOOST_CHECK_EQUAL(total_size, 50ul);
+  BOOST_CHECK(pmap.empty() == (pmap.local_size() == 0ul));
+
+}
+
+BOOST_AUTO_TEST_CASE( local_group )
+{
+  // Make sure the total number of elements in the local groups is correct
+  std::size_t total_size = std::distance(pmap.begin(), pmap.end());
+  GlobalFixture::world->gop.sum(total_size);
+
+  BOOST_CHECK_EQUAL(total_size, 50ul);
+
+  // Check that all local elements map to this rank
+  for(detail::HashPmap::const_iterator it = pmap.begin(); it != pmap.end(); ++it) {
+    BOOST_CHECK_EQUAL(pmap.owner(*it), GlobalFixture::world->rank());
+  }
+
+  // Check that all elements owned by this rank are in the local group exactly once
+  for(std::size_t i = 0; i < 50ul; ++i) {
+    if(pmap.owner(i) == GlobalFixture::world->rank()) {
+      BOOST_CHECK_EQUAL(std::count(pmap.begin(), pmap.end(), i), 1);
+    }
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
