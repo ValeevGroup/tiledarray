00b7caf4386b931d10c0ba47eb1f10985eb38418
justusc <justusc@vt.edu>
2012-05-07 21:25:39 +0000

    Added an object to broadcast tasks within a group of processes using a binary tree.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401092

---
+++ tests/bcast_group.cpp
@@ -0,0 +1,16 @@
+#include "TiledArray/bcast_group.h"
+#include "unit_test_config.h"
+
+struct BcastGroupFixture {
+
+}; // BcastGroupFixture
+
+
+BOOST_FIXTURE_TEST_SUITE( bcast_group_suite , BcastGroupFixture )
+
+BOOST_AUTO_TEST_CASE( constructors )
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
