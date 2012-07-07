8f1c0d0bdff7f26e8a90e9a179a912aead83a039
justusc <justusc@vt.edu>
2012-07-07 11:37:12 +0000

    Added an assertion to check that shape has the correct size.
    --HG--
    extra : convert_revision : svn%3A93e25986-1037-b172-37c8-b70b00179501/trunk%401120

--- tests/tensor_impl_base.cpp
+++ tests/tensor_impl_base.cpp
@@ -1,15 +1,48 @@
 #include "TiledArray/tensor_impl_base.h"
+#include "TiledArray/tensor.h"
+#include "range_fixture.h"
 #include "unit_test_config.h"
 
-struct TensorImplBaseFixture {
+using namespace TiledArray;
 
-}; // struct TensorImplBaseFixture
+struct StaticTensorImplBaseFixture : public TiledRangeFixture {
+  typedef StaticTiledRange<GlobalFixture::coordinate_system> trange_type;
+  typedef expressions::Tensor<int, trange_type::tile_range_type> value_type;
+  typedef DynamicTiledRange dynamic_trange_type;
+  typedef detail::TensorImplBase<trange_type, value_type> tensor_impl_base;
 
-BOOST_FIXTURE_TEST_SUITE( tensor_impl_base_suite , TensorImplBaseFixture )
+  StaticTensorImplBaseFixture() : impl(* GlobalFixture::world, tr) { }
+
+  tensor_impl_base impl;
+}; // struct StaticTensorImplBaseFixture
+
+BOOST_FIXTURE_TEST_SUITE( static_tensor_impl_base_suite , StaticTensorImplBaseFixture )
 
 BOOST_AUTO_TEST_CASE( constructor )
 {
 }
 
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+struct DynamicTensorImplBaseFixture : public TiledRangeFixture {
+  typedef DynamicTiledRange trange_type;
+  typedef expressions::Tensor<int, trange_type::tile_range_type> value_type;
+  typedef detail::TensorImplBase<trange_type, value_type> tensor_impl_base;
+
+  DynamicTensorImplBaseFixture() : impl(* GlobalFixture::world, tr) { }
+
+  tensor_impl_base impl;
+}; // struct DynamicTensorImplBaseFixture
+
+BOOST_FIXTURE_TEST_SUITE( dynamic_tensor_impl_base_suite , DynamicTensorImplBaseFixture )
+
+BOOST_AUTO_TEST_CASE( constructor )
+{
+  BOOST_REQUIRE_NO_THROW(tensor_impl_base x(* GlobalFixture::world, DynamicTiledRange(tr)));
+  BOOST_REQUIRE_NO_THROW(tensor_impl_base x(* GlobalFixture::world, tr));
+
+}
 
 BOOST_AUTO_TEST_SUITE_END()
