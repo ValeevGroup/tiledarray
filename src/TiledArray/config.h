ce3ea72683f06d207b083cd3a3bf7b606e84209a
justusc <justusc@vt.edu>
2013-02-05 09:45:46 -0500

    Updated default config.h

--- src/TiledArray/config.h
+++ src/TiledArray/config.h
@@ -4,9 +4,4 @@
 /* Defines the default error checking behavior. none = 0, throw = 1, assert = 2 */
 #define TA_DEFAULT_ERROR 1
 
-/* TiledArray will use blas for some math operations. */
-/* #undef TA_USE_CBLAS */
-/* Maximum number of allowed dimensions. */
-#define TA_MAX_DIM 10
-
 #endif // TILEDARRAY_CONFIGURE_H__INCULDED
