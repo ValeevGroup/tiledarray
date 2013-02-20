45b263938ae1bcf115fc4172e4f37485cca3b422
justusc <justusc@vt.edu>
2013-02-20 16:18:23 -0500

    Changed the dgemm tests so that each test is in a seperate program.
    --HG--
    rename : examples/ta_dgemm/Makefile.am => examples/dgemm/Makefile.am
    rename : examples/ta_dgemm/README => examples/dgemm/README
    rename : examples/ta_dgemm/dgemm.cpp => examples/dgemm/ta_dense.cpp

---
+++ examples/dgemm/ta_band.cpp
@@ -0,0 +1,123 @@
+/*
+ * This file is a part of TiledArray.
+ * Copyright (C) 2013  Virginia Tech
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <iostream>
+#include <tiled_array.h>
+
+int main(int argc, char** argv) {
+  madness::initialize(argc,argv);
+  madness::World world(SafeMPI::COMM_WORLD);
+
+  // Get command line arguments
+  if(argc < 2) {
+    std::cout << "Usage: ta_band matrix_size block_size band_width [repetitions]\n";
+    return 0;
+  }
+  const long matrix_size = atol(argv[1]);
+  const long block_size = atol(argv[2]);
+  const long band_width = atol(argv[3]);
+  if(matrix_size <= 0) {
+    std::cerr << "Error: matrix size must greater than zero.\n";
+    return 1;
+  }
+  if(block_size <= 0) {
+    std::cerr << "Error: block size must greater than zero.\n";
+    return 1;
+  }
+  if((matrix_size % block_size) != 0ul) {
+    std::cerr << "Error: matrix size must be evenly divisible by block size.\n";
+    return 1;
+  }
+  if(band_width < 0) {
+    std::cerr << "Error: Band width must be greater than zero.\n";
+    return 1;
+  }
+  if(band_width > (matrix_size / 2)) {
+    std::cerr << "Error: Band width must be less than half the matrix size.\n";
+    return 1;
+  }
+  const long repeat = (argc >= 4 ? atol(argv[4]) : 5);
+  if(repeat <= 0) {
+    std::cerr << "Error: number of repititions must greater than zero.\n";
+    return 1;
+  }
+
+  const std::size_t num_blocks = matrix_size / block_size;
+
+  if(world.rank() == 0)
+    std::cout << "Number of nodes   = " << world.size()
+              << "\nMatrix size       = " << matrix_size << "x" << matrix_size
+              << "\nBlock size        = " << block_size << "x" << block_size
+              << "\nMemory per matrix = " << double(matrix_size * matrix_size * sizeof(double)) / 1.0e9
+              << " GB\nNumber of blocks  = " << num_blocks * num_blocks << "\n";
+
+  // Construct TiledRange
+  std::vector<unsigned int> blocking;
+  blocking.reserve(num_blocks + 1);
+  for(std::size_t i = 0; i <= matrix_size; i += block_size)
+    blocking.push_back(i);
+
+  std::vector<TiledArray::TiledRange1> blocking2(2,
+      TiledArray::TiledRange1(blocking.begin(), blocking.end()));
+
+  TiledArray::TiledRange
+    trange(blocking2.begin(), blocking2.end());
+
+  // Construct shape
+  TiledArray::detail::Bitset<> shape(trange.tiles().volume());
+  for(long i = 0; i < matrix_size; ++i) {
+    long j = std::max(i - band_width + 1, 0);
+    const long j_end = std::min(i + band_width - 1, matrix_size);
+    long ij = i * matrix_size + j;
+    for(; j < j_end; ++j, ++ij)
+      shape.set(ij);
+  }
+
+  // Construct and initialize arrays
+  TiledArray::Array<double, 2> a(world, trange, shape);
+  TiledArray::Array<double, 2> b(world, trange, shape);
+  TiledArray::Array<double, 2> c(world, trange);
+  a.set_all_local(1.0);
+  b.set_all_local(1.0);
+
+  // Start clock
+  world.gop.fence();
+  const double wall_time_start = madness::wall_time();
+  const double cpu_time_start = madness::cpu_time();
+
+  // Do matrix multiplcation
+  for(int i = 0; i < repeat; ++i) {
+    c("m,n") = a("m,k") * b("k,n");
+    world.gop.fence();
+  }
+
+  // Stop clock
+  const double wall_time_stop = madness::wall_time();
+  const double cpu_time_stop = madness::cpu_time();
+
+  if(world.rank() == 0)
+    std::cout << "Average wall time = " << (wall_time_stop - wall_time_start) / double(repeat)
+        << "\nAverage cpu time = " << 0.2 * (cpu_time_stop - cpu_time_start) / double(repeat)
+        << "\nAverage GFLOPS = " << double(repeat) * 2.0 * double(matrix_size *
+            matrix_size * matrix_size) / (wall_time_stop - wall_time_start) / 1.0e9 << "\n";
+
+
+  madness::finalize();
+  return 0;
+}
