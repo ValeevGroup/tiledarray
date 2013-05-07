c1e6e2ac99279c450199f034a1f5339b6fa6d327
justusc <justusc@vt.edu>
2013-05-07 15:59:37 -0400

    Implemented test cases.

--- tests/tile_op_add.cpp
+++ tests/tile_op_add.cpp
@@ -15,10 +15,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  justus
- *  Department of Chemistry
- *  Virginia Tech
- *  Blacksburg, VA 24061
+ *  Justus Calvin
+ *  Department of Chemistry, Virginia Tech
  *
  *  tile_op_add.cpp
  *  May 7, 2013
@@ -30,19 +28,29 @@
 #include "TiledArray/tensor.h"
 #include "range_fixture.h"
 
+using namespace TiledArray;
+
 struct AddFixture : public RangeFixture {
 
   AddFixture() :
-    a(RangeFixture::r, 5),
-    b(RangeFixture::r, 7),
-    c(RangeFixture::r)
-  { }
+    a(RangeFixture::r),
+    b(RangeFixture::r),
+    c(),
+    perm(2,0,1)
+  {
+    GlobalFixture::world->srand(27);
+    for(std::size_t i = 0ul; i < r.volume(); ++i) {
+      a[i] = GlobalFixture::world->rand() / 101;
+      b[i] = GlobalFixture::world->rand() / 101;
+    }
+  }
 
   ~AddFixture() { }
 
   Tensor<int> a;
   Tensor<int> b;
   Tensor<int> c;
+  Permutation perm;
 
 }; // AddFixture
 
@@ -50,7 +58,365 @@ BOOST_FIXTURE_TEST_SUITE( tile_op_add_suite, AddFixture )
 
 BOOST_AUTO_TEST_CASE( constructor )
 {
+  // Check that the constructors can be called without throwing exceptions
+  BOOST_CHECK_NO_THROW((math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false>()));
+  BOOST_CHECK_NO_THROW((math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false>(perm)));
+  BOOST_CHECK_NO_THROW((math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false>()));
+  BOOST_CHECK_NO_THROW((math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false>(perm)));
+  BOOST_CHECK_NO_THROW((math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true>()));
+  BOOST_CHECK_NO_THROW((math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true>(perm)));
+}
 
+BOOST_AUTO_TEST_CASE( binary_add )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false> add_op;
+
+  // Store the sum of a and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], a[i] + b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_left_zero )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false> add_op;
+
+  // Store the sum of 0 and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(ZeroTensor<int>(), b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_right_zero )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false> add_op;
+
+  // Store the sum of a and 0 in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, ZeroTensor<int>()));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], a[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_perm )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false> add_op(perm);
+
+  // Store the sum of a and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ a.range().idx(i)], a[i] + b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_left_zero_perm )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false> add_op(perm);
+
+  // Store the sum of 0 and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(ZeroTensor<int>(), b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), b.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ b.range().idx(i)], b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_right_zero_perm )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, false> add_op(perm);
+
+  // Store the sum of a and 0 in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, ZeroTensor<int>()));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ a.range().idx(i)], a[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_consume_left )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false> add_op;
+  const Tensor<int> ax(a.range(), a.begin());
+
+  // Store the sum of a and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_EQUAL(c.data(), a.data());
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], ax[i] + b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_left_zero_consume_left )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false> add_op;
+
+  // Store the sum of 0 and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(ZeroTensor<int>(), b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_right_zero_consume_left )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false> add_op;
+  const Tensor<int> ax(a.range(), a.begin());
+
+  // Store the sum of a and 0 in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, ZeroTensor<int>()));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_EQUAL(c.data(), a.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], ax[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_perm_consume_left )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false> add_op(perm);
+
+  // Store the sum of a and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ a.range().idx(i)], a[i] + b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_left_zero_perm_consume_left )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false> add_op(perm);
+
+  // Store the sum of 0 and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(ZeroTensor<int>(), b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), b.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ b.range().idx(i)], b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_right_zero_perm_consume_left )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, true, false> add_op(perm);
+
+  // Store the sum of a and 0 in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, ZeroTensor<int>()));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ a.range().idx(i)], a[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_consume_right )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true> add_op;
+  const Tensor<int> bx(b.range(), b.begin());
+
+  // Store the sum of a and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+  BOOST_CHECK_EQUAL(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], a[i] + bx[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_left_zero_consume_right )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true> add_op;
+  const Tensor<int> bx(b.range(), b.begin());
+
+  // Store the sum of 0 and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(ZeroTensor<int>(), b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_EQUAL(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], bx[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_right_zero_consume_right )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true> add_op;
+
+  // Store the sum of a and 0 in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, ZeroTensor<int>()));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[i], a[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_perm_consume_right )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true> add_op(perm);
+
+  // Store the sum of a and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ a.range().idx(i)], a[i] + b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_left_zero_perm_consume_right )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true> add_op(perm);
+
+  // Store the sum of 0 and b in c
+  BOOST_CHECK_NO_THROW(c = add_op(ZeroTensor<int>(), b));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), b.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), b.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ b.range().idx(i)], b[i]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( binary_add_right_zero_perm_consume_right )
+{
+  math::Add<Tensor<int>, Tensor<int>, Tensor<int>, false, true> add_op(perm);
+
+  // Store the sum of a and 0 in c
+  BOOST_CHECK_NO_THROW(c = add_op(a, ZeroTensor<int>()));
+
+  // Check that the result range is correct
+  BOOST_CHECK_EQUAL(c.range(), a.range());
+
+  // Check that a nor b were consumed
+  BOOST_CHECK_NE(c.data(), a.data());
+
+  // Check that the data in the new tile is correct
+  for(std::size_t i = 0ul; i < r.volume(); ++i) {
+    BOOST_CHECK_EQUAL(c[perm ^ a.range().idx(i)], a[i]);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
