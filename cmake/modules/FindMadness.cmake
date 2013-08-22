af8082b0d6f031a5a904d1c3c25dbba686bf73f6
justusc <justusc@vt.edu>
2013-08-22 07:09:49 -0400

    Added user feedback and updated documentation.

--- cmake/modules/FindMadness.cmake
+++ cmake/modules/FindMadness.cmake
@@ -1,12 +1,13 @@
 # - Try to find MADNESS lib
 #
-# Once done this will define
+# This module will search for MADNESS components in MADNESS_DIR and the default
+# search path. This module will define:
 #
 #  MADNESS_FOUND                - System has MADNESS lib with correct version
 #  MADNESS_INCLUDE_DIR          - The MADNESS include directory
 #  MADNESS_INCLUDE_DIRS         - The MADNESS include directory
 #  MADNESS_LIBRARY              - The MADNESS library
-#  MADNESS_LIBRARIES            - The MADNESS library and its dependencies
+#  MADNESS_LIBRARIES            - The MADNESS libraries and their dependencies
 #  MADNESS_${COMPONENT}_FOUND   - System has the specified MADNESS COMPONENT
 #  MADNESS_${COMPONENT}_LIBRARY - The MADNESS COMPONENT library
 #
@@ -26,10 +27,19 @@ include(LibFindMacros)
 libfind_package(MADNESS LAPACK)
 
 # Find the include dir
+if (NOT MADNESS_FIND_QUIETLY)
+  message(STATUS "Looking for madness_config.h")
+endif()
 find_path(MADNESS_INCLUDE_DIR
     NAMES madness_config.h 
-    PATHS ${MADNESS_DIR} ${CMAKE_PREFIX_PATH}
-    NO_DEFAULT_PATH)
+    PATHS ${MADNESS_DIR})
+if (NOT MADNESS_FIND_QUIETLY)
+  if(MADNESS_PROCESS_INCLUDES)
+    message(STATUS "Looking for madness_config.h - found")
+  else()
+    message(STATUS "Looking for madness_config.h - not found")
+  endif()
+endif()
 
 # Remove duplicate libraries
 list(REMOVE_DUPLICATES MADNESS_FIND_COMPONENTS)
@@ -78,33 +88,40 @@ endif()
 foreach (COMPONENT ${MADNESS_FIND_COMPONENTS})
 
   # Find the component library.
+  if (NOT MADNESS_FIND_QUIETLY)
+    message(STATUS "Looking for ${COMPONENT}")
+  endif()
   set(MADNESS_${COMPONENT}_LIBRARY MADNESS_${COMPONENT}-NOTFOUND)
   find_library(MADNESS_${COMPONENT}_LIBRARY 
       NAMES ${COMPONENT}
       PATHS ${MADNESS_DIR})
-  #message(STATUS "Found ${COMPONENT}: ${MADNESS_${COMPONENT}_LIBRARY}")
   
-  # Set result variables
+  # Set result status variables
   if (MADNESS_${COMPONENT}_LIBRARY)
     set(MADNESS_${COMPONENT}_FOUND TRUE)
   else()
     set(MADNESS_${COMPONENT}_FOUND FALSE)
   endif()
+  
+  # Print the result of the search
+  if (NOT MADNESS_FIND_QUIETLY)
+    if(MADNESS_${COMPONENT}_FOUND)
+      message(STATUS "Looking for ${COMPONENT} - found")
+    else()
+      message(STATUS "Looking for ${COMPONENT} - not found")
+    endif()
+  endif()
 endforeach()
 
-# Add the found libraries to MADNESS_LIBRARY so that they are in the proper order
+# Add the found libraries to MADNESS_PROCESS_LIBS so that they are in the proper order
 foreach(COMPONENT MADmra MADtinyxml MADmuparser MADlinalg MADtensor MADmisc MADworld)
   if(MADNESS_${COMPONENT}_FOUND)
     list(APPEND MADNESS_LIBRARY ${MADNESS_${COMPONENT}_LIBRARY})
   endif()
 endforeach()
 
-#message(STATUS "MADNESS_LIBRARY: ${MADNESS_LIBRARY}")
-
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this lib depends on.
 set(MADNESS_PROCESS_INCLUDES MADNESS_INCLUDE_DIR)
 set(MADNESS_PROCESS_LIBS MADNESS_LIBRARY LAPACK_LIBRARIES)
 libfind_process(MADNESS)
-
-mark_as_advanced(MADNESS_INCLUDE_DIR MADNESS_LIBRARIES)
