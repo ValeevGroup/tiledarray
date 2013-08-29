e6bbf540160bbbd6cb34b196f9070cee173b0077
justusc <justusc@vt.edu>
2013-08-29 07:59:51 -0400

    Add a macro for adding missing dependencies in a list.

---
+++ cmake/modules/LibAddDep.cmake
@@ -0,0 +1,16 @@
+# Add library dependencies to a list if they are missing
+
+
+macro(lib_add_dep _lib_list _lib _deps)
+  
+  list(FIND _lib_list ${_lib} _lib_find)
+  if(NOT _lib_find EQUAL -1)
+    foreach(_dep ${deps})
+      list(FIND _lib_list ${_dep} _dep_find)
+      if(_dep_find EQUAL -1)
+        list(APPEND _lib_list ${_dep})
+      endif()
+    endforeach()
+  endif()
+
+endmacro()
