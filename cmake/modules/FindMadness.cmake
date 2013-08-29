35efc818e604bd5f59da673430820ab38e66494f
justusc <justusc@vt.edu>
2013-08-29 08:00:50 -0400

    The Madness module now behaves in a more CMake-like manner.

--- cmake/modules/FindMadness.cmake
+++ cmake/modules/FindMadness.cmake
@@ -1,16 +1,16 @@
 # - Try to find MADNESS lib
 #
-# This module will search for MADNESS components in MADNESS_DIR and the default
+# This module will search for MADNESS components in Madness_DIR and the default
 # search path. This module will define:
 #
-#  MADNESS_FOUND                - System has MADNESS lib with correct version
-#  MADNESS_INCLUDE_DIR          - The MADNESS include directory
-#  MADNESS_INCLUDE_DIRS         - The MADNESS include directory
-#  MADNESS_LIBRARY_DIR          - The MADNESS library directory
-#  MADNESS_LIBRARY              - The MADNESS library
-#  MADNESS_LIBRARIES            - The MADNESS libraries and their dependencies
-#  MADNESS_${COMPONENT}_FOUND   - System has the specified MADNESS COMPONENT
-#  MADNESS_${COMPONENT}_LIBRARY - The MADNESS COMPONENT library
+#  Madness_FOUND                - System has MADNESS lib with correct version
+#  Madness_INCLUDE_DIR          - The MADNESS include directory
+#  Madness_INCLUDE_DIRS         - The MADNESS include directory
+#  Madness_LIBRARY_DIR          - The MADNESS library directory
+#  Madness_LIBRARY              - The MADNESS library
+#  Madness_LIBRARIES            - The MADNESS libraries and their dependencies
+#  Madness_${COMPONENT}_FOUND   - System has the specified MADNESS COMPONENT
+#  Madness_${COMPONENT}_LIBRARY - The MADNESS COMPONENT library
 #
 #  Valid COMPONENTS are:
 #    MADmra 
@@ -25,22 +25,23 @@
 include(LibFindMacros)
 
 #Create a cache variable for the MADNESS install path
-set(MADNESS_DIR "" CACHE PATH "MADNESS install path")
+set(Madness_DIR "" CACHE PATH "MADNESS install path")
 
 # Dependencies
 libfind_package(MADNESS LAPACK)
+libfind_package(MADNESS MPI)
 
 # Find the MADNESS include dir
-if (NOT MADNESS_FIND_QUIETLY)
+if (NOT Madness_FIND_QUIETLY)
   message(STATUS "Looking for madness_config.h")
 endif()
-find_path(MADNESS_INCLUDE_DIR
+find_path(Madness_INCLUDE_DIR
     NAMES madness_config.h 
-    PATHS ${MADNESS_DIR}/include ${CMAKE_PREFIX_PATH}
+    PATHS ${Madness_DIR}/include ${CMAKE_PREFIX_PATH}
     DOC "MADNESS include path"
     NO_CMAKE_SYSTEM_PATH)
-if(NOT MADNESS_FIND_QUIETLY)
-  if(MADNESS_INCLUDE_DIR)
+if(NOT Madness_FIND_QUIETLY)
+  if(Madness_INCLUDE_DIR)
     message(STATUS "Looking for madness_config.h - found")
   else()
     message(STATUS "Looking for madness_config.h - not found")
@@ -50,101 +51,116 @@ endif()
 # Add any missing dependencies to the commponent list
 
 # Add missing dependencies for MADmra
-list(FIND MADNESS_FIND_COMPONENTS MADmra _lib_find)
+list(FIND Madness_FIND_COMPONENTS MADmra _lib_find)
 if(NOT _lib_find EQUAL -1)
   foreach(_libdep MADtinyxml MADmuparser MADlinalg)
-    list(FIND MADNESS_FIND_COMPONENTS ${_libdep} _lib_find)
+    list(FIND Madness_FIND_COMPONENTS ${_libdep} _lib_find)
     if(_lib_find EQUAL -1)
-      list(APPEND MADNESS_FIND_COMPONENTS ${_libdep})
+      list(APPEND Madness_FIND_COMPONENTS ${_libdep})
      endif()
   endforeach()
 endif()
 
 # Add missing dependencies for MADlinalg
-list(FIND MADNESS_FIND_COMPONENTS MADlinalg _lib_find)
+list(FIND Madness_FIND_COMPONENTS MADlinalg _lib_find)
 if(NOT _lib_find EQUAL -1)
-  list(FIND MADNESS_FIND_COMPONENTS MADtensor _lib_find)
+  list(FIND Madness_FIND_COMPONENTS MADtensor _lib_find)
   if(_lib_find EQUAL -1)
-    list(APPEND MADNESS_FIND_COMPONENTS MADtensor)
+    list(APPEND Madness_FIND_COMPONENTS MADtensor)
    endif()
 endif()
 
 # Add missing dependencies for MADtensor
-list(FIND MADNESS_FIND_COMPONENTS MADtensor _lib_find)
+list(FIND Madness_FIND_COMPONENTS MADtensor _lib_find)
 if(NOT _lib_find EQUAL -1)
-  list(FIND MADNESS_FIND_COMPONENTS MADmisc _lib_find)
+  list(FIND Madness_FIND_COMPONENTS MADmisc _lib_find)
   if(_lib_find EQUAL -1)
-    list(APPEND MADNESS_FIND_COMPONENTS MADmisc)
+    list(APPEND Madness_FIND_COMPONENTS MADmisc)
    endif()
 endif()
 
 # Add missing dependencies for MADmisc
-list(FIND MADNESS_FIND_COMPONENTS MADmisc _lib_find)
+list(FIND Madness_FIND_COMPONENTS MADmisc _lib_find)
 if(NOT _lib_find EQUAL -1)
-  list(FIND MADNESS_FIND_COMPONENTS MADworld _lib_find)
+  list(FIND Madness_FIND_COMPONENTS MADworld _lib_find)
   if(_lib_find EQUAL -1)
-    list(APPEND MADNESS_FIND_COMPONENTS MADworld)
+    list(APPEND Madness_FIND_COMPONENTS MADworld)
   endif()
 endif() 
 
 # Set the first library test path
-if(MADNESS_LIBRARY_DIR)
-  set(test_lib_dir ${MADNESS_LIBRARY_DIR})
-else(MADNESS_LIBRARY_DIR)
-  if(MADNESS_DIR)
-    set(test_lib_dir ${MADNESS_DIR}/lib)
+if(Madness_LIBRARY_DIR)
+  set(test_lib_dir ${Madness_LIBRARY_DIR})
+else(Madness_LIBRARY_DIR)
+  if(Madness_DIR)
+    set(test_lib_dir ${Madness_DIR}/lib)
   else()
     set(test_lib_dir "")
-  endif(MADNESS_DIR)
-endif(MADNESS_LIBRARY_DIR)
+  endif(Madness_DIR)
+endif(Madness_LIBRARY_DIR)
 
 # Finally the library itself
-foreach (COMPONENT ${MADNESS_FIND_COMPONENTS})
+foreach (COMPONENT ${Madness_FIND_COMPONENTS})
 
-  if(NOT DEFINED MADNESS_${COMPONENT}_FOUND)
+  if(NOT DEFINED Madness_${COMPONENT}_FOUND)
     # Find the component library.
-    if(NOT MADNESS_FIND_QUIETLY)
+    if(NOT Madness_FIND_QUIETLY)
       message(STATUS "Looking for ${COMPONENT}")
-    endif(NOT MADNESS_FIND_QUIETLY)
-#    set(MADNESS_${COMPONENT}_LIBRARY MADNESS_${COMPONENT}-NOTFOUND)
-    find_library(MADNESS_${COMPONENT}_LIBRARY 
+    endif(NOT Madness_FIND_QUIETLY)
+#    set(Madness_${COMPONENT}_LIBRARY Madness_${COMPONENT}-NOTFOUND)
+    find_library(Madness_${COMPONENT}_LIBRARY 
         NAMES ${COMPONENT}
         PATHS ${test_lib_dir} ${CMAKE_PREFIX_PATH}
         DOC "MADNESS ${COMPONENT} library"
         NO_CMAKE_SYSTEM_PATH)
 
     # Set result status variables
-    if(MADNESS_${COMPONENT}_LIBRARY)
-      if(NOT MADNESS_LIBRARY_DIR)
-        get_filename_component(MADNESS_LIBRARY_DIR ${MADNESS_${COMPONENT}_LIBRARY} PATH CACHE)
-      endif(NOT MADNESS_LIBRARY_DIR)
-      set(MADNESS_${COMPONENT}_FOUND TRUE)
-    else(MADNESS_${COMPONENT}_LIBRARY)
-      set(MADNESS_${COMPONENT}_FOUND FALSE)
-    endif(MADNESS_${COMPONENT}_LIBRARY)
+    if(Madness_${COMPONENT}_LIBRARY)
+      if(NOT Madness_LIBRARY_DIR)
+        get_filename_component(Madness_LIBRARY_DIR ${Madness_${COMPONENT}_LIBRARY} PATH CACHE)
+      endif(NOT Madness_LIBRARY_DIR)
+      set(Madness_${COMPONENT}_FOUND TRUE)
+    else(Madness_${COMPONENT}_LIBRARY)
+      set(Madness_${COMPONENT}_FOUND FALSE)
+    endif(Madness_${COMPONENT}_LIBRARY)
 
     # Print the result of the search
-    if(NOT MADNESS_FIND_QUIETLY)
-      if(MADNESS_${COMPONENT}_FOUND)
+    if(NOT Madness_FIND_QUIETLY)
+      if(Madness_${COMPONENT}_FOUND)
         message(STATUS "Looking for ${COMPONENT} - found")
-      else(MADNESS_${COMPONENT}_FOUND)
+      else(Madness_${COMPONENT}_FOUND)
         message(STATUS "Looking for ${COMPONENT} - not found")
-      endif(MADNESS_${COMPONENT}_FOUND)
-    endif(NOT MADNESS_FIND_QUIETLY)
-  endif(NOT DEFINED MADNESS_${COMPONENT}_FOUND)
+      endif(Madness_${COMPONENT}_FOUND)
+    endif(NOT Madness_FIND_QUIETLY)
+  endif(NOT DEFINED Madness_${COMPONENT}_FOUND)
 endforeach()
 
-# Add the found libraries to MADNESS_PROCESS_LIBS so that they are in the proper order
+# Add the found libraries to Madness_PROCESS_LIBS so that they are in the proper order
 foreach(COMPONENT MADmra MADtinyxml MADmuparser MADlinalg MADtensor MADmisc MADworld)
-  if(MADNESS_${COMPONENT}_FOUND)
-    list(APPEND MADNESS_LIBRARY ${MADNESS_${COMPONENT}_LIBRARY})
+  if(Madness_${COMPONENT}_FOUND)
+    list(APPEND Madness_LIBRARY ${Madness_${COMPONENT}_LIBRARY})
   endif()
 endforeach()
 
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this lib depends on.
-set(MADNESS_PROCESS_INCLUDES MADNESS_INCLUDE_DIR)
-set(MADNESS_PROCESS_LIBS MADNESS_LIBRARY LAPACK_LIBRARIES)
-libfind_process(MADNESS)
+foreach(lang _C_ _CXX_ _)
 
-mark_as_advanced(MADNESS_INCLUDE_DIR MADNESS_LIBRARY_DIR MADNESS_LIBRARY)
+  if(MPI${lang}FOUND)
+    set(Madness_PROCESS_INCLUDES Madness_INCLUDE_DIR MPI${lang}INCLUDE_PATH)
+    set(Madness_PROCESS_LIBS Madness_LIBRARY LAPACK_LIBRARIES MPI${lang}LIBRARIES)
+    
+    set(Madness_COMPILE_FLAGS "${MPI${lang}COMPILE_FLAGS}")
+    set(Madness_LINK_FLAGS "${LAPACK_LINKER_FLAGS} ${MPI${lang}LINK_FLAGS}")
+    break()
+  endif()
+
+endforeach()
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND NOT ${CMAKE_SYSTEM_VERSION} VERSION_LESS 11.0)
+  # Building on OS X 10.7 or later, so add "-Wl,-no_pie" linker flags.
+  set(Madness_LINK_FLAGS "${Madness_LINK_FLAGS} -Wl,-no_pie")
+endif()
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+libfind_process(Madness)
+
+mark_as_advanced(Madness_INCLUDE_DIR Madness_LIBRARY_DIR Madness_LIBRARY)
