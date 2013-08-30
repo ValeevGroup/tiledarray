2b1cc4674e1e6f9a5c17f5eed34cc6b96507f6ca
justusc <justusc@vt.edu>
2013-08-30 10:08:44 -0400

    convert_libs_to_compargs macro now converts to GCC style compile arguments.

--- cmake/modules/ConvertLibrariesListToCompilerArgs.cmake
+++ cmake/modules/ConvertLibrariesListToCompilerArgs.cmake
@@ -5,30 +5,65 @@
 #
 
 macro(convert_libs_to_compargs _args _libs )
- # transform library list into compiler args
- foreach (_lib ${_libs})
-  set(_libpath _libpath-NOTFOUND) # DON'T REMOVE THIS LINE
-  find_library(_libpath ${_lib})
-  #message(STATUS "_lib = ${_lib}, _libpath = ${_libpath}")
-  if (_libpath)
-    set(_library "${_libpath}")
-  else()
-    set(_library "${_lib}")
+  # transform library list into compiler args
+  set(_framework_paths)
+  set(_framework_names)
+  set(_lib_paths)
+  set(_lib_names)
+  foreach (_lib ${_libs})
+    # Extract the components of the full library path
+    get_filename_component(_path ${_lib} PATH)
+    get_filename_component(_ext ${_lib} EXT)
+    get_filename_component(_libname ${_lib} NAME_WE)
+
+    if(APPLE AND "${_ext}" STREQUAL ".framework")
+      list(APPEND _framework_paths ${_path})
+      list(APPEND _framework_names ${_libname})
+    else()
+      set(_name ${_libname})
+      foreach(_prefix ${CMAKE_FIND_LIBRARY_PREFIXES})
+        if(_libname MATCHES "^${_prefix}")
+          string(REGEX REPLACE "^${_prefix}(.*)$" "\\1" _name ${_libname})
+          break()
+        endif()
+      endforeach()
+
+      list(APPEND _lib_paths ${_path})
+      list(APPEND _lib_names ${_name})
+    endif()
+  endforeach()
+
+  if(APPLE)
+    # Add Framework paths to _args
+    list(LENGTH _framework_paths _num)
+    if(_num GREATER 1)
+      list(REMOVE_DUPLICATES _framework_paths)
+    endif()
+    foreach(_framework_path ${_framework_paths})
+      set(${_args} "${${_args}} -F${_framework_path}")
+    endforeach()
+
+    # Add Frameworks to _args
+    list(LENGTH _framework_names _num)
+    if(_num GREATER 1)
+      list(REMOVE_DUPLICATES _framework_names)
+    endif()
+    foreach(_framework_name ${_framework_names})
+      set(${_args} "${${_args}} -framework ${_framework_name}")
+    endforeach()
   endif()
-  #message(STATUS "_library = ${_library}")
-  
-  # Apple framework libs
-  if (APPLE)
-   get_filename_component(_ext ${_library} EXT)
-   if ("${_ext}" STREQUAL ".framework")
-    get_filename_component(_name ${_library} NAME_WE)
-    get_filename_component(_path ${_library} PATH)
-    #message(STATUS "${_library} = ${_name} ${_path} ${_ext}")
-    set(_library "-F ${_path} -framework ${_name}")
-   endif()
-   #message(STATUS "${_lib} => ${_library}")
-   set(${_args} "${${_args}} ${_library}")
-  endif() # APPLE only
-  
- endforeach()
+
+  # Add library paths to _args
+  list(LENGTH _lib_paths _num)
+  if(_num GREATER 1)
+    list(REMOVE_DUPLICATES _lib_paths)
+  endif()
+  foreach(_lib_path ${_lib_paths})
+    set(${_args} "${${_args}} -L${_lib_path}")
+  endforeach()
+
+  # Add libraries to _args
+  foreach(_lib_name ${_lib_names})
+    set(${_args} "${${_args}} -l${_lib_name}")
+  endforeach()
 endmacro()
