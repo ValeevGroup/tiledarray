fd14700b978b74237e143533c842b7987f1faa9f
justusc <justusc@vt.edu>
2013-09-03 14:02:58 -0400

    Once again back to the full path version.

--- cmake/modules/ConvertLibrariesListToCompilerArgs.cmake
+++ cmake/modules/ConvertLibrariesListToCompilerArgs.cmake
@@ -5,70 +5,25 @@
 
 macro(convert_libs_to_compargs _args _libs )
   # transform library list into compiler args
-  set(_framework_paths)
-  set(_framework_names)
-  set(_lib_paths)
-  set(_lib_names)
   foreach (_lib ${_libs})
-    # Extract the components of the full library path
-    get_filename_component(_path ${_lib} PATH)
     get_filename_component(_ext ${_lib} EXT)
     get_filename_component(_libname ${_lib} NAME_WE)
-
+    
     if(APPLE AND "${_ext}" STREQUAL ".framework")
-      # Store the framework path (if it is not the default system path) and name.
-      if(NOT ${_path} STREQUAL "/System/Library/Frameworks")
-        list(APPEND _framework_paths ${_path})
+
+      # Handle Apple Frameworks
+      get_filename_component(_path ${_lib} PATH)
+      if(${_path} STREQUAL "/System/Library/Frameworks")
+        set(MAD_LIBS "${${_args}} -F${_path} -framework ${_libname}")
+      else()
+        set(MAD_LIBS "${${_args}} -framework ${_libname}")
       endif()
-      list(APPEND _framework_names ${_libname})
+
     else()
-      # Get the base name of the library
-      set(_name ${_libname})
-      foreach(_prefix ${CMAKE_FIND_LIBRARY_PREFIXES})
-        if(_libname MATCHES "^${_prefix}")
-          # Strip the library prefix from the name.
-          string(REGEX REPLACE "^${_prefix}(.*)$" "\\1" _name ${_libname})
-          break()
-        endif()
-      endforeach()
-
-      # Store the library path and base name
-      list(APPEND _lib_paths ${_path})
-      list(APPEND _lib_names ${_name})
+      
+      # Handle the general case
+      set(MAD_LIBS "${${_args}} ${_lib}")
     endif()
-  endforeach()
 
-  if(APPLE)
-    # Add Framework paths to _args
-    list(LENGTH _framework_paths _num)
-    if(_num GREATER 1)
-      list(REMOVE_DUPLICATES _framework_paths)
-    endif()
-    foreach(_framework_path ${_framework_paths})
-      set(${_args} "${${_args}} -F${_framework_path}")
-    endforeach()
-
-    # Add Frameworks to _args
-    list(LENGTH _framework_names _num)
-    if(_num GREATER 1)
-      list(REMOVE_DUPLICATES _framework_names)
-    endif()
-    foreach(_framework_name ${_framework_names})
-      set(${_args} "${${_args}} -framework ${_framework_name}")
-    endforeach()
-  endif()
-
-  # Add library paths to _args
-  list(LENGTH _lib_paths _num)
-  if(_num GREATER 1)
-    list(REMOVE_DUPLICATES _lib_paths)
-  endif()
-  foreach(_lib_path ${_lib_paths})
-    set(${_args} "${${_args}} ${CMAKE_LIBRARY_PATH_FLAG}${_lib_path}")
-  endforeach()
-
-  # Add libraries to _args
-  foreach(_lib_name ${_lib_names})
-    set(${_args} "${${_args}} ${CMAKE_LINK_LIBRARY_FLAG}${_lib_name}")
   endforeach()
 endmacro()
