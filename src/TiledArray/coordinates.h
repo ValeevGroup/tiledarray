03a0da490984c624763be06c435465b96a18f52e
Eduard Valeyev <eduard@valeyev.net>
2013-12-02 09:31:15 -0500

    coordinates.h is obsolete. Moved print_array to utility.h

--- src/TiledArray/coordinates.h
+++ src/TiledArray/coordinates.h
@@ -462,20 +462,6 @@ namespace TiledArray {
     return result;
   }
 
-  namespace detail {
-    template <typename A>
-    void print_array(std::ostream& out, const A& a) {
-      std::size_t n = a.size();
-      out << "[";
-      for(std::size_t i = 0; i < n; ++i) {
-        out << a[i];
-        if (i != (n - 1))
-          out << ",";
-      }
-      out << "]";
-    }
-  } // namespace detail
-
   /// Append an ArrayCoordinate to an output stream.
 
   /// \tparam DIM the coordinate dimension
