2653672e5dfd473d0c1ce777d30d46a0656a5df6
justusc <justusc@vt.edu>
2014-01-24 17:46:22 -0500

    Modified the unit tests of SparseShape to be compatible with the new interface.

--- tests/sparse_shape.cpp
+++ tests/sparse_shape.cpp
@@ -54,8 +54,8 @@ struct SparseShapeFixture : public SparseShapeBaseFixture {
   typedef std::vector<std::size_t> vec_type;
 
   SparseShapeFixture() :
-    left(left_tensor, 1.0),
-    right(right_tensor, 1.0)
+    left(left_tensor, 10.1),
+    right(right_tensor, 10.1)
   { }
 
   ~SparseShapeFixture() { }
@@ -83,67 +83,38 @@ BOOST_AUTO_TEST_CASE( constructor )
 BOOST_AUTO_TEST_CASE( cont_sparse_sparse )
 {
   // Create a matrix with the expected output
+  const std::size_t m = left.data().range().size().front();
+  const std::size_t n = right.data().range().size().back();
+  const std::size_t k = left.data().size() / m;
   EigenMatrixXf test_result =
-      math::eigen_map(left.data().data(), 5, 25) * math::eigen_map(right.data().data(), 25, 5);
+      math::eigen_map(left.data().data(), m, k) * math::eigen_map(right.data().data(), k, n);
 
   // Evaluate the contraction of sparse shapes
-  ShapeCont<SparseShape<float>, SparseShape<float> > op;
-  SparseShape<float> result = op(Permutation(), 5, 5, 25, left, right,
-      Range(Range(vec_type(2, 0), vec_type(2, 5))));
+  math::GemmHelper gemm_helper(madness::cblas::NoTrans, madness::cblas::NoTrans,
+      2u, left.data().range().dim(), right.data().range().dim());
+  SparseShape<float> result = left.gemm(right, 1.0, gemm_helper);
 
   // Check that the result is correct
   std::array<std::size_t, 2> i = {{ 0, 0 }};
   for(i[0] = 0ul; i[0] < 5; ++i[0])
     for(i[1] = 0ul; i[1] < 5; ++i[1])
-      BOOST_CHECK_EQUAL(result.data()[i], test_result(i[0], i[1]));
+      BOOST_CHECK_EQUAL(result[i], test_result(i[0], i[1]));
 }
 
 BOOST_AUTO_TEST_CASE( cont_sparse_sparse_perm )
 {
   // Create a matrix with the expected output
+  const std::size_t m = left.data().range().size().front();
+  const std::size_t n = right.data().range().size().back();
+  const std::size_t k = left.data().size() / m;
   EigenMatrixXf test_result =
-      (math::eigen_map(left.data().data(), 5, 25) * math::eigen_map(right.data().data(), 25, 5)).transpose();
+      (math::eigen_map(left.data().data(), m, k) *
+          math::eigen_map(right.data().data(), k, n)).transpose();
 
   // Evaluate the contraction of sparse shapes
-  ShapeCont<SparseShape<float>, SparseShape<float> > op;
-  SparseShape<float> result = op(Permutation(1,0), 5, 5, 25, left, right,
-      Range(Range(vec_type(2, 0), vec_type(2, 5))));
-
-  // Check that the result is correct
-  std::array<std::size_t, 2> i = {{ 0, 0 }};
-  for(i[0] = 0ul; i[0] < 5; ++i[0])
-    for(i[1] = 0ul; i[1] < 5; ++i[1])
-      BOOST_CHECK_EQUAL(result.data()[i], test_result(i[0], i[1]));
-}
-
-BOOST_AUTO_TEST_CASE( scal_cont_sparse_sparse )
-{
-  // Create a matrix with the expected output
-  EigenMatrixXf test_result =
-      math::eigen_map(left.data().data(), 5, 25) * math::eigen_map(right.data().data(), 25, 5) * factor;
-
-  // Evaluate the contraction of sparse shapes
-  ShapeScalCont<SparseShape<float>, SparseShape<float> > op;
-  SparseShape<float> result = op(Permutation(), 5, 5, 25, left, right,
-      Range(Range(vec_type(2, 0), vec_type(2, 5))), factor);
-
-  // Check that the result is correct
-  std::array<std::size_t, 2> i = {{ 0, 0 }};
-  for(i[0] = 0ul; i[0] < 5; ++i[0])
-    for(i[1] = 0ul; i[1] < 5; ++i[1])
-      BOOST_CHECK_EQUAL(result.data()[i], test_result(i[0], i[1]));
-}
-
-BOOST_AUTO_TEST_CASE( scal_cont_sparse_sparse_perm )
-{
-  // Create a matrix with the expected output
-  EigenMatrixXf test_result =
-      (math::eigen_map(left.data().data(), 5, 25) * math::eigen_map(right.data().data(), 25, 5)).transpose() * factor;
-
-  // Evaluate the contraction of sparse shapes
-  ShapeScalCont<SparseShape<float>, SparseShape<float> > op;
-  SparseShape<float> result = op(Permutation(1,0), 5, 5, 25, left, right,
-      Range(Range(vec_type(2, 0), vec_type(2, 5))), factor);
+  math::GemmHelper gemm_helper(madness::cblas::NoTrans, madness::cblas::NoTrans,
+      2u, left.data().range().dim(), right.data().range().dim());
+  SparseShape<float> result = left.gemm(right, 1.0, gemm_helper).perm(Permutation(1,0));
 
   // Check that the result is correct
   std::array<std::size_t, 2> i = {{ 0, 0 }};
