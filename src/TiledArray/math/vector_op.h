57f9d6220549cd115331168f70e6f95e2d584edb
Eduard Valeyev <eduard@valeyev.net>
2014-04-12 15:12:37 -0600

    Added missing header

--- src/TiledArray/math/vector_op.h
+++ src/TiledArray/math/vector_op.h
@@ -45,6 +45,7 @@
 
 #include <TiledArray/math/math.h>
 #include <TiledArray/math/eigen.h>
+#include <world/enable_if.h>
 #include <stdint.h>
 
 // Add macro TILEDARRAY_FORCE_INLINE which does as the name implies.
