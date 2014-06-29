3b5370631d99815d7c65329b4c4b3d8bef62a4c6
justusc <justusc@vt.edu>
2014-06-29 11:38:00 -0400

    - Improved performance of transpose operations. - Implemented unit tests for each of the three transpose operations.

--- tests/math_transpose.cpp
+++ tests/math_transpose.cpp
@@ -37,7 +37,7 @@ struct TransposeFixture {
 
 BOOST_FIXTURE_TEST_SUITE( transpose_suite, TransposeFixture )
 
-BOOST_AUTO_TEST_CASE( constructor )
+BOOST_AUTO_TEST_CASE( copy )
 {
   const std::size_t m = 20;
   const std::size_t n = 25;
@@ -72,4 +72,81 @@ BOOST_AUTO_TEST_CASE( constructor )
   delete [] b;
 }
 
+BOOST_AUTO_TEST_CASE( unary )
+{
+  const std::size_t m = 20;
+  const std::size_t n = 25;
+  const std::size_t mn = m * n;
+
+  int* a = new int[mn];
+  int* b = new int[mn];
+
+  GlobalFixture::world->srand(1764);
+  for(std::size_t i = 0ul; i < mn; ++i)
+    a[i] = GlobalFixture::world->rand() % 42;
+
+  TiledArray::math::Scale<int> op(3);
+
+  for(std::size_t x = 1ul; x < m; ++x) {
+    for(std::size_t y = 1ul; y < n; ++y) {
+      std::fill_n(b, mn, 0);
+
+      TiledArray::math::uninitialized_unary_transpose(x, y, a, n, b, m, op);
+
+      for(std::size_t i = 0ul; i < m; ++i) {
+        for(std::size_t j = 0ul; j < n; ++j) {
+          if((i < x) && (j < y)) {
+            BOOST_CHECK_EQUAL(b[j * m + i], op(a[i * n + j]));
+          } else {
+            BOOST_CHECK_EQUAL(b[j * m + i], 0);
+          }
+        }
+      }
+    }
+  }
+
+  delete [] a;
+  delete [] b;
+}
+
+BOOST_AUTO_TEST_CASE( binary )
+{
+  const std::size_t m = 20;
+  const std::size_t n = 25;
+  const std::size_t mn = m * n;
+
+  int* a = new int[mn];
+  int* b = new int[mn];
+  int* c = new int[mn];
+
+  GlobalFixture::world->srand(1764);
+  for(std::size_t i = 0ul; i < mn; ++i)
+    a[i] = GlobalFixture::world->rand() % 42;
+  for(std::size_t i = 0ul; i < mn; ++i)
+    b[i] = GlobalFixture::world->rand() % 42;
+
+  TiledArray::math::Minus<int, int, int> op;
+
+  for(std::size_t x = 1ul; x < m; ++x) {
+    for(std::size_t y = 1ul; y < n; ++y) {
+      std::fill_n(c, mn, 0);
+
+      TiledArray::math::uninitialized_binary_transpose(x, y, a, b, n, c, m, op);
+
+      for(std::size_t i = 0ul; i < m; ++i) {
+        for(std::size_t j = 0ul; j < n; ++j) {
+          if((i < x) && (j < y)) {
+            BOOST_CHECK_EQUAL(c[j * m + i], op(a[i * n + j], b[i * n + j]));
+          } else {
+            BOOST_CHECK_EQUAL(c[j * m + i], 0);
+          }
+        }
+      }
+    }
+  }
+
+  delete [] a;
+  delete [] b;
+  delete [] c;
+}
 BOOST_AUTO_TEST_SUITE_END()
