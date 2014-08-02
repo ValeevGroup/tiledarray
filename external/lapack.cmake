6feb9573f97469df6f6c14645158236ac5dd3c47
justusc <justusc@vt.edu>
2014-08-02 08:17:52 -0400

    Added a LAPACK configure script to properly handle user provided libraries and flags.

---
+++ external/lapack.cmake
@@ -0,0 +1,64 @@
+# Find BLAS and LAPACK.
+include(CheckCFortranFunctionExists)
+include(CMakePushCheckState)
+
+if(LAPACK_LIBRARIES OR BLAS_LIBRARIES OR LAPACK_LINKER_FLAGS OR BLAS_LINKER_FLAGS)
+  # Here we varify that the we can link against LAPACK and BLAS based on the
+  # given library and linker flags. If BLAS_FOUND and/or LAPACK_FOUND are true,
+  # we assume that these values have been varified.
+
+  if(NOT BLAS_FOUND)
+    # Varify that we can link against BLAS
+
+    cmake_push_check_state()
+    
+    if(UNIX AND BLA_STATIC AND BLAS_LIBRARIES)
+      set(CMAKE_REQUIRED_LIBRARIES ${BLAS_LINKER_FLAGS} "-Wl,-start-group"
+          ${BLAS_LIBRARIES} "-Wl,-end-group" ${CMAKE_THREAD_LIBS_INIT}
+          ${CMAKE_REQUIRED_LIBRARIES})
+    else()
+      set(CMAKE_REQUIRED_LIBRARIES ${BLAS_LINKER_FLAGS} ${BLAS_LIBRARIES}
+          ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_REQUIRED_LIBRARIES})
+    endif()
+
+    check_c_fortran_function_exists(sgemm BLAS_FOUND)
+
+    if(BLAS_FOUND)
+      message(STATUS "A library with BLAS API found.")
+    else()
+      message(FATAL_ERROR "The user specified BLAS libraries do not support the BLAS API.\nRerun cmake with BLAS_LIBRARIES and/or BLAS_LINKER_FLAGS.")
+    endif()
+    
+    cmake_pop_check_state()
+  endif()
+  
+  if(NOT LAPACK_FOUND)
+    # Varify that we can link against LAPACK
+
+    cmake_push_check_state()
+
+    if(UNIX AND BLA_STATIC AND LAPACK_LIBRARIES)
+      set(CMAKE_REQUIRED_LIBRARIES ${LAPACK_LINKER_FLAGS} "-Wl,--start-group"
+          ${LAPACK_LIBRARIES} "-Wl,--end-group" ${CMAKE_THREAD_LIBS_INIT}
+          ${CMAKE_REQUIRED_LIBRARIES})
+    else()
+      set(CMAKE_REQUIRED_LIBRARIES ${LAPACK_LINKER_FLAGS} ${LAPACK_LIBRARIES} 
+          ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_REQUIRED_LIBRARIES})
+    endif()
+
+    check_c_fortran_function_exists(cheev LAPACK_FOUND)
+
+    if(LAPACK_FOUND)
+      message(STATUS "A library with LAPACK API found.")
+    else()
+      message(FATAL_ERROR "The user specified LAPACK libraries do not support the LAPACK API.\nRerun cmake with LAPACK_LIBRARIES and/or LAPACK_LINKER_FLAGS.")
+    endif()
+    
+    cmake_pop_check_state()
+    
+  endif()
+else()
+  # Try to find BLAS and LAPACK
+  find_package(LAPACK REQUIRED)
+endif()
+
