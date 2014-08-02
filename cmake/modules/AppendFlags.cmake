bb8e252b41c2ad8e76f04db6411c5e4e29eec71e
justusc <justusc@vt.edu>
2014-08-02 08:20:11 -0400

    Changed append_flags macro to use the CMake string strip command instead of regular expression replace.

--- cmake/modules/AppendFlags.cmake
+++ cmake/modules/AppendFlags.cmake
@@ -1,11 +1,7 @@
 macro(append_flags _flags _append_flag)
 
-  string(REGEX REPLACE "^[ ]+(.*)$" "\\1" _temp_flags "${_append_flag}")
-  string(REGEX REPLACE "^(.*)[ ]+$" "\\1" _temp_flags "${_temp_flags}")
-  
-  set(${_flags} "${${_flags}} ${_temp_flags}")
-
-  string(REGEX REPLACE "^[ ]+(.*)$" "\\1" ${_flags} "${${_flags}}")
-  string(REGEX REPLACE "^(.*)[ ]+$" "\\1" ${_flags} "${${_flags}}")
+  string(STRIP "${_append_flag}" _append_flag )
+  set(${_flags} "${${_flags}} ${_append_flag}")
+  string(STRIP "${${_flags}}" ${_flags})
 
 endmacro()
