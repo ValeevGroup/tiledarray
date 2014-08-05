8f22190b74ebbf54dce122805cc26e6143f1a406
justusc <justusc@vt.edu>
2014-08-04 23:02:34 -0400

    Updated the osx toolchain file. - Fixed the variable names used for BLAS and LAPACK linker flags. - Added C++11 compile flags.

--- cmake/toolchains/osx-mpi-accelerate.cmake
+++ cmake/toolchains/osx-mpi-accelerate.cmake
@@ -6,16 +6,16 @@ set(CMAKE_CXX_COMPILER clang++)
 set(MPI_C_COMPILER mpicc)
 set(MPI_CXX_COMPILER mpicxx)
 
-set(BLAS_FLAGS "-framework Accelerate")
-set(LAPACK_FLAGS "-framework Accelerate")
+# Set compile flags
+set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++")
+
+# Set BLAS/LAPACK flags
+set(BLAS_LINKER_FLAGS "-framework Accelerate")
+set(LAPACK_LINKER_FLAGS "-framework Accelerate")
 set(INTEGER4 TRUE)
 
 if(NOT ${CMAKE_SYSTEM_VERSION} VERSION_LESS 11.0)
   # Building on OS X 10.7 or later, so add "-Wl,-no_pie" linker flags.
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-no_pie")
   string(STRIP "${CMAKE_EXE_LINKER_FLAGS}" CMAKE_EXE_LINKER_FLAGS)
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-no_pie")
-  string(STRIP "${CMAKE_SHARED_LINKER_FLAGS}" CMAKE_SHARED_LINKER_FLAGS)
-  set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} -Wl,-no_pie")
-  string(STRIP "${CMAKE_STATIC_LINKER_FLAGS}" CMAKE_STATIC_LINKER_FLAGS)
 endif()
