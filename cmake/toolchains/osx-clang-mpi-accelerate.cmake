ac451c4c6d18a3453d5c44f46788dc2f57469830
justusc <justusc@vt.edu>
2014-08-05 12:24:37 -0400

    OS X clang toolchain now correctly sets the compile flags.

--- cmake/toolchains/osx-clang-mpi-accelerate.cmake
+++ cmake/toolchains/osx-clang-mpi-accelerate.cmake
@@ -7,15 +7,22 @@ set(MPI_C_COMPILER mpicc)
 set(MPI_CXX_COMPILER mpicxx)
 
 # Set compile flags
-set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++")
-
-# Set BLAS/LAPACK flags
-set(BLAS_LINKER_FLAGS "-framework Accelerate")
-set(LAPACK_LINKER_FLAGS "-framework Accelerate")
-set(INTEGER4 TRUE)
+if(CMAKE_CXX_FLAGS)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++" CACHE STRING "C++ compile flags")
+else()
+  set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++" CACHE STRING "C++ compile flags")
+endif()
 
 if(NOT ${CMAKE_SYSTEM_VERSION} VERSION_LESS 11.0)
   # Building on OS X 10.7 or later, so add "-Wl,-no_pie" linker flags.
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-no_pie")
-  string(STRIP "${CMAKE_EXE_LINKER_FLAGS}" CMAKE_EXE_LINKER_FLAGS)
+  if(CMAKE_EXE_LINKER_FLAGS)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-no_pie" CACHE STRING "Executable linker flags")
+  else()
+    set(CMAKE_EXE_LINKER_FLAGS "-Wl,-no_pie" CACHE STRING "Executable linker flags")
+  endif()
 endif()
+
+# Set BLAS/LAPACK flags
+set(BLAS_LINKER_FLAGS "-framework Accelerate" CACHE STRING "BLAS linker flags")
+set(LAPACK_LINKER_FLAGS "-framework Accelerate" CACHE STRING "LAPACK linker flags")
+set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
