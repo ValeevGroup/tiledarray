a0b93eddfe7a216c208a9a77248b504be41ec9ce
justusc <justusc@vt.edu>
2014-09-15 12:14:08 -0400

    ta_band and ta_sparse now chart exceptions

--- examples/dgemm/ta_band.cpp
+++ examples/dgemm/ta_band.cpp
@@ -21,116 +21,139 @@
 #include <tiledarray.h>
 
 int main(int argc, char** argv) {
-  // Initialize runtime
-  madness::World& world = madness::initialize(argc, argv);
+  int rc = 0;
 
-  // Get command line arguments
-  if(argc < 2) {
-    std::cout << "Usage: ta_band matrix_size block_size band_width [repetitions]\n";
-    return 0;
-  }
-  const long matrix_size = atol(argv[1]);
-  const long block_size = atol(argv[2]);
-  const long band_width = atol(argv[3]);
-  if(matrix_size <= 0) {
-    std::cerr << "Error: matrix size must greater than zero.\n";
-    return 1;
-  }
-  if(block_size <= 0) {
-    std::cerr << "Error: block size must greater than zero.\n";
-    return 1;
-  }
-  if((matrix_size % block_size) != 0ul) {
-    std::cerr << "Error: matrix size must be evenly divisible by block size.\n";
-    return 1;
-  }
-  if(band_width < 0) {
-    std::cerr << "Error: Band width must be greater than zero.\n";
-    return 1;
-  }
-  if(band_width > (matrix_size / 2)) {
-    std::cerr << "Error: Band width must be less than half the matrix size.\n";
-    return 1;
-  }
-  const long repeat = (argc >= 5 ? atol(argv[4]) : 5);
-  if(repeat <= 0) {
-    std::cerr << "Error: number of repetitions must greater than zero.\n";
-    return 1;
-  }
+  try {
 
-  const std::size_t num_blocks = matrix_size / block_size;
-  std::size_t block_count = 0;
-  for (int i = -band_width + 1; i < band_width; ++i) {
-    block_count += num_blocks - (2 * std::abs(i));
-  }
+    // Initialize runtime
+    madness::World& world = madness::initialize(argc, argv);
 
-  if(world.rank() == 0)
-    std::cout << "TiledArray: block-banded matrix multiply test...\n"
-              << "Number of nodes    = " << world.size()
-              << "\nMatrix size        = " << matrix_size << "x" << matrix_size
-              << "\nBlock size         = " << block_size << "x" << block_size
-              << "\nMemory per matrix  = " << double(block_count * block_size * block_size * sizeof(double)) / 1.0e9
-              << " GB\nNumber of blocks   = " << block_count
-              << "\nAverage blocks/node = " << block_count / world.size() << "\n";
+    // Get command line arguments
+    if(argc < 2) {
+      std::cout << "Usage: ta_band matrix_size block_size band_width [repetitions]\n";
+      return 0;
+    }
+    const long matrix_size = atol(argv[1]);
+    const long block_size = atol(argv[2]);
+    const long band_width = atol(argv[3]);
+    if(matrix_size <= 0) {
+      std::cerr << "Error: matrix size must greater than zero.\n";
+      return 1;
+    }
+    if(block_size <= 0) {
+      std::cerr << "Error: block size must greater than zero.\n";
+      return 1;
+    }
+    if((matrix_size % block_size) != 0ul) {
+      std::cerr << "Error: matrix size must be evenly divisible by block size.\n";
+      return 1;
+    }
+    if(band_width < 0) {
+      std::cerr << "Error: Band width must be greater than zero.\n";
+      return 1;
+    }
+    if(band_width > (matrix_size / 2)) {
+      std::cerr << "Error: Band width must be less than half the matrix size.\n";
+      return 1;
+    }
+    const long repeat = (argc >= 5 ? atol(argv[4]) : 5);
+    if(repeat <= 0) {
+      std::cerr << "Error: number of repetitions must greater than zero.\n";
+      return 1;
+    }
 
-  // Construct TiledRange
-  std::vector<unsigned int> blocking;
-  blocking.reserve(num_blocks + 1);
-  for(long i = 0l; i <= matrix_size; i += block_size)
-    blocking.push_back(i);
+    const long num_blocks = matrix_size / block_size;
+    std::size_t block_count = 0;
+    for (int i = -band_width + 1; i < band_width; ++i) {
+      block_count += num_blocks - (2 * std::abs(i));
+    }
 
-  std::vector<TiledArray::TiledRange1> blocking2(2,
-      TiledArray::TiledRange1(blocking.begin(), blocking.end()));
-
-  TiledArray::TiledRange
-    trange(blocking2.begin(), blocking2.end());
-
-  TiledArray::SparseShape<float>::threshold(0.5);
-
-  // Construct shape
-  TiledArray::Tensor<float> shape_tensor(trange.tiles(), 0.0f);
-  for(long i = 0l; i < num_blocks; ++i) {
-    long j = std::max<long>(i - band_width + 1, 0);
-    const long j_end = std::min<long>(i + band_width - 1, num_blocks);
-    long ij = i * num_blocks + j;
-    for(; j < j_end; ++j, ++ij)
-      shape_tensor[ij] = 1.0;
-  }
-
-  TiledArray::SparseShape<float> shape(shape_tensor, trange);
-
-  typedef TiledArray::Array<double, 2, TiledArray::Tensor<double>,
-      TiledArray::SparsePolicy > SpTArray2;
-
-  // Construct and initialize arrays
-  SpTArray2 a(world, trange, shape);
-  SpTArray2 b(world, trange, shape);
-  SpTArray2 c(world, trange);
-  a.set_all_local(1.0);
-  b.set_all_local(1.0);
-
-  // Start clock
-  world.gop.fence();
-  const double wall_time_start = madness::wall_time();
-
-  // Do matrix multiplication
-  for(int i = 0; i < repeat; ++i) {
-    c("m,n") = a("m,k") * b("k,n");
-    world.gop.fence();
     if(world.rank() == 0)
-      std::cout << "Iteration " << i + 1 << "\n";
+      std::cout << "TiledArray: block-banded matrix multiply test...\n"
+                << "Number of nodes    = " << world.size()
+                << "\nMatrix size        = " << matrix_size << "x" << matrix_size
+                << "\nBlock size         = " << block_size << "x" << block_size
+                << "\nMemory per matrix  = " << double(block_count * block_size * block_size * sizeof(double)) / 1.0e9
+                << " GB\nNumber of blocks   = " << block_count
+                << "\nAverage blocks/node = " << block_count / world.size() << "\n";
+
+    // Construct TiledRange
+    std::vector<unsigned int> blocking;
+    blocking.reserve(num_blocks + 1);
+    for(long i = 0l; i <= matrix_size; i += block_size)
+      blocking.push_back(i);
+
+    std::vector<TiledArray::TiledRange1> blocking2(2,
+        TiledArray::TiledRange1(blocking.begin(), blocking.end()));
+
+    TiledArray::TiledRange
+      trange(blocking2.begin(), blocking2.end());
+
+    TiledArray::SparseShape<float>::threshold(0.5);
+
+    // Construct shape
+    TiledArray::Tensor<float> shape_tensor(trange.tiles(), 0.0f);
+    for(long i = 0l; i < num_blocks; ++i) {
+      long j = std::max<long>(i - band_width + 1, 0);
+      const long j_end = std::min<long>(i + band_width - 1, num_blocks);
+      long ij = i * num_blocks + j;
+      for(; j < j_end; ++j, ++ij)
+        shape_tensor[ij] = 1.0;
+    }
+
+    TiledArray::SparseShape<float> shape(shape_tensor, trange);
+
+    typedef TiledArray::Array<double, 2, TiledArray::Tensor<double>,
+        TiledArray::SparsePolicy > SpTArray2;
+
+    // Construct and initialize arrays
+    SpTArray2 a(world, trange, shape);
+    SpTArray2 b(world, trange, shape);
+    SpTArray2 c(world, trange);
+    a.set_all_local(1.0);
+    b.set_all_local(1.0);
+
+    // Start clock
+    world.gop.fence();
+    const double wall_time_start = madness::wall_time();
+
+    // Do matrix multiplication
+    for(int i = 0; i < repeat; ++i) {
+      c("m,n") = a("m,k") * b("k,n");
+      world.gop.fence();
+      if(world.rank() == 0)
+        std::cout << "Iteration " << i + 1 << "\n";
+    }
+
+    // Stop clock
+    const double wall_time_stop = madness::wall_time();
+
+    // Print results
+    const long flop = 2.0 * c("m,n").sum().get();
+    if(world.rank() == 0) {
+      std::cout << "Average wall time = " << (wall_time_stop - wall_time_start) / double(repeat)
+          << "\nAverage GFLOPS = " << double(repeat) * double(flop) / (wall_time_stop - wall_time_start) / 1.0e9 << "\n";
+    }
+
+    madness::finalize();
+
+  } catch(TiledArray::Exception& e) {
+    std::cerr << "!!ERROR TiledArray: " << e.what() << "\n";
+    rc = 1;
+  } catch(madness::MadnessException& e) {
+    std::cerr << "!!ERROR MADNESS: " << e.what() << "\n";
+    rc = 1;
+  } catch(SafeMPI::Exception& e) {
+    std::cerr << "!!ERROR SafeMPI: " << e.what() << "\n";
+    rc = 1;
+  } catch(std::exception& e) {
+    std::cerr << "!!ERROR std: " << e.what() << "\n";
+    rc = 1;
+  } catch(...) {
+    std::cerr << "!!ERROR: unknown exception\n";
+    rc = 1;
   }
 
-  // Stop clock
-  const double wall_time_stop = madness::wall_time();
 
-  // Print results
-  const long flop = 2.0 * c("m,n").sum().get();
-  if(world.rank() == 0) {
-    std::cout << "Average wall time = " << (wall_time_stop - wall_time_start) / double(repeat)
-        << "\nAverage GFLOPS = " << double(repeat) * double(flop) / (wall_time_stop - wall_time_start) / 1.0e9 << "\n";
-  }
-
-  madness::finalize();
-  return 0;
+  return rc;
 }
--- examples/dgemm/ta_sparse.cpp
+++ examples/dgemm/ta_sparse.cpp
@@ -25,184 +25,206 @@
 
 
 int main(int argc, char** argv) {
-  // Initialize runtime
-  madness::World& world = madness::initialize(argc, argv);
+  int rc = 0;
 
-  // Get command line arguments
-  if(argc < 2) {
-    std::cout << "Usage: ta_sparse matrix_size block_size [repetitions]\n";
-    return 0;
-  }
-  const long matrix_size = atol(argv[1]);
-  const long block_size = atol(argv[2]);
-  if(matrix_size <= 0) {
-    std::cerr << "Error: matrix size must greater than zero.\n";
-    return 1;
-  }
-  if(block_size <= 0) {
-    std::cerr << "Error: block size must greater than zero.\n";
-    return 1;
-  }
-  if((matrix_size % block_size) != 0ul) {
-    std::cerr << "Error: matrix size must be evenly divisible by block size.\n";
-    return 1;
-  }
-  const long repeat = (argc >= 4 ? atol(argv[3]) : 4);
-  if(repeat <= 0) {
-    std::cerr << "Error: number of repetitions must greater than zero.\n";
-    return 1;
-  }
+  try {
+    // Initialize runtime
+    madness::World& world = madness::initialize(argc, argv);
 
-  // Print information about the test
-  const std::size_t num_blocks = matrix_size / block_size;
-  std::vector<std::vector<double> > gflops;
-  std::vector<std::vector<double> > times;
+    // Get command line arguments
+    if(argc < 2) {
+      std::cout << "Usage: ta_sparse matrix_size block_size [repetitions]\n";
+      return 0;
+    }
+    const long matrix_size = atol(argv[1]);
+    const long block_size = atol(argv[2]);
+    if(matrix_size <= 0) {
+      std::cerr << "Error: matrix size must greater than zero.\n";
+      return 1;
+    }
+    if(block_size <= 0) {
+      std::cerr << "Error: block size must greater than zero.\n";
+      return 1;
+    }
+    if((matrix_size % block_size) != 0ul) {
+      std::cerr << "Error: matrix size must be evenly divisible by block size.\n";
+      return 1;
+    }
+    const long repeat = (argc >= 4 ? atol(argv[3]) : 4);
+    if(repeat <= 0) {
+      std::cerr << "Error: number of repetitions must greater than zero.\n";
+      return 1;
+    }
 
-  std::cout << "TiledArray: block-sparse matrix multiply test...\n"
-            << "Number of nodes    = " << world.size()
-            << "\nMatrix size        = " << matrix_size << "x" << matrix_size
-            << "\nBlock size         = " << block_size << "x" << block_size;
+    // Print information about the test
+    const std::size_t num_blocks = matrix_size / block_size;
+    std::vector<std::vector<double> > gflops;
+    std::vector<std::vector<double> > times;
 
-  for(unsigned int left_sparsity = 10; left_sparsity <= 100; left_sparsity += 10){
-    std::vector<double> inner_gflops;
-    std::vector<double> inner_times;
-    for(unsigned int right_sparsity = 10; right_sparsity <= left_sparsity; right_sparsity += 10){
-      const long l_block_count = (double(left_sparsity) / 100.0) * double(num_blocks * num_blocks);
-      const long r_block_count = (double(right_sparsity) / 100.0) * double(num_blocks * num_blocks);
-      if(world.rank() == 0)
-                  std::cout << "\nMemory per left matrix  = " << double(l_block_count * block_size * block_size * sizeof(double)) / 1.0e9 << " GB"
-                  << "\nMemory per right matrix  = " << double(r_block_count * block_size * block_size * sizeof(double)) / 1.0e9 << " GB"
-                  << "\nNumber of left blocks   = " << l_block_count
-                  << " " << left_sparsity << " percent"
-                  << "\nNumber of right blocks   = " << r_block_count
-                  << " " << right_sparsity << " percent"
-                  << "\nAverage left blocks/node = " << double(l_block_count) / double(world.size())
-                  << "\nAverage right blocks/node = " << double(r_block_count) / double(world.size()) << "\n";
+    std::cout << "TiledArray: block-sparse matrix multiply test...\n"
+              << "Number of nodes    = " << world.size()
+              << "\nMatrix size        = " << matrix_size << "x" << matrix_size
+              << "\nBlock size         = " << block_size << "x" << block_size;
 
-      // Construct TiledRange
-      std::vector<unsigned int> blocking;
-      blocking.reserve(num_blocks + 1);
-      for(long i = 0l; i <= matrix_size; i += block_size)
-        blocking.push_back(i);
-
-      std::vector<TiledArray::TiledRange1> blocking2(2,
-          TiledArray::TiledRange1(blocking.begin(), blocking.end()));
-
-      TiledArray::TiledRange
-        trange(blocking2.begin(), blocking2.end());
-
-      // Construct shape
-      TiledArray::Tensor<float>
-          a_shape_tensor(trange.tiles(), 0.0),
-          b_shape_tensor(trange.tiles(), 0.0);
-      if(world.rank() == 0) {
-        world.srand(time(NULL));
-        const long l_process_block_count = l_block_count / world.size() +
-            (world.rank() < (l_block_count / world.size()) ? 1 : 0);
-        const long r_process_block_count = r_block_count / world.size() +
-            (world.rank() < (r_block_count / world.size()) ? 1 : 0);
-        for(long i = 0; i < l_process_block_count; ++i)
-          a_shape_tensor.data()[world.rand() % trange.tiles().volume()] = 1.0;
-
-        for(long i = 0; i < r_process_block_count; ++i)
-          b_shape_tensor.data()[world.rand() % trange.tiles().volume()] = 1.0;
-
-      }
-      TiledArray::SparseShape<float>
-          a_shape(world, a_shape_tensor, trange),
-          b_shape(world, b_shape_tensor, trange);
-
-
-      typedef TiledArray::Array<double, 2, TiledArray::Tensor<double>,
-          TiledArray::SparsePolicy > SpTArray2;
-
-      // Construct and initialize arrays
-      SpTArray2 a(world, trange, a_shape);
-      SpTArray2 b(world, trange, b_shape);
-      SpTArray2 c(world, trange);
-      a.set_all_local(1.0);
-      b.set_all_local(1.0);
-
-      // Start clock
-      world.gop.fence();
-      const double wall_time_start = madness::wall_time();
-
-      // Do matrix multiplication
-      for(int i = 0; i < repeat; ++i) {
-        c("m,n") = a("m,k") * b("k,n");
-        world.gop.fence();
+    for(unsigned int left_sparsity = 10; left_sparsity <= 100; left_sparsity += 10){
+      std::vector<double> inner_gflops;
+      std::vector<double> inner_times;
+      for(unsigned int right_sparsity = 10; right_sparsity <= left_sparsity; right_sparsity += 10){
+        const long l_block_count = (double(left_sparsity) / 100.0) * double(num_blocks * num_blocks);
+        const long r_block_count = (double(right_sparsity) / 100.0) * double(num_blocks * num_blocks);
         if(world.rank() == 0)
-          std::cout << "Iteration " << i + 1 << "\n";
-      }
+                    std::cout << "\nMemory per left matrix  = " << double(l_block_count * block_size * block_size * sizeof(double)) / 1.0e9 << " GB"
+                    << "\nMemory per right matrix  = " << double(r_block_count * block_size * block_size * sizeof(double)) / 1.0e9 << " GB"
+                    << "\nNumber of left blocks   = " << l_block_count
+                    << " " << left_sparsity << " percent"
+                    << "\nNumber of right blocks   = " << r_block_count
+                    << " " << right_sparsity << " percent"
+                    << "\nAverage left blocks/node = " << double(l_block_count) / double(world.size())
+                    << "\nAverage right blocks/node = " << double(r_block_count) / double(world.size()) << "\n";
 
-      // Stop clock
-      const double wall_time_stop = madness::wall_time();
-      const long flop = 2.0 * c("m,n").sum();
-      inner_gflops.push_back(double(repeat) * double(flop) / (wall_time_stop - wall_time_start) / 1.0e9);
-      inner_times.push_back((wall_time_stop - wall_time_start)/double(repeat));
+        // Construct TiledRange
+        std::vector<unsigned int> blocking;
+        blocking.reserve(num_blocks + 1);
+        for(long i = 0l; i <= matrix_size; i += block_size)
+          blocking.push_back(i);
 
-      // Print results
-      if(world.rank() == 0) {
-        std::cout << "Average wall time = " << (wall_time_stop - wall_time_start) / double(repeat)
-            << "\nAverage GFLOPS = " << double(repeat) * double(flop) / (wall_time_stop - wall_time_start) / 1.0e9 << "\n";
+        std::vector<TiledArray::TiledRange1> blocking2(2,
+            TiledArray::TiledRange1(blocking.begin(), blocking.end()));
+
+        TiledArray::TiledRange
+          trange(blocking2.begin(), blocking2.end());
+
+        // Construct shape
+        TiledArray::Tensor<float>
+            a_shape_tensor(trange.tiles(), 0.0),
+            b_shape_tensor(trange.tiles(), 0.0);
+        if(world.rank() == 0) {
+          world.srand(time(NULL));
+          const long l_process_block_count = l_block_count / world.size() +
+              (world.rank() < (l_block_count / world.size()) ? 1 : 0);
+          const long r_process_block_count = r_block_count / world.size() +
+              (world.rank() < (r_block_count / world.size()) ? 1 : 0);
+          for(long i = 0; i < l_process_block_count; ++i)
+            a_shape_tensor.data()[world.rand() % trange.tiles().volume()] = 1.0;
+
+          for(long i = 0; i < r_process_block_count; ++i)
+            b_shape_tensor.data()[world.rand() % trange.tiles().volume()] = 1.0;
+
+        }
+        TiledArray::SparseShape<float>
+            a_shape(world, a_shape_tensor, trange),
+            b_shape(world, b_shape_tensor, trange);
+
+
+        typedef TiledArray::Array<double, 2, TiledArray::Tensor<double>,
+            TiledArray::SparsePolicy > SpTArray2;
+
+        // Construct and initialize arrays
+        SpTArray2 a(world, trange, a_shape);
+        SpTArray2 b(world, trange, b_shape);
+        SpTArray2 c(world, trange);
+        a.set_all_local(1.0);
+        b.set_all_local(1.0);
+
+        // Start clock
+        world.gop.fence();
+        const double wall_time_start = madness::wall_time();
+
+        // Do matrix multiplication
+        for(int i = 0; i < repeat; ++i) {
+          c("m,n") = a("m,k") * b("k,n");
+          world.gop.fence();
+          if(world.rank() == 0)
+            std::cout << "Iteration " << i + 1 << "\n";
+        }
+
+        // Stop clock
+        const double wall_time_stop = madness::wall_time();
+        const long flop = 2.0 * c("m,n").sum();
+        inner_gflops.push_back(double(repeat) * double(flop) / (wall_time_stop - wall_time_start) / 1.0e9);
+        inner_times.push_back((wall_time_stop - wall_time_start)/double(repeat));
+
+        // Print results
+        if(world.rank() == 0) {
+          std::cout << "Average wall time = " << (wall_time_stop - wall_time_start) / double(repeat)
+              << "\nAverage GFLOPS = " << double(repeat) * double(flop) / (wall_time_stop - wall_time_start) / 1.0e9 << "\n";
+        }
       }
+      gflops.push_back(inner_gflops);
+      times.push_back(inner_times);
+      world.gop.fence();
     }
-    gflops.push_back(inner_gflops);
-    times.push_back(inner_times);
-    world.gop.fence();
-  }
-  if(world.rank() == 0){
-    for(unsigned int i = 0; i < gflops.size(); ++i){
-      if(i == 0){
-        std::cout << std::defaultfloat;
-        std::cout << "   ";
-        for(unsigned int j = 10; j <= 100; j+=10){
-          std::cout << "        " << j;
+    if(world.rank() == 0){
+      for(unsigned int i = 0; i < gflops.size(); ++i){
+        if(i == 0){
+          std::cout << std::defaultfloat;
+          std::cout << "   ";
+          for(unsigned int j = 10; j <= 100; j+=10){
+            std::cout << "        " << j;
+          }
+          std::cout << std::endl;
+        }
+        for(unsigned int j = 0; j < gflops[i].size(); ++j){
+          if(j == 0){
+            std::cout << std::defaultfloat;
+            int num = (i+1) * 10;
+            if(num < 100){
+              std::cout << num << " |";
+            } else { std::cout << num << "|"; }
+          }
+
+          std::cout << std::setprecision(3) << std::scientific;
+          std::cout << double(gflops[i][j]) << " ";
         }
         std::cout << std::endl;
       }
-      for(unsigned int j = 0; j < gflops[i].size(); ++j){
-        if(j == 0){
-          std::cout << std::defaultfloat;
-          int num = (i+1) * 10;
-          if(num < 100){
-            std::cout << num << " |";
-          } else { std::cout << num << "|"; }
-        }
-
-        std::cout << std::setprecision(3) << std::scientific;
-        std::cout << double(gflops[i][j]) << " ";
-      }
-      std::cout << std::endl;
     }
-  }
 
-  if(world.rank() == 0){
-    for(unsigned int i = 0; i < times.size(); ++i){
-      if(i == 0){
-        std::cout << std::defaultfloat;
-        std::cout << "   ";
-        for(unsigned int j = 10; j <= 100; j+=10){
-          std::cout << "        " << j;
+    if(world.rank() == 0){
+      for(unsigned int i = 0; i < times.size(); ++i){
+        if(i == 0){
+          std::cout << std::defaultfloat;
+          std::cout << "   ";
+          for(unsigned int j = 10; j <= 100; j+=10){
+            std::cout << "        " << j;
+          }
+          std::cout << std::endl;
+        }
+        for(unsigned int j = 0; j < times[i].size(); ++j){
+          if(j == 0){
+            std::cout << std::defaultfloat;
+            int num = (i+1) * 10;
+            if(num < 100){
+              std::cout << num << " |";
+            } else { std::cout << num << "|"; }
+          }
+
+          std::cout << std::setprecision(3) << std::scientific;
+          std::cout << double(times[i][j]) << " ";
         }
         std::cout << std::endl;
       }
-      for(unsigned int j = 0; j < times[i].size(); ++j){
-        if(j == 0){
-          std::cout << std::defaultfloat;
-          int num = (i+1) * 10;
-          if(num < 100){
-            std::cout << num << " |";
-          } else { std::cout << num << "|"; }
-        }
-
-        std::cout << std::setprecision(3) << std::scientific;
-        std::cout << double(times[i][j]) << " ";
-      }
-      std::cout << std::endl;
     }
+
+    madness::finalize();
+
+  } catch(TiledArray::Exception& e) {
+    std::cerr << "!!ERROR TiledArray: " << e.what() << "\n";
+    rc = 1;
+  } catch(madness::MadnessException& e) {
+    std::cerr << "!!ERROR MADNESS: " << e.what() << "\n";
+    rc = 1;
+  } catch(SafeMPI::Exception& e) {
+    std::cerr << "!!ERROR SafeMPI: " << e.what() << "\n";
+    rc = 1;
+  } catch(std::exception& e) {
+    std::cerr << "!!ERROR std: " << e.what() << "\n";
+    rc = 1;
+  } catch(...) {
+    std::cerr << "!!ERROR: unknown exception\n";
+    rc = 1;
   }
 
-  madness::finalize();
-  return 0;
+
+  return rc;
 }
