ValeevGroup/tiledarray

commit 8246434471e250a6dd6ebdf1953c9325ed1bb2cd

DistEval::wait() now prints some additional diagnostic information in the event of an exception (which occurs if there is a wait timeout).

src/TiledArray/dist_eval/dist_eval.h
@@ -165,8 +165,17 @@ namespace TiledArray {
       /// Wait for all tiles to be assigned
       void wait() const {
         if(task_count_ > 0) {
-          CounterProbe probe(set_counter_, task_count_);
-          TensorImpl_::get_world().await(probe);
+          try {
+            CounterProbe probe(set_counter_, task_count_);
+            TensorImpl_::get_world().await(probe);
+          } catch(...) {
+            std::stringstream ss;
+            ss << "!!TiledArray: Aborting due to exception.\n"
+               << "!!TiledArray: rank=" << TensorImpl_::get_world().rank()
+               << " id=" << TensorImpl_::id() << " " << set_counter_ << " of " task_count_ << " tiles set\n";
+            printf(ss.str().c_str());
+            throw;
+          }
         }
       }
 

commit 3bba966ac9d72ca259546aa565b49fccee331f5d

Fixed a compile error introduced in commit 8246434471e250a6dd6ebdf1953c9325ed1bb2cd.

src/TiledArray/dist_eval/dist_eval.h
@@ -172,7 +172,7 @@ namespace TiledArray {
             std::stringstream ss;
             ss << "!!TiledArray: Aborting due to exception.\n"
                << "!!TiledArray: rank=" << TensorImpl_::get_world().rank()
-               << " id=" << TensorImpl_::id() << " " << set_counter_ << " of " task_count_ << " tiles set\n";
+               << " id=" << TensorImpl_::id() << " " << set_counter_ << " of " << task_count_ << " tiles set\n";
             printf(ss.str().c_str());
             throw;
           }