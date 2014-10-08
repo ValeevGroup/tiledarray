ValeevGroup/tiledarray

commit fe7cb819d187c411a1c5c09784899598baebddfb

TileReference and TileConstReference now use C++ references in the type conversion operators.

src/TiledArray/tensor_impl.h
@@ -77,9 +77,9 @@ namespace TiledArray {
         return future().get();
       }
 
-      operator typename Impl::future() const { return future(); }
+      operator typename Impl::future&() const { return future(); }
 
-      operator typename Impl::value_type() const { return get(); }
+      operator typename Impl::value_type&() const { return get(); }
     }; // class TileReference
 
     /// Tensor tile reference
@@ -121,9 +121,9 @@ namespace TiledArray {
         return future().get();
       }
 
-      operator typename Impl::future() const { return future(); }
+      operator typename Impl::future&() const { return future(); }
 
-      operator typename Impl::value_type() const { return get(); }
+      operator typename Impl::value_type&() const { return get(); }
     }; // class TileConstReference
 
   } // namespace detail

commit c0d81a976428cbb1b297a76719848d33e18df7f7

Fixed compile errors introduced in commit fe7cb819d187c411a1c5c09784899598baebddfb.

src/TiledArray/tensor_impl.h
@@ -66,18 +66,17 @@ namespace TiledArray {
         return *this;
       }
 
-
       typename Impl::future future() const {
         TA_ASSERT(tensor_);
         return tensor_->get(index_);
       }
 
-      const typename Impl::value_type get() const {
+      typename Impl::value_type& get() const {
         TA_ASSERT(tensor_);
         return future().get();
       }
 
-      operator typename Impl::future&() const { return future(); }
+      operator typename Impl::future() const { return tensor_->get(index_); }
 
       operator typename Impl::value_type&() const { return get(); }
     }; // class TileReference
@@ -116,14 +115,14 @@ namespace TiledArray {
         return tensor_->get(index_);
       }
 
-      const typename Impl::value_type get() const {
+      const typename Impl::value_type& get() const {
         TA_ASSERT(tensor_);
         return future().get();
       }
 
-      operator typename Impl::future&() const { return future(); }
+      operator typename Impl::future() const { return tensor_->get(index_); }
 
-      operator typename Impl::value_type&() const { return get(); }
+      operator const typename Impl::value_type&() const { return get(); }
     }; // class TileConstReference
 
   } // namespace detail