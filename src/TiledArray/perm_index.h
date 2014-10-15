85803750319cd889381b3fa42f592a961e31116a
justusc <justusc@vt.edu>
2014-10-15 14:28:55 -0400

    Added a permute index functor and accompanying unit test.

---
+++ src/TiledArray/perm_index.h
@@ -0,0 +1,169 @@
+/*
+ *  This file is a part of TiledArray.
+ *  Copyright (C) 2014  Virginia Tech
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Justus Calvin
+ *  Department of Chemistry, Virginia Tech
+ *
+ *  perm_index.h
+ *  Oct 10, 2014
+ *
+ */
+
+#ifndef TILEDARRAY_PERM_INDEX_H__INCLUDED
+#define TILEDARRAY_PERM_INDEX_H__INCLUDED
+
+#include <TiledArray/madness.h>
+#include <TiledArray/range.h>
+
+namespace TiledArray {
+  namespace detail {
+
+
+    /// A functor that permutes ordinal indices
+    class PermIndex {
+      std::size_t* weights_;
+      int ndim_;
+
+    public:
+
+      /// Default constructor
+      PermIndex() : weights_(NULL), ndim_(0) { }
+
+
+      PermIndex(const Range& range, const Permutation& perm) :
+        weights_(NULL), ndim_(perm.dim())
+      {
+        if(ndim_ > 0) {
+          // Check the input data
+          TA_ASSERT(range.dim() == perm.dim());
+
+          // Construct the inverse permutation
+          const Permutation inv_perm_ = -perm;
+
+          // Allocate memory for this object
+          weights_ = static_cast<std::size_t*>(malloc((ndim_ + ndim_) * sizeof(std::size_t)));
+          if(! weights_)
+            throw std::bad_alloc();
+
+          // Construct restricted pointers to the input data
+          const Permutation::index_type* restrict const inv_perm = & inv_perm_.data().front();
+          const Range::size_type* restrict const range_size = range.size().data();
+          const Range::size_type* restrict const range_weight = range.weight().data();
+
+          // Construct restricted pointers to the object data
+          std::size_t* restrict const input_weight = weights_;
+          std::size_t* restrict const output_weight = weights_ + ndim_;
+
+          // Initialize input and output weights
+          std::size_t volume = 1;
+          for(int i = ndim_ - 1; i >= 0; --i) {
+            // Load input data for iteration i.
+            const Permutation::index_type inv_perm_i = inv_perm[i];
+            const Range::size_type weight = range_weight[i];
+            const Range::size_type size = range_size[inv_perm_i];
+
+            // Store the input and output weights
+            output_weight[inv_perm_i] = volume;
+            volume *= size;
+            input_weight[i] = weight;
+          }
+        }
+      }
+
+      PermIndex(const PermIndex& other) :
+        weights_(NULL), ndim_(other.ndim_)
+      {
+        if(ndim_) {
+          // Allocate memory for this object
+          weights_ = static_cast<std::size_t*>(malloc((ndim_ + ndim_) * sizeof(std::size_t)));
+          if(! weights_)
+            throw std::bad_alloc();
+
+          // Copy data
+          memcpy(weights_, other.weights_, (ndim_ + ndim_) * sizeof(std::size_t));
+        }
+      }
+
+      ~PermIndex() {
+        free(weights_);
+        weights_ = NULL;
+      }
+
+      PermIndex& operator=(const PermIndex& other) {
+        // Deallocate memory
+        if(ndim_ && (ndim_ != other.ndim_)) {
+          free(weights_);
+          weights_ = NULL;
+        }
+
+        const std::size_t bytes = (other.ndim_ + other.ndim_) * sizeof(std::size_t);
+
+        if(! weights_ && bytes) {
+          // Allocate new memory
+          weights_ = static_cast<std::size_t*>(malloc(bytes));
+          if(! weights_)
+            throw std::bad_alloc();
+        }
+
+        // copy the data (safe if ndim_ == 0)
+        ndim_ = other.ndim_;
+        memcpy(weights_, other.weights_, bytes);
+
+        return *this;
+      }
+
+      /// Dimension accessor
+
+      /// \return The dimension of the indices that can be permuted
+      int dim() const { return ndim_; }
+
+      /// Data accessor
+
+      /// \return A pointer to the result data
+      const std::size_t* data() const { return weights_; }
+
+      /// Compute the permuted index for the current block
+      std::size_t operator()(std::size_t index) const {
+        TA_ASSERT(ndim_);
+        TA_ASSERT(weights_);
+
+        // Construct restricted pointers to data
+        const std::size_t* restrict const input_weight = weights_;
+        const std::size_t* restrict const output_weight = weights_ + ndim_;
+
+        // create result index
+        std::size_t perm_index = 0ul;
+
+        for(unsigned int i = 0u; i < ndim_; ++i) {
+          const std::size_t input_weight_i = input_weight[i];
+          const std::size_t output_weight_i = output_weight[i];
+          perm_index += index / input_weight_i * output_weight_i;
+          index %= input_weight_i;
+        }
+
+        return perm_index;
+      }
+
+      // Check for valid permutation
+      operator bool() const { return ndim_; }
+    }; // class PermIndex
+
+  }  // namespace detail
+} // namespace TiledArray
+
+#endif // MADNESS_PERM_INDEX_H__INCLUDED
+TILEDARRAY_PERM_INDEX_H__INCLUDED
---
+++ tests/perm_index.cpp
@@ -0,0 +1,159 @@
+/*
+ *  This file is a part of TiledArray.
+ *  Copyright (C) 2014  Virginia Tech
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  justus
+ *  Department of Chemistry, Virginia Tech
+ *
+ *  perm_index.cpp
+ *  Oct 10, 2014
+ *
+ */
+
+#include "TiledArray/perm_index.h"
+#include "unit_test_config.h"
+
+using namespace TiledArray;
+using namespace TiledArray::detail;
+
+struct PermIndexFixture {
+
+  PermIndexFixture() : perm(1,2,0,3), range(start, finish), perm_range(perm ^ range) { }
+
+  ~PermIndexFixture() { }
+
+  static const std::array<std::size_t, 4> start;
+  static const std::array<std::size_t, 4> finish;
+
+  Permutation perm;
+  Range range;
+  Range perm_range;
+
+}; // PermIndexFixture
+
+
+const std::array<std::size_t, 4> PermIndexFixture::start = {{0ul, 0ul, 0ul, 0ul}};
+const std::array<std::size_t, 4> PermIndexFixture::finish = {{3ul, 5ul, 7ul, 11ul}};
+
+BOOST_FIXTURE_TEST_SUITE( perm_index_suite, PermIndexFixture )
+
+BOOST_AUTO_TEST_CASE( default_constructor ) {
+  BOOST_CHECK_NO_THROW( PermIndex x; );
+  PermIndex x;
+
+  // Check that the data has not bee initialized
+  BOOST_CHECK(! bool(x));
+  BOOST_CHECK(! x.data());
+
+
+#ifdef TA_EXCEPTION_ERROR
+  // Check that an exception is thrown when using a default constructed object
+  BOOST_CHECK_THROW(x(0), TiledArray::Exception);
+#endif // TA_EXCEPTION_ERROR
+}
+
+BOOST_AUTO_TEST_CASE( constructor ) {
+
+  BOOST_CHECK_NO_THROW( PermIndex x(range, perm); );
+  PermIndex x(range, perm);
+
+  // Check for valid permutation object
+  BOOST_CHECK(bool(x));
+  BOOST_CHECK(x.data());
+
+  // Check for valid permutation data
+
+  BOOST_CHECK_EQUAL(x.dim(), 4);
+
+  const std::size_t* const input_weight_begin = x.data();
+  const std::size_t* const input_weight_end = x.data() + x.dim();
+  BOOST_CHECK_EQUAL_COLLECTIONS(input_weight_begin, input_weight_end, range.weight().begin(), range.weight().end());
+
+  const std::size_t* const output_weight_begin = input_weight_end;
+  const std::size_t* const output_weight_end = input_weight_end + x.dim();
+  const std::vector<Range::size_type> inv_result_weight = -perm ^ perm_range.weight();
+  BOOST_CHECK_EQUAL_COLLECTIONS(output_weight_begin, output_weight_end, inv_result_weight.begin(), inv_result_weight.end());
+}
+
+BOOST_AUTO_TEST_CASE( assignment_operator ) {
+  PermIndex x;
+
+  // Verify initial state of x
+  BOOST_CHECK(! bool(x));
+  BOOST_CHECK(! x.data());
+
+  // Assign x
+  BOOST_CHECK_NO_THROW( x = PermIndex(range, perm); );
+
+  // Check for valid permutation object
+  BOOST_CHECK(bool(x));
+  BOOST_CHECK(x.data());
+
+  // Check for valid permutation data
+
+  BOOST_CHECK_EQUAL(x.dim(), 4);
+
+  const std::size_t* const input_weight_begin = x.data();
+  const std::size_t* const input_weight_end = x.data() + x.dim();
+  BOOST_CHECK_EQUAL_COLLECTIONS(input_weight_begin, input_weight_end, range.weight().begin(), range.weight().end());
+
+  const std::size_t* const output_weight_begin = input_weight_end;
+  const std::size_t* const output_weight_end = input_weight_end + x.dim();
+  const std::vector<Range::size_type> inv_result_weight = -perm ^ perm_range.weight();
+  BOOST_CHECK_EQUAL_COLLECTIONS(output_weight_begin, output_weight_end, inv_result_weight.begin(), inv_result_weight.end());
+}
+
+
+BOOST_AUTO_TEST_CASE( permute_constructor_tensor ) {
+  std::array<unsigned int, 4> p = {{0,1,2,3}};
+
+  while(std::next_permutation(p.begin(), p.end())) {
+    // set new permutation permutation
+    perm = Permutation(p.begin(), p.end());
+    perm_range = perm ^ range;
+
+    // Construct the permute index object
+    BOOST_CHECK_NO_THROW( PermIndex perm_index(range, perm); );
+    PermIndex perm_index(range, perm);
+
+    // Check for valid permutation object
+    BOOST_CHECK(bool(perm_index));
+    BOOST_CHECK(perm_index.data());
+
+    // Check for valid permutation data
+
+    BOOST_CHECK_EQUAL(perm_index.dim(), 4);
+
+    const std::size_t* const input_weight_begin = perm_index.data();
+    const std::size_t* const input_weight_end = perm_index.data() + perm_index.dim();
+    BOOST_CHECK_EQUAL_COLLECTIONS(input_weight_begin, input_weight_end, range.weight().begin(), range.weight().end());
+
+    const std::size_t* const output_weight_begin = input_weight_end;
+    const std::size_t* const output_weight_end = input_weight_end + perm_index.dim();
+    const std::vector<Range::size_type> inv_result_weight = -perm ^ perm_range.weight();
+    BOOST_CHECK_EQUAL_COLLECTIONS(output_weight_begin, output_weight_end, inv_result_weight.begin(), inv_result_weight.end());
+
+
+    for(std::size_t i = 0ul; i < range.volume(); ++i) {
+      std::size_t pi = 0;
+      BOOST_CHECK_NO_THROW(pi = perm_index(i));
+
+      BOOST_CHECK_EQUAL(pi, perm_range.ord(perm ^ range.idx(i)));
+    }
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
