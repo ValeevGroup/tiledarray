f8660edcf80aa93d515ffcb9e4983b9ce8f3d806
drewlewis <drew90@vt.edu>
2014-10-27 15:37:28 -0400

    Removed -march=native from the vesta-gcc-toolchain file since I had issues compiling with it on.

--- cmake/toolchains/vesta-gcc-essl.cmake
+++ cmake/toolchains/vesta-gcc-essl.cmake
@@ -26,13 +26,13 @@ set(MPI_C_COMPILER mpicc)
 # Set compile flags
 set(CMAKE_C_FLAGS_INIT             "-std=c99" CACHE STRING "Inital C compile flags")
 set(CMAKE_C_FLAGS_DEBUG            "-g -Wall" CACHE STRING "Inital C debug compile flags")
-set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -march=native -DNDEBUG" CACHE STRING "Inital C minimum size release compile flags")
-set(CMAKE_C_FLAGS_RELEASE          "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C release compile flags")
+set(CMAKE_C_FLAGS_MINSIZEREL       "-Os  -DNDEBUG" CACHE STRING "Inital C minimum size release compile flags")
+set(CMAKE_C_FLAGS_RELEASE          "-O3  -DNDEBUG" CACHE STRING "Inital C release compile flags")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -Wall" CACHE STRING "Inital C release with debug info compile flags")
 set(CMAKE_CXX_FLAGS_INIT           "-std=c++11" CACHE STRING "Inital C++ compile flags")
 set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall" CACHE STRING "Inital C++ debug compile flags")
-set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -march=native -DNDEBUG" CACHE STRING "Inital C++ minimum size release compile flags")
-set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C++ release compile flags")
+set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os  -DNDEBUG" CACHE STRING "Inital C++ minimum size release compile flags")
+set(CMAKE_CXX_FLAGS_RELEASE        "-O3  -DNDEBUG" CACHE STRING "Inital C++ release compile flags")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall" CACHE STRING "Inital C++ release with debug info compile flags")
 
 
