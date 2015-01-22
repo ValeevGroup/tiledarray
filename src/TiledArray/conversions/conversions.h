001fe10b4e67cce722fc0a28972fc691bde87823
drewlewis <drew90@vt.edu>
2015-01-22 18:46:24 -0500

    Added conversion functions from sparse array to dense array and vice versa.  The code likely needs reviewing for TiledArray coding standards, but I will worry about that later after a pull request.

---
+++ src/TiledArray/conversions/conversions.h
@@ -0,0 +1,9 @@
+#pragma once 
+#ifndef TILEDARRAY_CONVERSIONS_H__INCLUDED
+#define TILEDARRAY_CONVERSIONS_H__INCLUDED
+
+#include <TiledArray/conversions/sparse_to_dense.h>
+#include <TiledArray/conversions/dense_to_sparse.h>
+
+
+#endif /* end of include guard: TILEDARRAY_CONVERSIONS_H__INCLUDED */
