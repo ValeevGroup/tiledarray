2e238205f8aebe5cafe9aa5e741fbaabfa958aec
drewlewis <drew90@vt.edu>
2015-02-02 14:57:46 -0500

    Finished conversion from an array of one tile type to an array of a different tile type.

--- src/TiledArray/conversions/to_new_tile_type.h
+++ src/TiledArray/conversions/to_new_tile_type.h
@@ -6,36 +6,6 @@
 
 namespace TiledArray {
 namespace conversion {
-namespace detail {
-
-template <typename NewTile>
-class array_creator {
-   public:
-    template <typename T, unsigned int DIM, typename Tile, typename Policy>
-    Array<T, DIM, NewTile, Policy> create_from_prototype(
-        Array<T, DIM, Tile, Policy> const &proto) const;
-};
-
-template <typename NewTile>
-template <typename T, unsigned int DIM, typename Tile>
-Array<T, DIM, NewTile, SparsePolicy>
-array_creator<NewTile>::create_from_prototype<T, DIM, Tile, SparsePolicy>(
-    Array<T, DIM, Tile, SparsePolicy> const &proto) const {
-    return Array<T, DIM, NewTile, SparsePolicy>{
-        proto.get_world(), proto.trange(), proto.get_shape().clone()};
-}
-
-template <typename NewTile>
-template <typename T, unsigned int DIM, typename Tile>
-Array<T, DIM, NewTile, DensePolicy>
-array_creator<NewTile>::create_from_prototype<T, DIM, Tile, DensePolicy>(
-    Array<T, DIM, Tile, DensePolicy> const &proto) const {
-    return Array<T, DIM, NewTile, DensePolicy>{proto.get_world(),
-                                               proto.trange()};
-}
-
-}  // namespace detail
-}  // namespace conversion
 
 /// Function to convert an array to a new array with a different tile type.
 
@@ -43,12 +13,12 @@ template <
     typename T, unsigned int DIM, typename Tile, typename Policy, typename Fn,
     typename std::enable_if<!std::is_same<Tile, typename Fn::TileType>::value,
                             Tile>::type * = nullptr>
-Array<T, DIM, Fn::TileType, Policy> to_new_tile_type(
+Array<T, DIM, typename std::result_of<Fn(Tile)>::type, Policy> to_new_tile_type(
     Array<T, DIM, Tile, Policy> const &old_array, Fn converting_function) {
-    using namespace conversion::detail;
-    using TileType = Fn::TileType;
+    using TileType = typename std::result_of<Fn(Tile)>::type;
 
-    auto new_array = array_creator<TileType>{}.create_from_prototype(old_array);
+    auto new_array = Array<T, DIM, TileType, Policy>{
+        old_array.get_world(), old_array.trange(), old_array.get_shape()};
 
     const auto end = old_array.end();
     for (auto it = old_array.begin(); it != end; ++it) {
@@ -61,5 +31,6 @@ Array<T, DIM, Fn::TileType, Policy> to_new_tile_type(
     return new_array;
 }
 
+}  // namespace conversion
 }  // namespace TiledArray
 #endif /* end of include guard: TILEDARRAY_TONEWTILETYPE_H__INCLUDED */
