8ae5eab61bdb190dc5ec36329c0b957a53b088f9
Drew Lewis <drew90@vt.edu>
2015-02-09 06:58:28 -0500

    Added toolchain files for both clang and gcc with mkl on fedora.

---
+++ cmake/toolchains/fedora-clang++-mkl.cmake
@@ -0,0 +1,25 @@
+# Set compilers (assumes the compilers are in the PATH)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+set(MPI_C_COMPILER mpicc)
+set(MPI_CXX_COMPILER mpicxx)
+
+set(MKLROOT "/opt/intel/mkl")
+
+# Set compile flags
+set(CMAKE_C_FLAGS_INIT             "-std=c99" CACHE STRING "Inital C compile flags")
+set(CMAKE_C_FLAGS_DEBUG            "-g -Wall" CACHE STRING "Inital C debug compile flags")
+set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -march=native -DNDEBUG" CACHE STRING "Inital C minimum size release compile flags")
+set(CMAKE_C_FLAGS_RELEASE          "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C release compile flags")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -Wall" CACHE STRING "Inital C release with debug info compile flags")
+set(CMAKE_CXX_FLAGS_INIT           "-m64 -I${MKLROOT}/include -std=c++11" CACHE STRING "Inital C++ compile flags")
+set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall" CACHE STRING "Inital C++ debug compile flags")
+set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -march=native -DNDEBUG" CACHE STRING "Inital C++ minimum size release compile flags")
+set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C++ release compile flags")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall" CACHE STRING "Inital C++ release with debug info compile flags")
+
+
+set(BLAS_LIBRARIES "-Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_core.a ${MKLROOT}/lib/intel64/libmkl_sequential.a -Wl,--end-group -lpthread -lm -ldl" CACHE STRING "BLAS linker flags")
+set(LAPACK_LIBRARIES "${BLAS_LIBRARIES};-ldl" CACHE STRING "LAPACK linker flags")
+set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
+
