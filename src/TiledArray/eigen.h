ValeevGroup/tiledarray

commit 87d8fee2d6590d71b5a1b4d89656ffc4f68bdf1f

Fixed the argument type for eigen_map function.

src/TiledArray/eigen.h
@@ -138,7 +138,7 @@ namespace TiledArray {
   /// \throw When \c tensor dimensions are not equal to 2 or 1.
   template <typename T, typename A>
   inline Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>, Eigen::AutoAlign>
-  eigen_map(Tensor<T, A>& tensor) {
+  eigen_map(const Tensor<T, A>& tensor) {
     TA_ASSERT((tensor.range().dim() == 2u) || (tensor.range().dim() == 1u));
 
     return eigen_map(tensor, tensor.range().size()[0],

commit d7ba7661bbc562659c2a5a81ff1d1da1ea396271

Fixed an error introduced in commit 87d8fee2d6590d71b5a1b4d89656ffc4f68bdf1f.

src/TiledArray/eigen.h
@@ -138,7 +138,7 @@ namespace TiledArray {
   /// \throw When \c tensor dimensions are not equal to 2 or 1.
   template <typename T, typename A>
   inline Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>, Eigen::AutoAlign>
-  eigen_map(const Tensor<T, A>& tensor) {
+  eigen_map(Tensor<T, A>& tensor) {
     TA_ASSERT((tensor.range().dim() == 2u) || (tensor.range().dim() == 1u));
 
     return eigen_map(tensor, tensor.range().size()[0],