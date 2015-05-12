1c2759453c1ccda5f6ff988f8cc16d9fe6f5fd6e
justusc <justusc@vt.edu>
2015-05-12 15:14:34 -0400

    Fixed an error in the size constructor of Range.

--- src/TiledArray/range.h
+++ src/TiledArray/range.h
@@ -216,7 +216,7 @@ namespace TiledArray {
         // Initialize array memory
         data_ = new size_type[n << 2];
         rank_ = n;
-        init_range_data({size});
+        init_range_data(size);
       }
     }
 
@@ -236,7 +236,7 @@ namespace TiledArray {
       // Initialize array memory
       data_ = new size_type[n << 2];
       rank_ = n;
-      compute_range_data({static_cast<size_type>(sizes)...});
+      init_range_data({static_cast<size_type>(sizes)...});
     }
 
     /// Copy Constructor
