9b860db9a7ba6d18f896713d6848dca63640ba3c
Drew Lewis <drew90@vt.edu>
2015-06-03 09:09:39 -0400

    Updating fedora toolchain to use TBB and to work with the yum installed mpich out of the box.

--- cmake/toolchains/fedora-g++-mkl.cmake
+++ cmake/toolchains/fedora-g++-mkl.cmake
@@ -1,8 +1,9 @@
 # Set compilers (assumes the compilers are in the PATH)
+set(MPICHROOT "/usr/lib64/mpich")
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
-set(MPI_C_COMPILER mpicc)
-set(MPI_CXX_COMPILER mpicxx)
+set(MPI_C_COMPILER ${MPICHROOT}/bin/mpicc)
+set(MPI_CXX_COMPILER ${MPICHROOT}/bin/mpicxx)
 
 set(MKLROOT "/opt/intel/mkl")
 
@@ -12,7 +13,7 @@ set(CMAKE_C_FLAGS_DEBUG            "-g -Wall" CACHE STRING "Inital C debug compi
 set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -march=native -DNDEBUG" CACHE STRING "Inital C minimum size release compile flags")
 set(CMAKE_C_FLAGS_RELEASE          "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C release compile flags")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -Wall" CACHE STRING "Inital C release with debug info compile flags")
-set(CMAKE_CXX_FLAGS_INIT           "-m64 -I${MKLROOT}/include -std=c++11" CACHE STRING "Inital C++ compile flags")
+set(CMAKE_CXX_FLAGS_INIT           "-m64 -I${MKLROOT}/include -I/opt/intel/tbb/include -std=c++11" CACHE STRING "Inital C++ compile flags")
 set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall" CACHE STRING "Inital C++ debug compile flags")
 set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -march=native -DNDEBUG" CACHE STRING "Inital C++ minimum size release compile flags")
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C++ release compile flags")
@@ -22,4 +23,6 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall" CACHE STRING "Inital C++ relea
 set(BLAS_LIBRARIES "-Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_core.a ${MKLROOT}/lib/intel64/libmkl_sequential.a -Wl,--end-group -lpthread -lm -ldl" CACHE STRING "BLAS linker flags")
 set(LAPACK_LIBRARIES "${BLAS_LIBRARIES};-ldl" CACHE STRING "LAPACK linker flags")
 set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
+set(MAD_EXTRA_CONF_FLAGS "--with-tbb=/opt/intel/tbb")
+set(TiledArray_LIBRARIES "{TiledArray_LIBRARIES} -ltbb")
 
