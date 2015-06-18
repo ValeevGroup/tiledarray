aee2b7392ab9a4129e7d438fb657640141d6ff74
justusc <justusc@vt.edu>
2015-06-18 08:03:51 -0400

    Updated the osx-intel-mpi-mkl.cmake tool chain file, for Intel Compiler v15.

--- cmake/toolchains/osx-intel-mpi-mkl.cmake
+++ cmake/toolchains/osx-intel-mpi-mkl.cmake
@@ -7,18 +7,18 @@ set(MPI_C_COMPILER mpicc)
 set(MPI_CXX_COMPILER mpicxx)
 
 # Set compile flags
-set(CMAKE_C_FLAGS_INIT             "-std=c99 -use-clang-env" CACHE STRING "Inital C compile flags")
+set(CMAKE_C_FLAGS_INIT             "-std=c99" CACHE STRING "Inital C compile flags")
 set(CMAKE_C_FLAGS_DEBUG            "-g -Wall -diag-disable remark,279,654,1125" CACHE STRING "Inital C debug compile flags")
 set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -march=native -DNDEBUG" CACHE STRING "Inital C minimum size release compile flags")
 set(CMAKE_C_FLAGS_RELEASE          "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C release compile flags")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -Wall -diag-disable remark,279,654,1125" CACHE STRING "Inital C release with debug info compile flags")
-set(CMAKE_CXX_FLAGS_INIT           "-std=c++11 -use-clang-env" CACHE STRING "Inital C++ compile flags")
+set(CMAKE_CXX_FLAGS_INIT           "-std=c++11" CACHE STRING "Inital C++ compile flags")
 set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall -diag-disable remark,279,654,1125" CACHE STRING "Inital C++ debug compile flags")
 set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -march=native -DNDEBUG" CACHE STRING "Inital C++ minimum size release compile flags")
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -march=native -DNDEBUG" CACHE STRING "Inital C++ release compile flags")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall -diag-disable remark,279,654,1125" CACHE STRING "Inital C++ release with debug info compile flags")
 
 # Set BLAS/LAPACK flags
-set(BLAS_LINKER_FLAGS "-mkl" CACHE STRING "BLAS linker flags")
+set(BLAS_LINKER_FLAGS "-L/opt/intel/mkl/lib -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lm" CACHE STRING "BLAS linker flags")
 set(LAPACK_LINKER_FLAGS "" CACHE STRING "LAPACK linker flags")
 set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
