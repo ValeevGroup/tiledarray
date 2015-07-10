d16dd5b292f57f35096b39116e35cc0402bb3cd3
justusc <justusc@vt.edu>
2015-07-10 07:30:01 -0400

    Added a new repository to install a more recent version of CMake (without building it from source).

--- bin/dep-linux.sh
+++ bin/dep-linux.sh
@@ -5,6 +5,7 @@ set -ev
 
 # Add repository for a newer version GCC
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:kalakris/cmake
 
 # Update package list
 sudo apt-get update -qq
@@ -21,12 +22,12 @@ $CC --version
 $CXX --version
 
 # Install CMAKE
-curl -O http://www.cmake.org/files/v2.8/cmake-2.8.8.tar.gz
-tar -xzf cmake-2.8.8.tar.gz
-cd ./cmake-2.8.8
-./configure --prefix=/usr/local
-make -j2
-sudo make install
+#curl -O http://www.cmake.org/files/v2.8/cmake-2.8.8.tar.gz
+#tar -xzf cmake-2.8.8.tar.gz
+#cd ./cmake-2.8.8
+#./configure --prefix=/usr/local
+#make -j2
+#sudo make install
 
 # Install packages
-sudo apt-get install -qq -y libblas-dev liblapack-dev mpich2 libtbb-dev libeigen3-dev libboost1.48-dev
+sudo apt-get install -qq -y cmake libblas-dev liblapack-dev mpich2 libtbb-dev libeigen3-dev libboost1.48-dev
