a54cb37a2b329e23d6916e6e30030074fd0a3f04
Eduard Valeyev <eduard@valeyev.net>
2015-09-06 13:53:02 -0400

    Introduced is_lexicographically_smallest + unit tests

--- tests/symm_permutation_group.cpp
+++ tests/symm_permutation_group.cpp
@@ -23,6 +23,9 @@
  *
  */
 
+#include <random>
+#include <chrono>
+
 #include "TiledArray/symm/permutation_group.h"
 #include "unit_test_config.h"
 
@@ -31,9 +34,12 @@ using TiledArray::Permutation;
 
 struct PermutationGroupFixture {
 
-  PermutationGroupFixture() {
+  PermutationGroupFixture() :
+    generator(std::chrono::system_clock::now().time_since_epoch().count()),
+    uniform_int_distribution(0,100)
+  {
 
-    { // construct set of generators for P4__01__23__02_13
+    { // construct set of generators for P4__01__23__02_13 = P4{(0,1),(2,3),(0,2)(1,3)}
       // this group describes symmetries under permutations 0<->1, 2<->3, and {0,1}<->{2,3}
       P4__01__23__02_13_generators.reserve(3);
       P4__01__23__02_13_generators.emplace_back(Permutation{1,0,2,3});
@@ -44,6 +50,18 @@ struct PermutationGroupFixture {
   }
 
   ~PermutationGroupFixture() { }
+
+  template <size_t N>
+  std::array<int, N> random_index() {
+    std::array<int, N> result;
+    for(auto& value : result)
+      value = uniform_int_distribution(generator);
+    return result;
+  }
+
+  // random number generation
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> uniform_int_distribution;
 
   // for testing symmetric group
   static const unsigned int max_degree = 5u;
@@ -108,32 +126,88 @@ struct PermutationGroupFixture {
 
 BOOST_FIXTURE_TEST_SUITE( symm_group_suite, PermutationGroupFixture )
 
-BOOST_AUTO_TEST_CASE( sn_constructor )
+BOOST_AUTO_TEST_CASE( constructor )
 {
 
-  unsigned int order = 1u;
-  for(unsigned int degree = 1u; degree <= max_degree; ++degree, order *= degree) {
-    BOOST_REQUIRE_NO_THROW(PermutationGroup S(degree));
-    PermutationGroup S(degree);
+  // symmetric group constructors
+  {
+    unsigned int order = 1u;
+    for(unsigned int degree = 1u; degree <= max_degree; ++degree, order *= degree) {
+      BOOST_REQUIRE_NO_THROW(PermutationGroup S(degree));
+      PermutationGroup S(degree);
 
-    // Check that the group has the correct degree
-    BOOST_CHECK_EQUAL(S.degree(), degree);
+      // Check that the group has the correct degree
+      BOOST_CHECK_EQUAL(S.degree(), degree);
+
+      // Check that the number of elements in the group is correct
+      BOOST_CHECK_EQUAL(S.order(), order);
+
+      validate_group(S);
+    }
+  }
+
+  // non-Sn permutation groups
+  {
+    PermutationGroup P4__01__23__02_13(4,P4__01__23__02_13_generators);
 
     // Check that the number of elements in the group is correct
-    BOOST_CHECK_EQUAL(S.order(), order);
+    BOOST_CHECK_EQUAL(P4__01__23__02_13.order(), 8u);
 
-    validate_group(S);
+    validate_group(P4__01__23__02_13);
   }
+
 }
 
-BOOST_AUTO_TEST_CASE( pn_constructor )
+BOOST_AUTO_TEST_CASE( lexicographical_order )
 {
-   PermutationGroup P4__01__23__02_13(4,P4__01__23__02_13_generators);
+  { // check S5
+    typedef std::array<int,5> index_type;
+    PermutationGroup S5(5);
 
-   // Check that the number of elements in the group is correct
-   BOOST_CHECK_EQUAL(P4__01__23__02_13.order(), 8u);
+//    std::cout<<"Permutation group S5:" << std::endl;
+//    std::copy(S5.begin(), S5.end(), std::ostream_iterator<Permutation>(std::cout, "\n"));
+//    std::cout<<std::endl;
 
-   validate_group(P4__01__23__02_13);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,3,4,5},S5), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,1,300,300,500},S5), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{300,300,300,300,5},S5), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,1,0,0,5},S5), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,3,4,0},S5), false);
+  }
+
+  { // check P5{(0,2,4)(1,3)}
+    typedef std::array<int,5> index_type;
+    PermutationGroup P(5, std::vector<Permutation>{Permutation{4,3,0,1,2}});
+
+//    std::cout<<"Permutation group P5{(0,2,4)(1,3)}:" << std::endl;
+//    std::copy(P.begin(), P.end(), std::ostream_iterator<Permutation>(std::cout, "\n"));
+//    std::cout<<std::endl;
+
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,3,4,5},P), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,3,1,4,1},P), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,1,1,1},P), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,1,2,0,3},P), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,2,3,1},P), false);
+  }
+
+  { // check P4{(0,1),(2,3),(0,2)(1,3)}
+    typedef std::array<int,4> index_type;
+    PermutationGroup P(4, P4__01__23__02_13_generators);
+
+//    std::cout<<"Permutation group P4{(0,1),(2,3),(0,2)(1,3)}:" << std::endl;
+//    std::copy(P.begin(), P.end(), std::ostream_iterator<Permutation>(std::cout, "\n"));
+//    std::cout<<std::endl;
+
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,3,4},P), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,3,2,4},P), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,2,4},P), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{2,3,2,3},P), true);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,2,3,2},P), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{2,1,3,4},P), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{1,3,1,2},P), false);
+    BOOST_CHECK_EQUAL(is_lexicographically_smallest(index_type{2,3,1,4},P), false);
+  }
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
