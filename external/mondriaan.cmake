a2c4eb97aaac2908cdd88a52584b2cc78d9719a7
Justus Calvin <justusc@vt.edu>
2015-09-13 11:56:15 -0400

    Fixed config script for Modriaan so that the header directory is included.

--- external/mondriaan.cmake
+++ external/mondriaan.cmake
@@ -3,6 +3,8 @@ if(ENABLE_MONDRIAAN)
   find_package(Mondriaan REQUIRED)
     
   # Set the  build variables
+  include_directories(${Mondriaan_INCLUDE_DIRS})
+  list(APPEND TiledArray_CONFIG_INCLUDE_DIRS ${Mondriaan_INCLUDE_DIRS})
   set(TiledArray_LIBRARIES ${Mondriaan_LIBRARIES} ${TiledArray_LIBRARIES})
   set(TiledArray_CONFIG_LIBRARIES ${Mondriaan_LIBRARIES} ${TiledArray_CONFIG_LIBRARIES})
   set(TILEDARRAY_HAS_MONDRIAAN 1)
