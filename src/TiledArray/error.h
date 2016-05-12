a9cde7b148d2de3475f01aeb4999369a18e5c9bf
Eduard Valeyev <eduard@valeyev.net>
2016-05-12 15:15:12 -0400

    added the "standard" DEPRECATED macro

--- src/TiledArray/error.h
+++ src/TiledArray/error.h
@@ -138,4 +138,15 @@ namespace TiledArray {
 
 #endif
 
+// mark functions as deprecated using this macro
+// will result in a warning
+#ifdef __GNUC__
+#define DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define DEPRECATED
+#endif
+
 #endif // TILEDARRAY_ERROR_H__INCLUDED
