97697c2342f1ebe68900ffd27fbc288c2be29107
Eduard Valeyev <eduard.valeyev@gmail.com>
2016-06-21 14:58:49 -0400

    travis-ci: upgraded to container-based infstr, "disabled" clang builds

--- bin/dep-linux.sh
+++ bin/dep-linux.sh
@@ -3,39 +3,46 @@
 # Exit on error
 set -ev
 
-# Add PPA for a newer version GCC
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-# Add PPA for newer cmake (3.2.3)
-sudo add-apt-repository ppa:george-edison55/precise-backports -y
-# Add repo with newer clang
-sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main'
-sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+# Install packages
 
-# Update package list
-sudo apt-get update -qq
-
-# Install updated gcc compilers
-sudo apt-get install -qq -y gcc-$GCC_VERSION g++-$GCC_VERSION
-if [ "$CXX" = "g++" ]; then
-    export CC=/usr/bin/gcc-$GCC_VERSION
-    export CXX=/usr/bin/g++-$GCC_VERSION
-fi
+case "$CXX" in
+    g++)
+        export CC=/usr/bin/gcc-$GCC_VERSION
+        export CXX=/usr/bin/g++-$GCC_VERSION
+        ;;
+    clang++)
+        export CC=/usr/bin/clang-3.7
+        export CXX=/usr/bin/clang++-3.7
+        export CXXFLAGS="-std=c++11"
+        ;;
+    *)
+        echo "Unknown C++ compiler:"
+        echo "$CXX"
+        exit 1
+        ;;
+esac
 
 # Print compiler information
 $CC --version
 $CXX --version
 
-# Install very recent CMAKE
-#curl -O https://cmake.org/files/v3.4/cmake-3.4.1.tar.gz
-#tar -xzf cmake-3.4.1.tar.gz
-#cd ./cmake-3.4.1
-#./configure --prefix=/usr/local
-#make -j2
-#sudo make install
-
-# Install CMake 3
-sudo apt-get -y -qq --no-install-suggests --no-install-recommends --force-yes install cmake cmake-data
+# log the CMake version (need 3+)
 cmake --version
 
-# Install the rest
-sudo apt-get install -qq -y libblas-dev liblapack-dev mpich2 libtbb-dev libeigen3-dev libboost1.48-dev clang-3.7
+# Install MPICH
+if [ ! -d "${HOME}/mpich" ]; then
+    wget --no-check-certificate -q http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+    tar -xzf mpich-3.2.tar.gz
+    cd mpich-3.2
+    ./configure CC=$CC CXX=$CXX --disable-fortran --disable-romio --prefix=${HOME}/mpich
+    make -j2
+    make install
+    ${HOME}/mpich/bin/mpichversion
+    ${HOME}/mpich/bin/mpicc -show
+    ${HOME}/mpich/bin/mpicxx -show
+else
+    echo "MPICH installed..."
+    find ${HOME}/mpich -name mpiexec
+    find ${HOME}/mpich -name mpicc
+    find ${HOME}/mpich -name mpicxx
+fi
