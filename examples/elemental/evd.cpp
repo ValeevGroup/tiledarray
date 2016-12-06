6f054a24ff54fb74574aeae357daab161f0d9a33
drewlewis <drew90@vt.edu>
2016-12-06 17:59:35 -0500

    Update file headers to correct date and author.

--- examples/elemental/evd.cpp
+++ examples/elemental/evd.cpp
@@ -1,11 +1,11 @@
 /*
- * This file is a part of TiledArray.
- * Copyright (C) 2013  Virginia Tech
+ *  This file is a part of TiledArray.
+ *  Copyright (C) 2016  Virginia Tech
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,20 +15,16 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ *  Drew Lewis
+ *  Department of Chemistry, Virginia Tech
+ *
+ *  evd.cpp
+ *  Dec 6th, 2016
+ *  
  */
 
 #include <tiledarray.h>
 #include <iostream>
-
-bool to_bool(const char* str) {
-    if (not strcmp(str, "0") || not strcmp(str, "no") ||
-        not strcmp(str, "false"))
-        return false;
-    if (not strcmp(str, "1") || not strcmp(str, "yes") ||
-        not strcmp(str, "true"))
-        return true;
-    throw std::runtime_error("unrecognized string specification of bool");
-}
 
 int main(int argc, char** argv) {
     int rc = 0;
