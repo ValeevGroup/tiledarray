c39e2faef552a0f4e103145b667ff83f2d45673a
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-04-16 22:44:53 -0400

    updated meta unites

--- tests/meta.cpp
+++ tests/meta.cpp
@@ -51,4 +51,80 @@ BOOST_AUTO_TEST_CASE(sanity) {
   invoke(sin, invoke(async_cos, 2.0));
 }
 
+template <typename T>
+class vec {
+ public:
+  vec() = default;
+  vec(const vec& other) : size_(other.size_), data_(std::make_unique<T[]>(size_)) {
+    std::copy(other.begin(), other.end(), this->begin());
+  }
+  vec(vec& other) : size_(other.size_), data_(std::make_unique<T[]>(size_)) {
+    std::copy(other.begin(), other.end(), this->begin());
+  }
+  vec(vec&& other) {
+    std::cout << "moving vec\n";
+    std::swap(size_, other.size_);
+    std::swap(data_, other.data_);
+  }
+  explicit vec(size_t size) : size_(size), data_(std::make_unique<T[]>(size)) {}
+  vec(size_t size, T value) : size_(size), data_(std::make_unique<T[]>(size)) {
+    std::fill(begin(), end(), value);
+  }
+  vec& operator=(const vec& other) {
+    size_ = other.size_;
+    data_ = std::make_unique<T[]>(size_);
+    std::copy(other.begin(), other.end(), this->begin());
+    return *this;
+  }
+  vec& operator=(vec&& other) = default;
+  T* begin() { return data_.get(); }
+  T* end() { return begin() + size_; }
+  const T* begin() const { return data_.get(); }
+  const T* end() const { return begin() + size_; }
+  size_t size() const { return size_; }
+  template <typename Archive> void serialize(Archive& ar) {abort();}
+  T& operator[](size_t idx) { return *(data_ + idx); };
+ private:
+  size_t size_;
+  std::unique_ptr<T[]> data_;
+};
+
+vec<double> vsin(const vec<double>& x) {
+  vec<double> result(x.size());
+  std::transform(x.begin(), x.end(), result.begin(), sin);
+  return result;
+}
+vec<double> vcos(const vec<double>& x) {
+  vec<double> result(x.size());
+  std::transform(x.begin(), x.end(), result.begin(), cos);
+  return result;
+}
+
+// these must be disabled since call to taskq.add() cannot deduce rvalue refs as args
+//vec<double> vsin(vec<double>&& x) {
+//  for(auto& v : x) {
+//    v = sin(v);
+//  }
+//  return std::move(x);
+//}
+//vec<double> vcos(vec<double>&& x) {
+//  for(auto& v : x) {
+//    v = cos(v);
+//  }
+//  return std::move(x);
+//}
+
+template <typename T> struct type_printer;
+madness::Future<vec<double>> async_vcos(const vec<double>& x) {
+  auto exec = [](const vec<double>& x) {
+    return vcos(x);
+  };
+  return TiledArray::get_default_world().taskq.add(exec, x);
+}
+
+BOOST_AUTO_TEST_CASE(movability) {
+  invoke(vsin, invoke(vcos, vec<double>(13, 2.0)));
+  invoke(vsin, invoke(async_vcos, vec<double>(13, 2.0)));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
