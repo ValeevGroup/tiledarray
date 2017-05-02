e945c7fb573c8c6bc5c354001fd9981a74c165cb
Drew Lewis <drew90@vt.edu>
2017-05-02 12:41:45 -0400

    Added other fortrans besideds gfortran-5 and also attempting to have El use libblas instead of compiling openBlas.

--- bin/build-linux.sh
+++ bin/build-linux.sh
@@ -4,22 +4,21 @@
 set -ev
 
 # Environment variables
-export CXXFLAGS="-mno-avx"
 if [ "$CXX" = "g++" ]; then
     export CC=/usr/bin/gcc-$GCC_VERSION
     export CXX=/usr/bin/g++-$GCC_VERSION
+    export CXXFLAGS="-mno-avx -fext-numeric-literals"
+    export F77=gfortran-$GCC_VERSION
 else
     export CC=/usr/bin/clang-3.8
     export CXX=/usr/bin/clang++-3.8
+    export CXXFLAGS="-mno-avx"
 fi
 
 export MPI_HOME=$HOME/mpich
 export MPICC=$MPI_HOME/bin/mpicc
 export MPICXX=$MPI_HOME/bin/mpicxx
 export LD_LIBRARY_PATH=/usr/lib/lapack:/usr/lib/libblas:$LD_LIBRARY_PATH
-
-# Options for Elemental
-export F77=gfortran-5
 
 # Configure TiledArray
 mkdir _build
@@ -30,7 +29,7 @@ cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DCMAKE_CXX_COMPILER=$CXX \
   -DCMAKE_C_COMPILER=$CC -DMPI_CXX_COMPILER=$MPICXX -DMPI_C_COMPILER=$MPICC \
   -DTA_BUILD_UNITTEST=ON -DCMAKE_BUILD_TYPE=Debug -DTA_ERROR="throw" \
   -DENABLE_ELEMENTAL=ON -Wno-dev -DCMAKE_CXX_FLAGS="-fext-numeric-literals" \
-  -DMADNESS_CMAKE_EXTRA_ARGS="-Wno-dev;-DELEMENTAL_CMAKE_BUILD_TYPE=Debug;-DELEMENTAL_CXXFLAGS='-fext-numeric-literals';-DELEMENTAL_CMAKE_EXTRA_ARGS='-DCMAKE_Fortran_COMPILER=gfortran-5'"
+  -DMADNESS_CMAKE_EXTRA_ARGS="-Wno-dev;-DELEMENTAL_CMAKE_BUILD_TYPE=Debug;-DELEMENTAL_MATH_LIBS='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -lapack';-DELEMENTAL_CMAKE_EXTRA_ARGS=\"-DCMAKE_Fortran_COMPILER=$F77\""
 
 # Build all libraries, examples, and applications
 make -j2 all VERBOSE=1
