808c1d60bcbd3fb4e52c9f90bbb8ae587be8262c
Drew Lewis <drew90@vt.edu>
2017-05-02 06:35:36 -0400

    Attempting to fix Elem compile issues with Travis.

--- bin/build-linux.sh
+++ bin/build-linux.sh
@@ -12,19 +12,26 @@ else
     export CC=/usr/bin/clang-3.8
     export CXX=/usr/bin/clang++-3.8
 fi
+
 export MPI_HOME=$HOME/mpich
 export MPICC=$MPI_HOME/bin/mpicc
 export MPICXX=$MPI_HOME/bin/mpicxx
 export LD_LIBRARY_PATH=/usr/lib/lapack:/usr/lib/libblas:$LD_LIBRARY_PATH
+
+# Options for Elemental
+export F77=gfortran-5
+ElemOpts="-DCMAKE_Fortran_COMPILER=$F77 -DCMAKE_BUILD_TYPE=Debug -DMATH_LIBS='-lapack -lblas'"
+
 
 # Configure TiledArray
 mkdir _build
 mkdir _install
 cd _build
 # TA_ERROR="throw" is the recommended way to configure TA for running unit tests
-cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC \
-      -DMPI_CXX_COMPILER=$MPICXX -DMPI_C_COMPILER=$MPICC -DTA_BUILD_UNITTEST=ON \
-      -DCMAKE_BUILD_TYPE=Debug -DTA_ERROR="throw" -DENABLE_ELEMENTAL=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DCMAKE_CXX_COMPILER=$CXX \
+  -DCMAKE_C_COMPILER=$CC -DMPI_CXX_COMPILER=$MPICXX -DMPI_C_COMPILER=$MPICC \
+  -DTA_BUILD_UNITTEST=ON -DCMAKE_BUILD_TYPE=Debug -DTA_ERROR="throw" \
+  -DENABLE_ELEMENTAL=ON -Wno-dev -DMAD_ELEMENTAL_OPTIONS="$ElemOpts"
 
 # Build all libraries, examples, and applications
 make -j2 all VERBOSE=1
