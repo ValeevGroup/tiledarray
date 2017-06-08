cd1d9b9f3d0c09985f88ea614845511ed4abdeb2
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-06-07 22:27:02 -0400

    bumped madness revision

--- external/madness.cmake
+++ external/madness.cmake
@@ -98,7 +98,7 @@ else()
         "Path to the MADNESS build directory")
   set(MADNESS_URL "https://github.com/m-a-d-n-e-s-s/madness.git" CACHE STRING 
         "Path to the MADNESS repository")
-  set(MADNESS_TAG "517684fad448561c1922c6d7f9d2837c22c16f30" CACHE STRING 
+  set(MADNESS_TAG "e13f7c3b727753654b40b3d146a210048aa36ac4" CACHE STRING 
         "Revision hash or tag to use when building MADNESS")
   
   if("${MADNESS_TAG}" STREQUAL "")
