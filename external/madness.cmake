70000fbe8badeb94b481c5f85ff596960cd550e7
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-06-10 07:23:30 -0400

    simplified foreach MP boilerplate and bumped to version of MADNESS that can work with the forach code

--- external/madness.cmake
+++ external/madness.cmake
@@ -98,7 +98,7 @@ else()
         "Path to the MADNESS build directory")
   set(MADNESS_URL "https://github.com/m-a-d-n-e-s-s/madness.git" CACHE STRING 
         "Path to the MADNESS repository")
-  set(MADNESS_TAG "e13f7c3b727753654b40b3d146a210048aa36ac4" CACHE STRING 
+  set(MADNESS_TAG "01c1f2fde546d0737a06e58c396ad35947bdfa4b" CACHE STRING 
         "Revision hash or tag to use when building MADNESS")
   
   if("${MADNESS_TAG}" STREQUAL "")
