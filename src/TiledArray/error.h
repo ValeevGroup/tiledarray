501abda6e4d801dc98fcc4cd001adfed77ac3a44
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-07-10 17:29:46 -0400

    added DEPRECATEDMSG macro that takes an additional message

--- src/TiledArray/error.h
+++ src/TiledArray/error.h
@@ -147,4 +147,15 @@ namespace TiledArray {
 #define DEPRECATED
 #endif
 
+// same as DEPRECATED, but annotated with a message
+// will result in a warning
+#if __cplusplus >= 201402L
+#define DEPRECATEDMSG(msg)  [[deprecated(msg)]]
+#elif defined(__GNUC__)
+#define DEPRECATEDMSG(msg) __attribute__((deprecated(msg)))
+#else
+#pragma message("WARNING: You need to implement DEPRECATEDMSG for this compiler")
+#define DEPRECATEDMSG(msg)
+#endif
+
 #endif // TILEDARRAY_ERROR_H__INCLUDED
