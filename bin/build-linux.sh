add3b7bcc65f1d8985f46e2aa4da4bedec0a9318
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-11-20 08:52:41 -0500

    revised travis config to cache install dir (+ reorganized along the lines of mpqc)

--- bin/build-linux.sh
+++ bin/build-linux.sh
@@ -16,28 +16,44 @@ else
     export F77=gfortran-$GCC_VERSION
 fi
 
-export MPI_HOME=$HOME/mpich
+export MPI_HOME=${INSTALL_PREFIX}/mpich
 export MPICC=$MPI_HOME/bin/mpicc
 export MPICXX=$MPI_HOME/bin/mpicxx
 export LD_LIBRARY_PATH=/usr/lib/lapack:/usr/lib/libblas:$LD_LIBRARY_PATH
 
-# Configure TiledArray
-mkdir _build
-mkdir _install
-cd _build
-# TA_ERROR="throw" is the recommended way to configure TA for running unit tests
-cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DCMAKE_CXX_COMPILER=$CXX \
-  -DCMAKE_C_COMPILER=$CC -DMPI_CXX_COMPILER=$MPICXX -DMPI_C_COMPILER=$MPICC \
-  -DTA_BUILD_UNITTEST=ON -DCMAKE_BUILD_TYPE=Debug -DTA_ERROR="throw" \
-  -DENABLE_ELEMENTAL=ON -Wno-dev \
-  -DMADNESS_CMAKE_EXTRA_ARGS="-Wno-dev;-DELEMENTAL_CMAKE_BUILD_TYPE=Debug;-DELEMENTAL_MATH_LIBS='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -llapack';-DELEMENTAL_CMAKE_EXTRA_ARGS=-DCMAKE_Fortran_COMPILER=$F77"
+# list the prebuilt prereqs
+ls ${INSTALL_PREFIX}
+
+# where to install MPQC4 (need for testing installed code)
+export INSTALL_DIR=${INSTALL_PREFIX}/TA
+
+# make build dir
+cd ${BUILD_PREFIX}
+mkdir -p TA
+cd TA
+
+cmake ${TRAVIS_BUILD_DIR} \
+    -DCMAKE_CXX_COMPILER=$CXX \
+    -DCMAKE_C_COMPILER=$CC \
+    -DMPI_CXX_COMPILER=$MPICXX \
+    -DMPI_C_COMPILER=$MPICC \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_CXX_FLAGS="-ftemplate-depth=1024 -Wno-unused-command-line-argument" \
+    -DTA_BUILD_UNITTEST=ON \
+    -DTA_ERROR="throw" \
+    -DENABLE_ELEMENTAL=ON -Wno-dev \
+    -DMADNESS_CMAKE_EXTRA_ARGS="-Wno-dev;-DELEMENTAL_CMAKE_BUILD_TYPE=Debug;-DELEMENTAL_MATH_LIBS='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -llapack';-DELEMENTAL_CMAKE_EXTRA_ARGS=-DCMAKE_Fortran_COMPILER=$F77"
 
 # Build all libraries, examples, and applications
 make -j2 all VERBOSE=1
 make install
+
+# Validate
 make -j2 ta_test VERBOSE=1
-cd tests
 export MAD_NUM_THREADS=2
-./ta_test --show_progress
-cd ..
+setarch `uname -m` -R make check
+
+# Elemental-only: build evd example
 make evd
