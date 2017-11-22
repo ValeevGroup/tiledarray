937bab523dc2f971a1c5bb74bf2e09d0c446298a
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-11-22 16:59:53 -0500

    travis: duh, check for cached copy of mad+el! chopping off a few more secs from the standard timeout to make rebuilding cache more robust

--- bin/build-madness-linux.sh
+++ bin/build-madness-linux.sh
@@ -30,43 +30,46 @@ if [ "$BUILD_TYPE" = "Debug" ]; then
   # where to install MADNESS+Elemental (need for testing installed code)
   export INSTALL_DIR=${INSTALL_PREFIX}/madness
 
-  # make build dir
-  cd ${BUILD_PREFIX}
-  mkdir -p madness
-  cd madness
+  if [ ! -d "${INSTALL_DIR}" ]; then
 
-  git clone https://github.com/m-a-d-n-e-s-s/madness madness_src
-  cd madness_src
-  git checkout ta-master-track
-  cd ..
+    # make build dir
+    cd ${BUILD_PREFIX}
+    mkdir -p madness
+    cd madness
 
-  cmake madness_src \
-    -DCMAKE_CXX_COMPILER=$CXX \
-    -DCMAKE_C_COMPILER=$CC \
-    -DMPI_CXX_COMPILER=$MPICXX \
-    -DMPI_C_COMPILER=$MPICC \
-    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_CXX_FLAGS="-ftemplate-depth=1024 -Wno-unused-command-line-argument ${EXTRACXXFLAGS}" \
-    -DENABLE_MPI=ON \
-    -DMPI_THREAD=multiple \
-    -DENABLE_TBB=ON \
-    -DTBB_ROOT_DIR=/usr \
-    -DENABLE_MKL=OFF \
-    -DFORTRAN_INTEGER_SIZE=4 \
-    -DENABLE_LIBXC=OFF \
-    -DLAPACK_LIBRARIES='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -llapack' \
-    -DENABLE_GPERFTOOLS=OFF \
-    -DASSERTION_TYPE=throw \
-    -DDISABLE_WORLD_GET_DEFAULT=ON \
-    -DENABLE_ELEMENTAL=ON \
-    -DELEMENTAL_TAG=ca171b964a02d7a7c52d019fdf18cf01986e3de6 \
-    -DELEMENTAL_CMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DELEMENTAL_MATH_LIBS='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -llapack' \
-    -DELEMENTAL_CMAKE_EXTRA_ARGS=-DCMAKE_Fortran_COMPILER=$F77
+    git clone https://github.com/m-a-d-n-e-s-s/madness madness_src
+    cd madness_src
+    git checkout ta-master-track
+    cd ..
 
-  # Build all libraries, examples, and applications
-  make -j2 install-elemental install-world install-clapack install-common install-config VERBOSE=1
+    cmake madness_src \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_COMPILER=$CC \
+      -DMPI_CXX_COMPILER=$MPICXX \
+      -DMPI_C_COMPILER=$MPICC \
+      -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_CXX_FLAGS="-ftemplate-depth=1024 -Wno-unused-command-line-argument ${EXTRACXXFLAGS}" \
+      -DENABLE_MPI=ON \
+      -DMPI_THREAD=multiple \
+      -DENABLE_TBB=ON \
+      -DTBB_ROOT_DIR=/usr \
+      -DENABLE_MKL=OFF \
+      -DFORTRAN_INTEGER_SIZE=4 \
+      -DENABLE_LIBXC=OFF \
+      -DLAPACK_LIBRARIES='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -llapack' \
+      -DENABLE_GPERFTOOLS=OFF \
+      -DASSERTION_TYPE=throw \
+      -DDISABLE_WORLD_GET_DEFAULT=ON \
+      -DENABLE_ELEMENTAL=ON \
+      -DELEMENTAL_TAG=ca171b964a02d7a7c52d019fdf18cf01986e3de6 \
+      -DELEMENTAL_CMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DELEMENTAL_MATH_LIBS='-L/usr/lib/libblas -L/usr/lib/lapack -lblas -llapack' \
+      -DELEMENTAL_CMAKE_EXTRA_ARGS=-DCMAKE_Fortran_COMPILER=$F77
+
+    # Build all libraries, examples, and applications
+    make -j2 install-elemental install-world install-clapack install-common install-config VERBOSE=1
+  fi
 
 fi
