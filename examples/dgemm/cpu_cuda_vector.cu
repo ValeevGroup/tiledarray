92910545ea9332b85ba754dfb847531bd557241b
Eduard Valeyev <eduard@valeyev.net>
2018-01-25 09:01:57 -0500

    Manually added missing instantiations of cuda_cub::copy functions.

--- examples/dgemm/cpu_cuda_vector.cu
+++ examples/dgemm/cpu_cuda_vector.cu
@@ -17,3 +17,26 @@ void resize<float,thrust::device_malloc_allocator<float>>(
 template class cpu_cuda_vector<double>;
 template class cpu_cuda_vector<float>;
 
+
+void force_missing_instantiations_double() {
+
+using Real = double;
+thrust::cuda_cub::execution_policy<thrust::cuda_cub::tag> policy;
+thrust::detail::normal_iterator<thrust::device_ptr<Real const> > it;
+thrust::device_ptr<Real> ptr;
+
+auto x = thrust::cuda_cub::copy<thrust::cuda_cub::tag, thrust::detail::normal_iterator<thrust::device_ptr<Real const> >, thrust::device_ptr<Real> >(policy, it, it, ptr);
+
+}
+
+void force_missing_instantiations_float() {
+
+using Real = float;
+thrust::cuda_cub::execution_policy<thrust::cuda_cub::tag> policy;
+thrust::detail::normal_iterator<thrust::device_ptr<Real const> > it;
+thrust::device_ptr<Real> ptr;
+
+auto x = thrust::cuda_cub::copy<thrust::cuda_cub::tag, thrust::detail::normal_iterator<thrust::device_ptr<Real const> >, thrust::device_ptr<Real> >(policy, it, it, ptr);
+
+}
+
