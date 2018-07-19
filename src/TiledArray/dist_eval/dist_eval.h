766d622502eab90b2781b18c0f0f586ae4050e79
Eduard Valeyev <eduard@valeyev.net>
2018-07-19 11:09:01 -0400

    more detailed handling of exceptions in DistEval::wait + instead of rethrowing abort

--- src/TiledArray/dist_eval/dist_eval.h
+++ src/TiledArray/dist_eval/dist_eval.h
@@ -174,16 +174,30 @@ namespace TiledArray {
       void wait() const {
         const int task_count = task_count_;
         if(task_count > 0) {
+          auto report_and_abort = [&,this](const char* type, const char* what = nullptr) {
+            std::stringstream ss;
+            ss << "!! ERROR TiledArray: Aborting due to " << type << " exception.\n"
+               << (what != nullptr ? "!! ERROR TiledArray: " : "")
+               << (what != nullptr ? what : "")
+               << (what != nullptr ? "\n" : "")
+               << "!! ERROR TiledArray: rank=" << TensorImpl_::world().rank()
+               << " id=" << id_ << " " << set_counter_ << " of " << task_count << " tiles set" << std::endl;
+            std::cerr << ss.str().c_str();
+            abort();
+          };
           try {
             TensorImpl_::world().await([this,task_count] ()
                 { return this->set_counter_ == task_count; });
+          } catch(TiledArray::Exception& e) {
+            report_and_abort("TiledArray", e.what());
+          } catch(madness::MadnessException& e) {
+            report_and_abort("MADNESS", e.what());
+          } catch(SafeMPI::Exception& e) {
+            report_and_abort("SafeMPI", e.what());
+          } catch(std::exception& e) {
+            report_and_abort("std", e.what());
           } catch(...) {
-            std::stringstream ss;
-            ss << "!! ERROR TiledArray: Aborting due to exception.\n"
-               << "!! ERROR TiledArray: rank=" << TensorImpl_::world().rank()
-               << " id=" << id_ << " " << set_counter_ << " of " << task_count << " tiles set\n";
-            std::cerr << ss.str().c_str();
-            throw;
+            report_and_abort("", nullptr);
           }
         }
       }
