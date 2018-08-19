da37b6483c524b2c09ebc414f91e3f59bf6ef375
Eduard Valeyev <eduard@valeyev.net>
2018-08-19 15:42:12 -0400

    updated wiki example 1 to match the wiki [skip ci]

--- doc/devsamp/wiki/user-guide-1.cpp
+++ doc/devsamp/wiki/user-guide-1.cpp
@@ -1,11 +1,12 @@
-#include <tiledarray.h>
+#include <tiledarray.h> // imports most TiledArray features
 
 int main(int argc, char* argv[]) {
-  // Initialize runtime
+  // Initialize MADWorld
   madness::World& world = madness::initialize(argc, argv);
 
   // Do some work here.
 
+  // Finalize MADWorld
   madness::finalize();
   return 0;
 }
