23c8633f1dd4dc1013a363c4b11170f38c337a88
Eduard Valeyev <eduard@valeyev.net>
2019-07-06 12:24:07 -0400

    to_new_tile_type can handle "lazy" tiles

--- src/TiledArray/conversions/to_new_tile_type.h
+++ src/TiledArray/conversions/to_new_tile_type.h
@@ -30,17 +30,47 @@
 
 namespace TiledArray {
 
+  namespace detail {
+    template <typename DstTile, typename SrcTile, typename Op, typename Enabler = void>
+    struct cast_then_op;
+
+    template <typename Tile, typename Op>
+    struct cast_then_op<Tile, Tile, Op, void> {
+      cast_then_op(const Op& op) : op_(op) {}
+
+      auto operator()(const Tile& tile) const {
+        return op_(tile);
+      }
+
+      const Op& op_;
+    };
+
+    template <typename DstTile, typename SrcTile, typename Op>
+    struct cast_then_op<DstTile, SrcTile, Op, std::enable_if_t<!std::is_same<DstTile,SrcTile>::value>> {
+      cast_then_op(const Op& op) : op_(op) {}
+
+      auto operator()(const SrcTile& tile) const {
+        return op_(Cast<DstTile,SrcTile>{}(tile));
+      }
+
+      const Op& op_;
+    };
+
+  }  // namespace detail
+
   /// Function to convert an array to a new array with a different tile type.
 
   /// \tparam Tile The array tile type
+  /// \tparam ConvTile The tile type to which Tile can be converted to and
+  ///                  for which \c Op(ConvTile) is well-formed
   /// \tparam Policy The array policy type
   /// \tparam Op The tile conversion operation type
   /// \param array The array to be converted
   /// \param op The tile type conversion operation
-  template <typename Tile, typename Policy, typename Op>
-  inline DistArray<typename std::result_of<Op(Tile)>::type, Policy>
+  template <typename Tile, typename ConvTile = Tile, typename Policy, typename Op>
+  inline DistArray<typename std::result_of<Op(ConvTile)>::type, Policy>
   to_new_tile_type(DistArray<Tile, Policy> const &old_array, Op &&op) {
-    using OutTileType = typename std::result_of<Op(Tile)>::type;
+    using OutTileType = typename std::result_of<Op(ConvTile)>::type;
     using OutArray = DistArray<OutTileType, Policy>;
 
     static_assert(!std::is_same<Tile, OutTileType>::value,
@@ -55,12 +85,31 @@ namespace TiledArray {
     pmap_iter it = old_array.pmap()->begin();
     pmap_iter end = old_array.pmap()->end();
 
+#if __cplusplus < 201703L
+    const detail::cast_then_op<ConvTile, Tile, std::remove_reference_t<Op>> cast_op(op);
+#endif
+
     for(; it != end; ++it) {
       // Must check for zero because pmap_iter does not.
       if(!old_array.is_zero(*it)) {
         // Spawn a task to evaluate the tile
-        Future<OutTileType> tile = world.taskq.add(op, old_array.find(*it));
-        new_array.set(*it, tile);
+        // 2 cases:
+        // - ConvTile = Tile -> call op directly
+        // - ConvTile != Tile -> chain Cast<> and op
+#if __cplusplus >= 201703L
+        if constexpr (std::is_same<ConvTile,Tile>::value) {
+          new_array.set(*it, world.taskq.add(op, old_array.find(*it)));
+        }
+        else {
+          auto cast_op = [=] (const Tile& tile) {
+            return op(Cast<ConvTile, Tile>{}(tile));
+          };
+          new_array.set(*it, world.taskq.add(cast_op, old_array.find(*it)));
+        }
+#else
+        new_array.set(*it, world.taskq.add(cast_op, old_array.find(*it)));
+#endif
+
       }
     }
 
