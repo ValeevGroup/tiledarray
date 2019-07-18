6bb8e359cdefd4ded185b9ca8b35a3ecc0d72f96
Eduard Valeyev <eduard@valeyev.net>
2019-07-18 09:54:13 -0400

    yet another bump of madness

--- external/madness.cmake
+++ external/madness.cmake
@@ -11,7 +11,7 @@ include(ExternalProject)
 include(ConvertIncludesListToCompilerArgs)
 include(ConvertLibrariesListToCompilerArgs)
 
-set(MADNESS_OLDEST_TAG "0ff934c10616b6f0e3a72548b6be24b75c111253" CACHE STRING
+set(MADNESS_OLDEST_TAG "3fbebbf385e5e94de04666d898b63cc8cc807dff" CACHE STRING
         "The oldest revision hash or tag of MADNESS that can be used")
 
 find_package(MADNESS 0.10.1 CONFIG QUIET COMPONENTS world HINTS ${MADNESS_ROOT_DIR})
