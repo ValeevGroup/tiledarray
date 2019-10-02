d7501195794183e81b93450ffdc2621da1f115be
Eduard Valeyev <eduard@valeyev.net>
2019-10-02 15:18:56 -0400

    sync w madness

--- external/madness.cmake
+++ external/madness.cmake
@@ -11,7 +11,7 @@ include(ExternalProject)
 include(ConvertIncludesListToCompilerArgs)
 include(ConvertLibrariesListToCompilerArgs)
 
-set(MADNESS_OLDEST_TAG "b11c26391d53d12a429a257186053240fb80ad54" CACHE STRING
+set(MADNESS_OLDEST_TAG "7a1f9d77b712cde823acadd27bffbc49e2052b90" CACHE STRING
         "The oldest revision hash or tag of MADNESS that can be used")
 
 find_package(MADNESS 0.10.1 CONFIG QUIET COMPONENTS world HINTS ${MADNESS_ROOT_DIR})
