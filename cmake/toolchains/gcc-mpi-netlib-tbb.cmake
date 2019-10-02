d49a06e39f1c457ac8a914a7ad938021e3213255
Eduard Valeyev <eduard@valeyev.net>
2019-10-02 10:58:39 -0400

    better names for generic toolchains + two new generic toolchains for netlib/lapacke

--- cmake/toolchains/gcc-mpi-netlib-tbb.cmake
+++ cmake/toolchains/gcc-mpi-netlib-tbb.cmake
@@ -11,7 +11,6 @@
 #
 
 # Set paths (add a cache entry for the paths used by the configure script)
-set(MKL_ROOT_DIR "$ENV{INTEL_DIR}/mkl")
 set(TBB_ROOT_DIR "$ENV{INTEL_DIR}/tbb" CACHE PATH "TBB root directory")
 # query EIGEN3_DIR and (deprecated) EIGEN_DIR envvars
 if (DEFINED ENV{EIGEN3_DIR})
@@ -36,13 +35,11 @@ set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -march=native -DNDEBUG" CACHE STRING "In
 set(CMAKE_C_FLAGS_RELEASE          "-O3 -march=native -DNDEBUG" CACHE STRING "Initial C release compile flags")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -Wall" CACHE STRING "Initial C release with debug info compile flags")
 set(CMAKE_CXX_FLAGS_INIT           "" CACHE STRING "Initial C++ compile flags")
-set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall" CACHE STRING "Initial C++ debug compile flags")
+set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall -ftemplate-backtrace-limit=0" CACHE STRING "Initial C++ debug compile flags")
 set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -march=native -DNDEBUG" CACHE STRING "Initial C++ minimum size release compile flags")
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -march=native -DNDEBUG" CACHE STRING "Initial C++ release compile flags")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall" CACHE STRING "Initial C++ release with debug info compile flags")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall -ftemplate-backtrace-limit=0" CACHE STRING "Initial C++ release with debug info compile flags")
 
-set(LAPACK_LIBRARIES "-Wl,--start-group" "${MKL_ROOT_DIR}/lib/intel64/libmkl_intel_lp64.a" "${MKL_ROOT_DIR}/lib/intel64/libmkl_core.a" "${MKL_ROOT_DIR}/lib/intel64/libmkl_sequential.a" "-Wl,--end-group" "-lpthread" "-lm" "-ldl" CACHE STRING "BLAS linker flags")
-set(LAPACK_INCLUDE_DIRS ${MKL_ROOT_DIR}/include CACHE STRING "LAPACK include directories")
-set(LAPACK_COMPILE_DEFINITIONS BTAS_HAS_CBLAS=1;MADNESS_LINALG_USE_LAPACKE CACHE STRING "LAPACK preprocessor definitions")
+set(LAPACK_LIBRARIES "-llapack" "-lblas" CACHE STRING "BLAS linker flags")
 set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libraries")
