68737a97a292f82c7f55a8867b31c08204855c83
Eduard Valeyev <eduard@valeyev.net>
2019-10-21 11:54:19 -0400

    UM pool uses prefetched memory

--- src/TiledArray/external/cuda.h
+++ src/TiledArray/external/cuda.h
@@ -43,6 +43,7 @@
 #include <umpire/strategy/DynamicPool.hpp>
 #include <umpire/strategy/ThreadSafeAllocator.hpp>
 #include <umpire/strategy/SizeLimiter.hpp>
+#include "umpire/strategy/AllocationPrefetcher.hpp"
 
 #include <madness/tensor/cblas.h>
 #include <madness/world/thread.h>
@@ -256,8 +257,10 @@ class cudaEnv {
 
       auto mem_total_free = cudaEnv::memory_total_and_free();
 
+      auto um_prefetcher = rm.makeAllocator<umpire::strategy::AllocationPrefetcher>(
+                                   "UMPrefetcher", rm.getAllocator("UM"));
       auto um_dynamic_pool = rm.makeAllocator<umpire::strategy::DynamicPool>(
-          "UMDynamicPool", rm.getAllocator("UM"), 2048 * 1024 * 1024ul,
+          "UMDynamicPool", um_prefetcher, 2048 * 1024 * 1024ul,
           10 * 1024 * 1024ul);
       auto thread_safe_um_dynamic_pool =
           rm.makeAllocator<umpire::strategy::ThreadSafeAllocator>(
