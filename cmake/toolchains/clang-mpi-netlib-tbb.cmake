bb4e2fe93acda220483a2ad9cdd0792d595f27af
Eduard Valeyev <eduard@valeyev.net>
2019-12-27 12:12:38 -0500

    misc updates to the toolchains

--- cmake/toolchains/clang-mpi-netlib-tbb.cmake
+++ cmake/toolchains/clang-mpi-netlib-tbb.cmake
@@ -11,7 +11,6 @@
 #
 
 # Set paths (add a cache entry for the paths used by the configure script)
-set(MKL_ROOT_DIR "$ENV{INTEL_DIR}/mkl")
 set(TBB_ROOT_DIR "$ENV{INTEL_DIR}/tbb" CACHE PATH "TBB root directory")
 # query EIGEN3_DIR and (deprecated) EIGEN_DIR envvars
 if (DEFINED ENV{EIGEN3_DIR})
@@ -23,13 +22,8 @@ set(EIGEN3_INCLUDE_DIR "${ENV_EIGEN3_DIR}" CACHE PATH "Eigen3 library directory"
 set(BOOST_ROOT "$ENV{BOOST_DIR}" CACHE PATH "Boost root directory")
 
 # Set compilers (assumes the compilers are in the PATH)
-if ($ENV{GCC_VERSION})
-  set(CMAKE_C_COMPILER gcc-$ENV{GCC_VERSION})
-  set(CMAKE_CXX_COMPILER g++-$ENV{GCC_VERSION})
-else ()
-  set(CMAKE_C_COMPILER gcc)
-  set(CMAKE_CXX_COMPILER g++)
-endif ()
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 set(MPI_C_COMPILER mpicc)
 set(MPI_CXX_COMPILER mpicxx)
 set(MPI_CXX_SKIP_MPICXX ON)
@@ -40,14 +34,12 @@ set(CMAKE_C_FLAGS_DEBUG            "-g -Wall" CACHE STRING "Initial C debug comp
 set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -march=native -DNDEBUG" CACHE STRING "Initial C minimum size release compile flags")
 set(CMAKE_C_FLAGS_RELEASE          "-O3 -march=native -DNDEBUG" CACHE STRING "Initial C release compile flags")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -Wall" CACHE STRING "Initial C release with debug info compile flags")
-set(CMAKE_CXX_FLAGS_INIT           "-fno-omit-frame-pointer -fno-optimize-sibling-calls" CACHE STRING "Initial C++ compile flags")
+set(CMAKE_CXX_FLAGS_INIT           "" CACHE STRING "Initial C++ compile flags")
 set(CMAKE_CXX_FLAGS_DEBUG          "-g -Wall -ftemplate-backtrace-limit=0" CACHE STRING "Initial C++ debug compile flags")
 set(CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -march=native -DNDEBUG" CACHE STRING "Initial C++ minimum size release compile flags")
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -march=native -DNDEBUG" CACHE STRING "Initial C++ release compile flags")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-Og -g3 -Wall -ftemplate-backtrace-limit=0" CACHE STRING "Initial C++ release with debug info compile flags")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall -ftemplate-backtrace-limit=0" CACHE STRING "Initial C++ release with debug info compile flags")
 
-set(LAPACK_LIBRARIES "-Wl,--start-group" "${MKL_ROOT_DIR}/lib/intel64/libmkl_intel_lp64.a" "${MKL_ROOT_DIR}/lib/intel64/libmkl_core.a" "${MKL_ROOT_DIR}/lib/intel64/libmkl_sequential.a" "-Wl,--end-group" "-lpthread" "-lm" "-ldl" CACHE STRING "BLAS linker flags")
-set(LAPACK_INCLUDE_DIRS ${MKL_ROOT_DIR}/include CACHE STRING "LAPACK include directories")
-set(LAPACK_COMPILE_DEFINITIONS BTAS_HAS_CBLAS=1;MADNESS_LINALG_USE_LAPACKE CACHE STRING "LAPACK preprocessor definitions")
+set(LAPACK_LIBRARIES "-llapack" "-lblas" CACHE STRING "BLAS linker flags")
 set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libraries")
