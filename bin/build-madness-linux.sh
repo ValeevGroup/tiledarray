53464babe7f22e6a5416ef15ddb9327f250bb555
Eduard Valeyev <eduard@valeyev.net>
2019-12-29 09:11:46 -0500

    when building madness TA will add -fext-numeric-literals automatically if ENABLE_ELEMENTAL=ON and using gcc

--- bin/build-madness-linux.sh
+++ bin/build-madness-linux.sh
@@ -10,6 +10,7 @@ if [ "$BUILD_TYPE" = "Debug" ]; then
   if [ "$CXX" = "g++" ]; then
     export CC=/usr/bin/gcc-$GCC_VERSION
     export CXX=/usr/bin/g++-$GCC_VERSION
+    # MADNESS does not yet add -fext-numeric-literals when ENABLE_ELEMENTAL=ON and using GNU C++ .. TA does
     export EXTRACXXFLAGS="-mno-avx -fext-numeric-literals"
     export F77=gfortran-$GCC_VERSION
   else
