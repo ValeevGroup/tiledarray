ae0dd5bb90e3ab1a49388e130362ff8bcfa33a6b
Eduard Valeyev <eduard@valeyev.net>
2020-01-08 15:30:48 -0500

    "collective" TensorImpl ctor checks that the shapes are bitwise-identical across the World

--- src/TiledArray/dense_shape.h
+++ src/TiledArray/dense_shape.h
@@ -192,6 +192,8 @@ namespace TiledArray {
   constexpr inline bool operator==(const DenseShape& a, const DenseShape& b) { return true; }
   constexpr inline bool operator!=(const DenseShape& a, const DenseShape& b) { return !(a==b); }
 
+  constexpr inline bool is_replicated(World& world, const DenseShape& t) { return true; }
+
 } // namespace TiledArray
 
 #endif // TILEDARRAY_DENSE_SHAPE_H__INCLUDED
