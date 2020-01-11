323b71f21db71b112173f1fac56617b758cef9b9
Eduard Valeyev <eduard@valeyev.net>
2020-01-11 17:52:26 -0500

    fixed cmake syntax error in b8a59e7ac1f8815b63ac013bc191eaefb5db6f8a

--- external/madness.cmake
+++ external/madness.cmake
@@ -448,7 +448,7 @@ append_flags(CMAKE_CXX_FLAGS "${MADNESS_COMPILE_FLAGS}")
 # gcc needs -fext-numeric-literals to compile/use ELemental
 # neither MADNESS nor Elemental export this compile option
 if (${TILEDARRAY_HAS_ELEMENTAL} AND ${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
-  set(append_flags(CMAKE_CXX_FLAGS "-fext-numeric-literals")
+  append_flags(CMAKE_CXX_FLAGS "-fext-numeric-literals")
 endif()
 append_flags(CMAKE_EXE_LINKER_FLAGS "${MADNESS_LINKER_FLAGS}")
 
