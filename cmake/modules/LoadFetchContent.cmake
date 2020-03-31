635695af51f8390dda4e30ad1b01fa0aabb44d71
Eduard Valeyev <eduard@valeyev.net>
2020-03-31 16:11:59 -0400

    more updates

--- cmake/modules/LoadFetchContent.cmake
+++ cmake/modules/LoadFetchContent.cmake
@@ -4,12 +4,4 @@
 
 set(FETCHCONTENT_UPDATES_DISCONNECTED OFF CACHE BOOL "Enables UPDATE_DISCONNECTED behavior for all content population")
 include(FetchContent)
-if(${CMAKE_VERSION} VERSION_LESS 3.14)
-  macro(FetchContent_MakeAvailable NAME)
-    FetchContent_GetProperties(${NAME})
-    if(NOT ${NAME}_POPULATED)
-      FetchContent_Populate(${NAME})
-      add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
-    endif()
-  endmacro()
-endif()
+cmake_minimum_required(VERSION 3.14)  # neet FetchContent_MakeAvailable
