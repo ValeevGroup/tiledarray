9c71e2d2601a459136aeb193f8335e6573998f78
Eduard Valeyev <eduard@valeyev.net>
2020-04-07 22:37:58 -0400

    use CMAKE_HOST_SYSTEM_NAME if CMAKE_SYSTEM_NAME is not defined

--- cmake/toolchains/_tbb.cmake
+++ cmake/toolchains/_tbb.cmake
@@ -1,7 +1,12 @@
 ############## Only usable on UNIX platforms
 if (NOT UNIX)
-  message(FATAL_ERROR "intel-tbb.cmake is only usable on UNIX platforms")
+  message(FATAL_ERROR "_tbb.cmake is only usable on UNIX platforms")
 endif(NOT UNIX)
+if (DEFINED CMAKE_SYSTEM_NAME)
+  set(_cmake_system_name ${CMAKE_SYSTEM_NAME})
+else(DEFINED CMAKE_SYSTEM_NAME)
+  set(_cmake_system_name ${CMAKE_HOST_SYSTEM_NAME})
+endif(DEFINED CMAKE_SYSTEM_NAME)
 
 ############## Top tools dir
 if (DEFINED ENV{INTEL_DIR})
@@ -19,10 +24,10 @@ else(DEFINED ENV{TBBROOT})
 endif(DEFINED ENV{TBBROOT})
 set(TBB_ROOT_DIR "${_tbb_root_dir}" CACHE PATH "Intel TBB root directory")
 
-if(APPLE)
+if(${_cmake_system_name} MATCHES "Darwin")
   set(TBB_LIBRARY_DIRS "${TBB_ROOT_DIR}/lib/libc++;${TBB_ROOT_DIR}/lib"
       CACHE PATH "Intel TBB library directory list")
-else(APPLE)
+elseif(${_cmake_system_name} MATCHES "Linux")
   if(EXISTS ${TBB_ROOT_DIR}/lib/intel64/gcc4.8) # Intel packaged TBB
     set(TBB_LIBRARY_DIRS "${TBB_ROOT_DIR}/lib/intel64/gcc4.8"
         CACHE PATH "Intel TBB library directory list")
@@ -39,4 +44,7 @@ else(APPLE)
     set(TBB_LIBRARY_DIRS "${TBB_ROOT_DIR}/lib"
         CACHE PATH "Intel TBB library directory list")
   endif()
+else()
+  set(TBB_LIBRARY_DIRS "${TBB_ROOT_DIR}/lib"
+      CACHE PATH "Intel TBB library directory list")
 endif()
