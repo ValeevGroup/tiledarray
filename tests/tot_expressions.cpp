53faff22778488f84a0cbadffc5ea69b3b4eec32
Ryan Richard <ryanmrichard1@gmail.com>
2020-05-05 17:17:40 -0500

    backup

--- tests/tot_expressions.cpp
+++ tests/tot_expressions.cpp
@@ -16,4 +16,22 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(no_perm, TestParam, test_params){
   }
 }
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(permute_outer, TestParam, test_params){
+  for(auto tr_t : run_all<TestParam>()){
+    auto& in_rank = std::get<1>(tr_t);
+    auto& t       = std::get<2>(tr_t);
+
+    if(t.range().rank() == 1) continue;
+
+    std::string rhs_out_idx = "i, j";
+    std::string lhs_out_idx = "j, i";
+    std::string in_idx      = in_rank == 1 ? "k" : "k, l";
+    std::string rhs_idx     = rhs_out_idx + ";" + in_idx;
+    std::string lhs_idx     = lhs_out_idx + ";" + in_idx;
+    tensor_type<TestParam> result;
+    result(lhs_idx) = t(rhs_idx);
+  }
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
