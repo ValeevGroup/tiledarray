2d48c1202db3094dacf1f05318ab68f1a42d9f2c
Eduard Valeyev <eduard@valeyev.net>
2020-05-06 13:30:24 -0400

    index_parsing.h -> annotation.h

--- src/TiledArray/util/annotation.h
+++ src/TiledArray/util/annotation.h
@@ -1,6 +1,7 @@
 /*
  *  This file is a part of TiledArray.
- *  Copyright (C) 2015  Virginia Tech
+ *  Copyright (C) 2013  Virginia Tech
+ *  Copyright (C) 2020  Ames Laboratory
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,41 +16,52 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Justus Calvin
+ *  Eduard Valeyev
  *  Department of Chemistry, Virginia Tech
  *
- *  tensor.h
- *  Jun 16, 2015
+ *  Ryan Richards
+ *  Ames Laboratory
+ *
+ *  annotation.h
+ *  April 20, 2020
  *
  */
 
-#ifndef TILEDARRAY_INDEX_PARSING_H__INCLUDED
-#define TILEDARRAY_INDEX_PARSING_H__INCLUDED
-#include "TiledArray/error.h"
+#ifndef TILEDARRAY_UTILS_ANNOTATION_H__INCLUDED
+#define TILEDARRAY_UTILS_ANNOTATION_H__INCLUDED
+
 #include <algorithm>
 #include <string>
+#include "TiledArray/error.h"
 
 namespace TiledArray::detail {
 
-inline bool is_tot_index(const std::string& idx) {
-  return idx.find(";") != std::string::npos;
+inline std::string dummy_annotation(unsigned int ndim) {
+  std::ostringstream oss;
+  if (ndim > 0) oss << "i0";
+  for (unsigned int d = 1; d < ndim; ++d) oss << ",i" << d;
+  return oss.str();
+}
+
+inline bool is_tot_annotation(const std::string& annotation) {
+  return annotation.find(";") != std::string::npos;
 }
 
 inline auto split(const std::string& s, char delim) {
   std::stringstream ss(s);
   std::string buffer;
   std::vector<std::string> tokens;
-  while(std::getline(ss, buffer, delim))
+  while (std::getline(ss, buffer, delim))
     tokens.emplace_back(std::move(buffer));
   return tokens;
 }
 
-inline auto split_tot_index(const std::string& idx) {
-  auto temp = split(idx, ';');
-  TA_ASSERT(temp.size() == 2); // Must have outer and inner separation
+inline auto split_tot_annotation(const std::string& annotation) {
+  auto temp = split(annotation, ';');
+  TA_ASSERT(temp.size() == 2);  // Must have outer and inner separation
   return std::make_pair(split(temp[0], ','), split(temp[1], ','));
 }
 
-}
+}  // namespace TiledArray::detail
 
-#endif // TILEDARRAY_INDEX_PARSING_H__INCLUDED
+#endif  // TILEDARRAY_UTILS_ANNOTATION_H__INCLUDED
