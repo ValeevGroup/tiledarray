d1e4ec056fa7ff76ec9f035c71c84d9a152d9440
Eduard Valeyev <eduard@valeyev.net>
2020-05-28 16:15:38 -0400

    partially reverted 1025a3cdd44043558d15e419ac5fdd4089255a57

--- src/TiledArray/special/diagonal_array.h
+++ src/TiledArray/special/diagonal_array.h
@@ -233,7 +233,6 @@ Array diagonal_array(World &world, TiledRange const &trange, T val = 1) {
   if constexpr (is_dense_v<Policy>) {
     Array A(world, trange);
     detail::write_diag_tiles_to_array_val(A, val);
-    world.gop.fence();
     return A;
   } else {
     // Compute shape and init the Array
@@ -242,7 +241,6 @@ Array diagonal_array(World &world, TiledRange const &trange, T val = 1) {
     ShapeType shape(shape_norm, trange);
     Array A(world, trange, shape);
     detail::write_diag_tiles_to_array_val(A, val);
-    world.gop.fence();
     return A;
   }
 }
@@ -276,7 +274,6 @@ diagonal_array(World &world, TiledRange const &trange,
   if constexpr (is_dense_v<Policy>) {
     Array A(world, trange);
     detail::write_diag_tiles_to_array_rng(A, diagonals_begin);
-    world.gop.fence();
     return A;
   } else {
     // Compute shape and init the Array
@@ -286,7 +283,6 @@ diagonal_array(World &world, TiledRange const &trange,
     ShapeType shape(shape_norm, trange);
     Array A(world, trange, shape);
     detail::write_diag_tiles_to_array_rng(A, diagonals_begin);
-    world.gop.fence();
     return A;
   }
 }
