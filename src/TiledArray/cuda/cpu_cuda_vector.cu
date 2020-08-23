3a4b3e50eb5f081d7b2595675d11b63c1c572951
Eduard Valeyev <eduard@valeyev.net>
2020-08-23 08:23:39 -0400

    generate missing cub fn instantiations for CUDA 11 also

--- src/TiledArray/cuda/cpu_cuda_vector.cu
+++ src/TiledArray/cuda/cpu_cuda_vector.cu
@@ -22,8 +22,10 @@ template class cpu_cuda_vector<double>;
 template class cpu_cuda_vector<float>;
 }
 
-// Thrust included in CUDA 9 seems to generate uninstantiated CUB calls
-#if __CUDACC_VER_MAJOR__ == 10
+// Thrust included in CUDA 9+ seems to generate uninstantiated CUB calls
+#if __CUDACC_VER_MAJOR__  >= 9
+
+// cuda_cub::copy
 template <typename Real> auto force_missing_copy_instantiations() {
   return thrust::cuda_cub::copy<thrust::cuda_cub::tag, thrust::detail::normal_iterator<thrust::device_ptr<Real const> >, thrust::device_ptr<Real> >;
 }
@@ -37,44 +39,18 @@ auto force_missing_copy_instantiations_unsigned_long() {
   return force_missing_copy_instantiations<unsigned long>();
 }
 
+// cuda_cub::copy_n
+#if __CUDACC_VER_MAJOR__ >= 10  // CUDA 10+
 template <typename Input, typename Output>
 auto force_missing_copy_n_instantiations() {
   return thrust::cuda_cub::copy_n<thrust::cuda_cub::tag, thrust::pointer<Input, thrust::cuda_cub::tag>, long, thrust::device_ptr<Output> >;
 }
-
-auto force_missing_copy_n_instantiations_float_double(){
-  return force_missing_copy_n_instantiations<float, double>();
-}
-
-auto force_missing_copy_n_instantiations_double_float(){
-  return force_missing_copy_n_instantiations<double, float>();
-}
-
-auto force_missing_copy_n_instantiations_long_long(){
-  return force_missing_copy_n_instantiations<unsigned long, unsigned long>();
-}
-
-#elif __CUDACC_VER_MAJOR__ == 9
-
-
-template <typename Real> auto force_missing_copy_instantiations() {
-  return thrust::cuda_cub::copy<thrust::cuda_cub::tag, thrust::detail::normal_iterator<thrust::device_ptr<Real const> >, thrust::device_ptr<Real> >;
-}
-auto force_missing_copy_instantiations_double() {
-  return force_missing_copy_instantiations<double>();
-}
-auto force_missing_copy_instantiations_float() {
-  return force_missing_copy_instantiations<float>();
-}
-auto force_missing_copy_instantiations_unsigned_long() {
-  return force_missing_copy_instantiations<unsigned long>();
-}
-
-
+#elif __CUDACC_VER_MAJOR__ == 9  // CUDA 9
 template <typename Input, typename Output>
 auto force_missing_copy_n_instantiations() {
   return thrust::cuda_cub::copy_n<thrust::cuda_cub::tag, Input*, long, thrust::device_ptr<Output> >;
 }
+#endif
 
 auto force_missing_copy_n_instantiations_float_double(){
   return force_missing_copy_n_instantiations<float, double>();
@@ -87,4 +63,6 @@ auto force_missing_copy_n_instantiations_double_float(){
 auto force_missing_copy_n_instantiations_long_long(){
   return force_missing_copy_n_instantiations<unsigned long, unsigned long>();
 }
-#endif
+
+#endif  // __CUDACC_VER_MAJOR__  >= 9
+
