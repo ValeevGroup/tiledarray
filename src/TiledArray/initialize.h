524579af125ef952a529c2a7d02b3c586e6e583e
asadchev <asadchev@gmail.com>
2020-12-18 13:26:09 -0500

    Move initialize code to TiledArray/tiledarray.cpp

--- src/TiledArray/initialize.h
+++ src/TiledArray/initialize.h
@@ -6,68 +6,16 @@
 #define TILEDARRAY_INITIALIZE_H__INCLUDED
 
 #include <TiledArray/config.h>
-
 #include <TiledArray/external/madness.h>
-#ifdef TILEDARRAY_HAS_CUDA
-#include <TiledArray/external/cuda.h>
-#include <TiledArray/cuda/cublas.h>
-#include <cutt.h>
-#endif
-#ifdef HAVE_INTEL_MKL
-#include <mkl.h>
-#endif
 
 namespace TiledArray {
 
-#ifdef TILEDARRAY_HAS_CUDA
-/// initialize cuda environment
-inline void cuda_initialize() {
-  /// initialize cudaGlobal
-  cudaEnv::instance();
-  //
-  cuBLASHandlePool::handle();
-  // initialize cuTT
-  cuttInitialize();
-}
-
-/// finalize cuda environment
-inline void cuda_finalize() {
-  CudaSafeCall(cudaDeviceSynchronize());
-  cuttFinalize();
-  cublasDestroy(cuBLASHandlePool::handle());
-  delete &cuBLASHandlePool::handle();
-  cudaEnv::instance().reset(nullptr);
-}
-#endif
-
-namespace detail {
-inline bool& initialized_madworld_accessor() {
-  static bool flag = false;
-  return flag;
-}
-inline bool initialized_madworld() { return initialized_madworld_accessor(); }
-inline bool& initialized_accessor() {
-  static bool flag = false;
-  return flag;
-}
-inline bool& finalized_accessor() {
-  static bool flag = false;
-  return flag;
-}
-#ifdef HAVE_INTEL_MKL
-inline int& mklnumthreads_accessor() {
-  static int value = -1;
-  return value;
-}
-#endif
-}  // namespace detail
-
 /// @return true if TiledArray (and, necessarily, MADWorld runtime) is in an
 /// initialized state
-inline bool initialized() { return detail::initialized_accessor(); }
+bool initialized();
 
 /// @return true if TiledArray has been finalized at least once
-inline bool finalized() { return detail::finalized_accessor(); }
+bool finalized();
 
 /// @name TiledArray initialization.
 ///       These functions initialize TiledArray and (if needed) MADWorld
@@ -81,40 +29,11 @@ inline bool finalized() { return detail::finalized_accessor(); }
 
 /// @throw TiledArray::Exception if TiledArray initialized MADWorld and
 /// TiledArray::finalize() had been called
-inline World& initialize(int& argc, char**& argv,
-                         const SafeMPI::Intracomm& comm, bool quiet = true) {
-  if (detail::initialized_madworld() && finalized())
-    throw Exception(
-        "TiledArray finalized MADWorld already, cannot re-initialize MADWorld "
-        "again");
-  if (!initialized()) {
-    if (!madness::initialized())
-      detail::initialized_madworld_accessor() = true;
-    else {  // if MADWorld initialized, we must assume that comm is its default
-            // World.
-      if (madness::World::is_default(comm))
-        throw Exception(
-            "MADWorld initialized before TiledArray::initialize(argc, argv, "
-            "comm), but not initialized with comm");
-    }
-    auto& default_world = detail::initialized_madworld()
-                              ? madness::initialize(argc, argv, comm, quiet)
-                              : *madness::World::find_instance(comm);
-    TiledArray::set_default_world(default_world);
-#ifdef TILEDARRAY_HAS_CUDA
-    TiledArray::cuda_initialize();
-#endif
-#ifdef HAVE_INTEL_MKL
-    // record number of MKL threads and set to 1
-    detail::mklnumthreads_accessor() = mkl_get_max_threads();
-    mkl_set_num_threads(1);
-#endif
-    madness::print_meminfo_disable();
-    detail::initialized_accessor() = true;
-    return default_world;
-  } else
-    throw Exception("TiledArray already initialized");
-}
+World& initialize(
+  int& argc, char**& argv,
+  const SafeMPI::Intracomm& comm,
+  bool quiet = true
+);
 
 inline World& initialize(int& argc, char**& argv, bool quiet = true) {
   return TiledArray::initialize(argc, argv, SafeMPI::COMM_WORLD, quiet);
@@ -129,23 +48,7 @@ inline World& initialize(int& argc, char**& argv, const MPI_Comm& comm,
 
 /// Finalizes TiledArray (and MADWorld runtime, if it had not been initialized
 /// when TiledArray::initialize was called).
-inline void finalize() {
-#ifdef HAVE_INTEL_MKL
-  // reset number of MKL threads
-  mkl_set_num_threads(detail::mklnumthreads_accessor());
-#endif
-#ifdef TILEDARRAY_HAS_CUDA
-  TiledArray::cuda_finalize();
-#endif
-  TiledArray::get_default_world()
-      .gop.fence();  // TODO remove when madness::finalize() fences
-  if (detail::initialized_madworld()) {
-    madness::finalize();
-  }
-  TiledArray::reset_default_world();
-  detail::initialized_accessor() = false;
-  detail::finalized_accessor() = true;
-}
+void finalize();
 
 }  // namespace TiledArray
 
