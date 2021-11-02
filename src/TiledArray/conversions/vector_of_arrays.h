1b4438372e7743ebe5276d8b705ea4b2157e9a38
kmp5VT <kmp5@vt.edu>
2021-11-02 15:10:21 -0400

    Working split contract code

--- src/TiledArray/conversions/vector_of_arrays.h
+++ src/TiledArray/conversions/vector_of_arrays.h
@@ -591,10 +591,12 @@ void split_contract_tilewise_fused_array(
 
   // calculate the number of elements in the 0th dimension are in this tile
   auto tile_range = fused_arrayL.trange().dim(0).tile(tile_idxL);
+  auto tile_rangeR = fused_arrayR.trange().dim(0).tile(tile_idxR);
+
+  auto tile_size = tile_range.second - tile_range.first;
   // make sure that the size of the tile in the left array is the same as in
   // the right array.
-  TA_ASSERT(tile_range == fused_arrayR.trange().dim(0).tile(tile_idxR));
-  auto tile_size = tile_range.second - tile_range.first;
+  TA_ASSERT(tile_size == (tile_rangeR.second - tile_rangeR.first));
   std::size_t split_ntilesL = split_trangeL.tiles_range().volume(),
               split_ntilesR = split_trangeR.tiles_range().volume();
   auto& shapeL = fused_arrayL.shape(),
@@ -605,8 +607,8 @@ void split_contract_tilewise_fused_array(
       [&local_world = static_cast<const madness::World&>(local_world),
        &tile_range](const TA::DistArray<Tile, Policy>& fused_array,
                     TA::DistArray<Tile, Policy>& split_array,
-                                  std::size_t tile_idx,
-                                  std::size_t split_ntiles){
+                    std::size_t tile_idx, std::size_t split_ntiles,
+                    std::size_t tile_count){
         /// copy the data from tile
         auto make_tile = [](const TA::Range& range, const Tile& fused_tile,
                             const size_t i_offset_in_tile) {
@@ -618,16 +620,9 @@ void split_contract_tilewise_fused_array(
     for (std::size_t index : *split_array.pmap()) {
       std::size_t fused_array_index = tile_idx * split_ntiles + index;
       if (!fused_array.is_zero(fused_array_index)) {
-        auto tile = fused_array.find(fused_array_index);
-        auto range = split_array.trange().make_tile_range(index);
-        std::cout << tile.get().size() << std::endl;
-        std::cout << tile.get().range() << std::endl;
-        std::cout << split_array.trange().make_tile_range(index) << std::endl;
-        for (std::size_t i = tile_range.first, tile_count = 0;
-             i < tile_range.second; ++i, ++tile_count) {
-          split_array.set(index, local_world.taskq.add(
-                               make_tile, range, tile, tile_count));
-        }
+        split_array.set(
+            index, local_world.taskq.add(make_tile, split_array.trange().make_tile_range(index),
+                                         fused_array.find(fused_array_index), tile_count));
       }
     }
     return;
@@ -637,7 +632,8 @@ void split_contract_tilewise_fused_array(
   // and then contract them
   auto split_arrays_size = split_arrays.size();
   auto ptr_split_arrays_end = (split_arrays.begin() + split_arrays_size);
-  for (size_t i = tile_range.first; i < tile_range.second; ++i, ++ ptr_split_arrays_end) {
+  for (size_t i = tile_range.first, tile_count = 0; i < tile_range.second;
+       ++i, ++ ptr_split_arrays_end, ++tile_count) {
     // make split_array shapes
     auto split_shapeL = detail::tilewise_slice_of_fused_shape(
         split_trangeL, shapeL, tile_idxL, split_ntilesL, tile_size),
@@ -648,12 +644,15 @@ void split_contract_tilewise_fused_array(
     TA::DistArray<Tile, Policy> split_arrayL(local_world, split_trangeL
                                              ,split_shapeL),
         split_arrayR(local_world, split_trangeR, split_shapeR),
-        contraced_array;
-    std::cout << split_arrayL.trange() << std::endl;
-    construct_split_array(fused_arrayL, split_arrayL, tile_idxL, split_ntilesL);
-    construct_split_array(fused_arrayR, split_arrayR, tile_idxR, split_ntilesR);
-    contraced_array(contract_vars_final) = split_arrayL(contract_vars_L) * split_arrayR(contract_vars_R);
-    split_arrays.insert(ptr_split_arrays_end, contraced_array);
+        mult_array;
+
+    // construct the split arrays with the correct data
+    construct_split_array(fused_arrayL, split_arrayL, tile_idxL, split_ntilesL, tile_count);
+    construct_split_array(fused_arrayR, split_arrayR, tile_idxR, split_ntilesR, tile_count);
+
+    // contract the arrays of the tiles and insert them into the split arrays
+    mult_array(contract_vars_final) = split_arrayL(contract_vars_L) * split_arrayR(contract_vars_R);
+    split_arrays.insert(ptr_split_arrays_end, mult_array);
   }
 
   return;
