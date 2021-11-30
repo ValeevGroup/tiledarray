1e4a941440fd25f0604165323920d39eb45ac0a5
Eduard Valeyev <eduard@valeyev.net>
2021-11-30 12:09:39 -0500

    btas::make_ti accepts tensors with non-TA ranges

--- src/TiledArray/external/btas.h
+++ src/TiledArray/external/btas.h
@@ -109,18 +109,18 @@ inline bool is_congruent(const btas::RangeNd<Order, Args...>& r1,
                     r2.extent_data());
 }
 
-template <typename T, typename Storage>
-decltype(auto) make_ti(const btas::Tensor<T, TiledArray::Range, Storage>& arg) {
-  return TiledArray::detail::TensorInterface<
-      const T, TiledArray::Range, btas::Tensor<T, TiledArray::Range, Storage>>(
-      arg.range(), arg.data());
+template <typename T, typename Range, typename Storage>
+decltype(auto) make_ti(const btas::Tensor<T, Range, Storage>& arg) {
+  return TiledArray::detail::TensorInterface<const T, TiledArray::Range,
+                                             btas::Tensor<T, Range, Storage>>(
+      TiledArray::detail::make_ta_range(arg.range()), arg.data());
 }
 
-template <typename T, typename Storage>
-decltype(auto) make_ti(btas::Tensor<T, TiledArray::Range, Storage>& arg) {
-  return TiledArray::detail::TensorInterface<
-      T, TiledArray::Range, btas::Tensor<T, TiledArray::Range, Storage>>(
-      arg.range(), arg.data());
+template <typename T, typename Range, typename Storage>
+decltype(auto) make_ti(btas::Tensor<T, Range, Storage>& arg) {
+  return TiledArray::detail::TensorInterface<T, TiledArray::Range,
+                                             btas::Tensor<T, Range, Storage>>(
+      TiledArray::detail::make_ta_range(arg.range()), arg.data());
 }
 
 template <typename... Args>
