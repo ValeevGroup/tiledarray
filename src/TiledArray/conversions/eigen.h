1d10edf7d34cfae48f49792c8159f5e54e47b676
Eduard Valeyev <eduard@valeyev.net>
2021-11-29 20:52:07 -0500

    can convert DistArray to/fro Eigen::Tensor

--- src/TiledArray/conversions/eigen.h
+++ src/TiledArray/conversions/eigen.h
@@ -685,6 +685,345 @@ inline A column_major_buffer_to_array(
       pmap);
 }
 
+///////////////// Eigen::Tensor conversions ////////////////////////////////////
+
+// clang-format off
+/// Copy a block of a Eigen::Tensor into a (row-major) TiledArray::Tensor
+
+/// A block of Eigen::Tensor \c src will be copied into TiledArray::Tensor \c
+/// dst. The block dimensions will be determined by the dimensions of the range
+/// of \c dst .
+/// \tparam T The tensor element type
+/// \tparam NumIndices_ The order of \p src
+/// \tparam Options_
+/// \tparam IndexType_
+/// \tparam Tensor_ A tensor type (e.g., TiledArray::Tensor or btas::Tensor,
+///         optionally wrapped into TiledArray::Tile)
+/// \param[in] src The source object; its subblock defined by the {lower,upper}
+///            bounds \c {dst.lobound(),dst.upbound()} will be copied to \c dst
+/// \param[out] dst The object that will contain the contents of the
+///             corresponding subblock of src
+/// \throw TiledArray::Exception When the dimensions of \c src and \c dst do not
+///        match.
+// clang-format on
+template <typename T, int NumIndices_, int Options_, typename IndexType_,
+          typename Tensor_>
+inline void eigen_subtensor_to_tensor(
+    const Eigen::Tensor<T, NumIndices_, Options_, IndexType_>& src,
+    Tensor_& dst) {
+  TA_ASSERT(dst.range().rank() == NumIndices_);
+
+  auto to_array = [](const auto& seq) {
+    TA_ASSERT(seq.size() == NumIndices_);
+    std::array<IndexType_, NumIndices_> result;
+    std::copy(seq.begin(), seq.end(), result.begin());
+    return result;
+  };
+
+  auto reverse_extent_indices = []() {
+    std::array<IndexType_, NumIndices_> result;
+    std::iota(result.rbegin(), result.rend(), 0);
+    return result;
+  };
+
+  const auto& dst_range = dst.range();
+  auto src_block =
+      src.slice(to_array(dst_range.lobound()), to_array(dst_range.extent()));
+  auto dst_eigen_map = Eigen::TensorMap<
+      Eigen::Tensor<T, NumIndices_, Eigen::RowMajor, IndexType_>>(
+      dst.data(), to_array(dst_range.extent()));
+  if constexpr (std::decay_t<decltype(src)>::Layout == Eigen::ColMajor)
+    dst_eigen_map = src_block.swap_layout().shuffle(reverse_extent_indices());
+  else
+    dst_eigen_map = src_block;
+}
+
+// clang-format off
+/// Copy a (row-major) TiledArray::Tensor into a block of a Eigen::Tensor
+
+/// TiledArray::Tensor \c src will be copied into a block of Eigen::Tensor
+/// \c dst. The block dimensions will be determined by
+/// the dimensions of the range of \c src .
+/// \tparam Tensor_ A tensor type (e.g., TiledArray::Tensor or btas::Tensor,
+///         optionally wrapped into TiledArray::Tile)
+/// \tparam T The tensor element type
+/// \tparam NumIndices_ The order of \p dst
+/// \tparam Options_
+/// \tparam IndexType_
+/// \param[in] src The source object whose contents will be copied into
+///            a subblock of \c dst
+/// \param[out] dst The destination object; its subblock defined by the
+///             {lower,upper} bounds \c {src.lobound(),src.upbound()} will be
+///             overwritten with the content of \c src
+/// \throw TiledArray::Exception When the dimensions
+///        of \c src and \c dst do not match.
+// clang-format on
+template <typename Tensor_, typename T, int NumIndices_, int Options_,
+          typename IndexType_>
+inline void tensor_to_eigen_subtensor(
+    const Tensor_& src,
+    Eigen::Tensor<T, NumIndices_, Options_, IndexType_>& dst) {
+  TA_ASSERT(src.range().rank() == NumIndices_);
+
+  auto to_array = [](const auto& seq) {
+    TA_ASSERT(seq.size() == NumIndices_);
+    std::array<IndexType_, NumIndices_> result;
+    std::copy(seq.begin(), seq.end(), result.begin());
+    return result;
+  };
+
+  auto reverse_extent_indices = []() {
+    std::array<IndexType_, NumIndices_> result;
+    std::iota(result.rbegin(), result.rend(), 0);
+    return result;
+  };
+
+  const auto& src_range = src.range();
+  auto dst_block =
+      dst.slice(to_array(src_range.lobound()), to_array(src_range.extent()));
+  auto src_eigen_map = Eigen::TensorMap<
+      const Eigen::Tensor<T, NumIndices_, Eigen::RowMajor, IndexType_>>(
+      src.data(), to_array(src_range.extent()));
+  if constexpr (std::decay_t<decltype(dst)>::Layout == Eigen::ColMajor)
+    dst_block = src_eigen_map.swap_layout().shuffle(reverse_extent_indices());
+  else
+    dst_block = src_eigen_map;
+}
+
+namespace detail {
+
+/// Task function for converting Eigen::Tensor subblock to a
+/// TiledArray::DistArray
+
+/// \tparam DistArray_ a TiledArray::DistArray type
+/// \tparam Eigen_Tensor_ an Eigen::Tensor type
+/// \param src The btas::Tensor object whose block will be copied
+/// \param dst The array that will hold the result
+/// \param i The index of the tile to be copied
+/// \param counter The task counter
+/// \internal OK to use bare ptrs as args as long as the user blocks on the
+/// counter.
+template <typename DistArray_, typename Eigen_Tensor_>
+void counted_eigen_subtensor_to_tensor(const Eigen_Tensor_* src,
+                                       DistArray_* dst,
+                                       const typename Range::index_type i,
+                                       madness::AtomicInt* counter) {
+  typename DistArray_::value_type tensor(dst->trange().make_tile_range(i));
+  eigen_subtensor_to_tensor(*src, tensor);
+  dst->set(i, tensor);
+  (*counter)++;
+}
+
+/// Task function for assigning a tensor to an Eigen subtensor
+
+/// \tparam Tensor_ a TiledArray::Tensor type
+/// \tparam Eigen_Tensor_ an Eigen::Tensor type
+/// \param src The source tensor
+/// \param dst The destination tensor
+/// \param counter The task counter
+template <typename TA_Tensor_, typename Eigen_Tensor_>
+void counted_tensor_to_eigen_subtensor(const TA_Tensor_& src,
+                                       Eigen_Tensor_* dst,
+                                       madness::AtomicInt* counter) {
+  tensor_to_eigen_subtensor(src, *dst);
+  (*counter)++;
+}
+
+template <bool sparse>
+auto make_ta_shape(World& world, const TiledArray::TiledRange& trange);
+
+template <>
+inline auto make_ta_shape<true>(World& world,
+                                const TiledArray::TiledRange& trange) {
+  TiledArray::Tensor<float> tile_norms(trange.tiles_range(),
+                                       std::numeric_limits<float>::max());
+  return TiledArray::SparseShape<float>(world, tile_norms, trange);
+}
+
+template <>
+inline auto make_ta_shape<false>(World&, const TiledArray::TiledRange&) {
+  return TiledArray::DenseShape{};
+}
+
+}  // namespace detail
+
+/// Convert a Eigen::Tensor object into a TiledArray::DistArray object
+
+/// This function will copy the contents of \c src into a \c DistArray_ object
+/// that is tiled according to the \c trange object. If the \c DistArray_ object
+/// has sparse policy, a sparse map with large norm is created to ensure all the
+/// values from \c src copy to the \c DistArray_ object. The copy operation is
+/// done in parallel, and this function will block until all elements of
+/// \c src have been copied into the result array tiles.
+/// Each tile is created
+/// using the local contents of \c src, hence
+/// it is your responsibility to ensure that the data in \c src
+/// is distributed correctly among the ranks. If in doubt, you should replicate
+/// \c src among the ranks prior to calling this.
+///
+/// Upon completion,
+/// if the \c DistArray_ object has sparse policy truncate() is called.\n
+/// Usage:
+/// \code
+/// Eigen::Tensor<double, 3> src(100, 100, 100);
+/// // Fill src with data ...
+///
+/// // Create a range for the new array object
+/// std::vector<std::size_t> blocks;
+/// for(std::size_t i = 0ul; i <= 100ul; i += 10ul)
+///   blocks.push_back(i);
+/// std::array<TiledArray::TiledRange1, 3> blocks3 =
+///     {{ TiledArray::TiledRange1(blocks.begin(), blocks.end()),
+///        TiledArray::TiledRange1(blocks.begin(), blocks.end()),
+///        TiledArray::TiledRange1(blocks.begin(), blocks.end()) }};
+/// TiledArray::TiledRange trange(blocks3.begin(), blocks3.end());
+///
+/// // Create an Array from the source btas::Tensor object
+/// TiledArray::TArrayD array =
+///     eigen_tensor_to_array<decltype(array)>(world, trange, src);
+/// \endcode
+/// \tparam DistArray_ a TiledArray::DistArray type
+/// \tparam NumIndices_ The order of \p dst
+/// \tparam Options_
+/// \tparam IndexType_
+/// \param[in,out] world The world where the result array will live
+/// \param[in] trange The tiled range of the new array
+/// \param[in] src The Eigen::Tensor object whose contents will be
+/// copied to the result.
+/// \param replicated if true, the result will be replicated
+///        [default = false].
+/// \param pmap the process map object [default=null]; initialized to the
+/// default if \p replicated is false, or a replicated pmap if \p replicated
+/// is true; ignored if \p replicated is true and \c world.size()>1
+/// \return A \c DistArray_ object that is a copy of \c src
+/// \throw TiledArray::Exception When world size is greater than 1
+/// \note If using 2 or more World ranks, set \c replicated=true and make sure
+/// \c matrix is the same on each rank!
+template <typename DistArray_, typename T, int NumIndices_, int Options_,
+          typename IndexType_>
+DistArray_ eigen_tensor_to_array(
+    World& world, const TiledArray::TiledRange& trange,
+    const Eigen::Tensor<T, NumIndices_, Options_, IndexType_>& src,
+    bool replicated = false,
+    std::shared_ptr<typename DistArray_::pmap_interface> pmap = {}) {
+  // Test preconditions
+  const auto rank = trange.tiles_range().rank();
+  TA_ASSERT(rank == NumIndices_ &&
+            "TiledArray::eigen_tensor_to_array(): rank of destination "
+            "trange does not match the rank of source Eigen tensor.");
+  auto dst_range_extents = trange.elements_range().extent();
+  for (std::remove_const_t<decltype(rank)> d = 0; d != rank; ++d) {
+    TA_ASSERT(dst_range_extents[d] == src.dimension(d) &&
+              "TiledArray::eigen_tensor_to_array(): source dimension does "
+              "not match destination dimension.");
+  }
+
+  using Tensor_ = Eigen::Tensor<T, NumIndices_, Options_, IndexType_>;
+  using Policy_ = typename DistArray_::policy_type;
+  const auto is_sparse = !is_dense_v<Policy_>;
+
+  // Make a shape, only used if making a sparse array
+  using Shape_ = typename DistArray_::shape_type;
+  Shape_ shape = detail::make_ta_shape<is_sparse>(world, trange);
+
+  // Create a new tensor
+  if (replicated && (world.size() > 1))
+    pmap = std::static_pointer_cast<typename DistArray_::pmap_interface>(
+        std::make_shared<detail::ReplicatedPmap>(
+            world, trange.tiles_range().volume()));
+  DistArray_ array = (pmap ? DistArray_(world, trange, shape, pmap)
+                           : DistArray_(world, trange, shape));
+
+  // Spawn copy tasks
+  madness::AtomicInt counter;
+  counter = 0;
+  std::int64_t n = 0;
+  for (auto&& acc : array) {
+    world.taskq.add(
+        &detail::counted_eigen_subtensor_to_tensor<DistArray_, Tensor_>, &src,
+        &array, acc.index(), &counter);
+    ++n;
+  }
+
+  // Wait until the write tasks are complete
+  array.world().await([&counter, n]() { return counter == n; });
+
+  // Analyze tiles norms and truncate based on sparse policy
+  if (is_sparse) truncate(array);
+
+  return array;
+}
+
+/// Convert a TiledArray::DistArray object into a Eigen::Tensor object
+
+/// This function will copy the contents of \c src into a \c Eigen::Tensor
+/// object. The copy operation is done in parallel, and this function will block
+/// until all elements of \c src have been copied into the result array tiles.
+/// The size of \c src.world().size() must be equal to 1 or \c src must be a
+/// replicated TiledArray::DistArray. Usage:
+/// \code
+/// TiledArray::TArrayD
+/// array(world, trange);
+/// // Set tiles of array ...
+///
+/// auto t = array_to_eigen_tensor(array);
+/// \endcode
+/// \tparam Tile the tile type of \c src
+/// \tparam Policy the policy type of \c src
+/// \param[in] src The TiledArray::DistArray<Tile,Policy> object whose contents
+/// will be copied to the result.
+/// \return A \c Eigen::Tensor object that is a copy of \c src
+/// \throw TiledArray::Exception When world size is greater than
+///        1 and \c src is not replicated
+/// \param[in] target_rank the rank on which to create the Eigen:Tensor
+///            containing the data of \c src ; if \c target_rank=-1 then
+///            create the Eigen::Tensor on every rank (this requires
+///            that \c src.is_replicated()==true )
+/// \return Eigen::Tensor object containing the data of \c src , if my rank
+/// equals
+///         \c target_rank or \c target_rank==-1 ,
+///         default-initialized Eigen::Tensor otherwise.
+template <typename Tensor, typename Tile, typename Policy>
+Tensor array_to_eigen_tensor(const TiledArray::DistArray<Tile, Policy>& src,
+                             int target_rank = -1) {
+  // Test preconditions
+  if (target_rank == -1 && src.world().size() > 1 &&
+      !src.pmap()->is_replicated())
+    TA_ASSERT(
+        src.world().size() == 1 &&
+        "TiledArray::array_to_eigen_tensor(): a non-replicated array can only "
+        "be converted to a Eigen::Tensor on every rank if the number of World "
+        "ranks is 1.");
+
+  using result_type = Tensor;
+
+  // Construct the result
+  if (target_rank == -1 || src.world().rank() == target_rank) {
+    // if array is sparse must initialize to zero
+    result_type result(src.trange().elements_range().extent());
+    result.setZero();
+
+    // Spawn tasks to copy array tiles to btas::Tensor
+    madness::AtomicInt counter;
+    counter = 0;
+    int n = 0;
+    for (std::size_t i = 0; i < src.size(); ++i) {
+      if (!src.is_zero(i)) {
+        src.world().taskq.add(
+            &detail::counted_tensor_to_eigen_subtensor<Tile, result_type>,
+            src.find(i), &result, &counter);
+        ++n;
+      }
+    }
+
+    // Wait until the write tasks are complete
+    src.world().await([&counter, n]() { return counter == n; });
+
+    return result;
+  } else  // else
+    return result_type{};
+}
+
 }  // namespace TiledArray
 
 #endif  // TILEDARRAY_CONVERSIONS_EIGEN_H__INCLUDED
--- tests/eigen.cpp
+++ tests/eigen.cpp
@@ -28,23 +28,29 @@ struct EigenFixture : public TiledRangeFixture {
   EigenFixture()
       : trange(dims.begin(), dims.begin() + 2),
         trange1(dims.begin(), dims.begin() + 1),
+        trangeN(dims.begin(), dims.begin() + GlobalFixture::dim),
         array(*GlobalFixture::world, trange),
         array1(*GlobalFixture::world, trange1),
+        arrayN(*GlobalFixture::world, trangeN),
         matrix(dims[0].elements_range().second,
                dims[1].elements_range().second),
         rmatrix(dims[0].elements_range().second,
                 dims[1].elements_range().second),
         vector(dims[0].elements_range().second),
-        tensor(extents) {}
+        tensor(extents),
+        rtensor(extents) {}
 
   TiledRange trange;
   TiledRange trange1;
+  TiledRange trangeN;
   TArrayI array;
   TArrayI array1;
+  TArrayI arrayN;
   Eigen::MatrixXi matrix;
   EigenMatrixXi rmatrix;
   Eigen::VectorXi vector;
   Eigen::Tensor<int, GlobalFixture::dim> tensor;
+  Eigen::Tensor<int, GlobalFixture::dim, Eigen::RowMajor> rtensor;
 };
 
 BOOST_FIXTURE_TEST_SUITE(eigen_suite, EigenFixture)
@@ -194,21 +200,6 @@ BOOST_AUTO_TEST_CASE(vector_to_array) {
          tile_it != tile.get().range().end(); ++tile_it) {
       BOOST_CHECK_EQUAL(tile.get()[*tile_it], vector((*tile_it)[0]));
     }
-  }
-}
-
-BOOST_AUTO_TEST_CASE(tensor_to_array) {
-  // Fill tensor with random data and replicate across the world
-  tensor.setRandom();
-  GlobalFixture::world->gop.broadcast_serializable(tensor, 0);
-
-  // test serialization if have more than 1 rank
-  if (GlobalFixture::world->size() > 1) {
-    decltype(tensor) tensor_copy;
-    if (GlobalFixture::world->rank() == 1) tensor_copy = tensor;
-    GlobalFixture::world->gop.broadcast_serializable(tensor_copy, 1);
-    Eigen::Tensor<bool, 0> eq = (tensor == tensor_copy).all();
-    BOOST_CHECK(eq() == true);
   }
 }
 
@@ -377,6 +368,144 @@ BOOST_AUTO_TEST_CASE(array_to_vector) {
            tile_it != tile.get().range().end(); ++tile_it) {
         BOOST_CHECK_EQUAL(vector((*tile_it)[0]), tile.get()[*tile_it]);
       }
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(subtensor_to_tensor) {
+  // Fill the tensor with random data
+  tensor.setRandom();
+  // Make a target tensor
+  Tensor<int> ta_tensor(trangeN.make_tile_range(0));
+
+  // Copy the sub matrix to the tensor objects
+  BOOST_CHECK_NO_THROW(eigen_subtensor_to_tensor(tensor, ta_tensor));
+
+  // Check that the block contains the same values as the tensor
+  for (Range::const_iterator it = ta_tensor.range().begin();
+       it != ta_tensor.range().end(); ++it) {
+    const auto& ta_idx = (*it);
+    std::array<long, GlobalFixture::dim> idx;
+    std::copy(ta_idx.begin(), ta_idx.end(), idx.begin());
+    BOOST_CHECK_EQUAL(ta_tensor[*it], tensor(idx));
+  }
+}
+
+BOOST_AUTO_TEST_CASE(tensor_to_subtensor) {
+  // Fill a tensor with data
+  Tensor<int> ta_tensor(trangeN.make_tile_range(0));
+  GlobalFixture::world->srand(27);
+  for (auto it = ta_tensor.begin(); it != ta_tensor.end(); ++it)
+    *it = GlobalFixture::world->rand();
+
+  // Copy the tensor to the submatrix block
+  BOOST_CHECK_NO_THROW(tensor_to_eigen_subtensor(ta_tensor, tensor));
+
+  // Check that the block contains the same values as the tensor
+  for (auto it = ta_tensor.range().begin(); it != ta_tensor.range().end();
+       ++it) {
+    const auto& ta_idx = (*it);
+    std::array<long, GlobalFixture::dim> idx;
+    std::copy(ta_idx.begin(), ta_idx.end(), idx.begin());
+    BOOST_CHECK_EQUAL(tensor(idx), ta_tensor[*it]);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(tensor_to_array) {
+  // Fill tensor with random data and replicate across the world
+  tensor.setRandom();
+  GlobalFixture::world->gop.broadcast_serializable(tensor, 0);
+
+  // test serialization if have more than 1 rank
+  if (GlobalFixture::world->size() > 1) {
+    decltype(tensor) tensor_copy;
+    if (GlobalFixture::world->rank() == 1) tensor_copy = tensor;
+    GlobalFixture::world->gop.broadcast_serializable(tensor_copy, 1);
+    Eigen::Tensor<bool, 0> eq = (tensor == tensor_copy).all();
+    BOOST_CHECK(eq() == true);
+  }
+
+  // Copy matrix to array
+  BOOST_CHECK_NO_THROW((array = eigen_tensor_to_array<TArrayI>(
+                            *GlobalFixture::world, trangeN, tensor)));
+
+  // Check that the data in array is equal to that in matrix
+  for (Range::const_iterator it = array.range().begin();
+       it != array.range().end(); ++it) {
+    Future<TArrayI::value_type> tile = array.find(*it);
+    for (Range::const_iterator tile_it = tile.get().range().begin();
+         tile_it != tile.get().range().end(); ++tile_it) {
+      std::array<long, GlobalFixture::dim> idx;
+      auto& t_idx = *tile_it;
+      std::copy(t_idx.begin(), t_idx.end(), idx.begin());
+      BOOST_CHECK_EQUAL(tile.get()[*tile_it], tensor(idx));
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(array_to_tensor) {
+  using Tensor = Eigen::Tensor<int, GlobalFixture::dim>;
+  using RowTensor = Eigen::Tensor<int, GlobalFixture::dim, Eigen::RowMajor>;
+  auto a_to_e_rowmajor = [](const TArrayI& array) -> RowTensor {
+    return array_to_eigen_tensor<RowTensor>(array);
+  };
+
+  auto to_array = [](const auto& seq) {
+    TA_ASSERT(seq.size() == GlobalFixture::dim);
+    std::array<long, GlobalFixture::dim> result;
+    std::copy(seq.begin(), seq.end(), result.begin());
+    return result;
+  };
+
+  // Fill local tiles with data
+  GlobalFixture::world->srand(27);
+  TArrayI::pmap_interface::const_iterator it = arrayN.pmap()->begin();
+  TArrayI::pmap_interface::const_iterator end = arrayN.pmap()->end();
+  for (; it != end; ++it) {
+    TArrayI::value_type tile(arrayN.trange().make_tile_range(*it));
+    for (TArrayI::value_type::iterator tile_it = tile.begin();
+         tile_it != tile.end(); ++tile_it) {
+      *tile_it = GlobalFixture::world->rand();
+    }
+    arrayN.set(*it, tile);
+  }
+
+  if (GlobalFixture::world->size() > 1) {
+    // Check that array_to_eigen_tensor throws when there is more than one node
+    BOOST_CHECK_THROW(array_to_eigen_tensor<Tensor>(arrayN),
+                      TiledArray::Exception);
+  }
+
+  // Distribute the data of arrayN to all nodes
+  if (GlobalFixture::world->size() > 1) {
+    arrayN.make_replicated();
+    BOOST_CHECK(arrayN.pmap()->is_replicated());
+  }
+
+  // Convert the array to an Eigen matrix
+  BOOST_CHECK_NO_THROW(tensor = array_to_eigen_tensor<Tensor>(arrayN));
+  BOOST_CHECK_NO_THROW(rtensor = a_to_e_rowmajor(arrayN));
+
+  // Check that the matrix dimensions are the same as the array
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      tensor.dimensions().begin(), tensor.dimensions().end(),
+      arrayN.trange().elements_range().extent().begin(),
+      arrayN.trange().elements_range().extent().end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      rtensor.dimensions().begin(), rtensor.dimensions().end(),
+      arrayN.trange().elements_range().extent().begin(),
+      arrayN.trange().elements_range().extent().end());
+
+  // Check that the data in vector matches the data in array
+  for (Range::const_iterator it = arrayN.range().begin();
+       it != arrayN.range().end(); ++it) {
+    BOOST_CHECK(arrayN.is_local(*it));
+
+    Future<TArrayI::value_type> tile = arrayN.find(*it);
+    for (Range::const_iterator tile_it = tile.get().range().begin();
+         tile_it != tile.get().range().end(); ++tile_it) {
+      BOOST_CHECK_EQUAL(tensor(to_array(*tile_it)), tile.get()[*tile_it]);
+      BOOST_CHECK_EQUAL(rtensor(to_array(*tile_it)), tile.get()[*tile_it]);
     }
   }
 }
