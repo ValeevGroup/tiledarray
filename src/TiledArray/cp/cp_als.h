de3ea9ad0e1c4168b68541253b75cc68987be623
kmp5VT <kmp5@vt.edu>
2022-04-19 14:41:47 -0400

    Add doxs and start working on ALS

--- src/TiledArray/cp/cp_als.h
+++ src/TiledArray/cp/cp_als.h
@@ -7,29 +7,61 @@
 
 #include <TiledArray/cp/cp.h>
 
+/**
+ * This is a canonical polyadic (CP) optimization class which
+ * takes a reference order-N tensor and decomposes it into a
+ * set of order-2 tensors all coupled by a hyperdimension called the rank.
+ * These factors are optimized using an alternating least squares
+ * algorithm. This class is derived form the base CP class
+ *
+ * @tparam Tile typing for the DistArray tiles
+ * @tparam Policy policy of the DistArray
+**/
 namespace TiledArray::cp {
 template <typename Tile, typename Policy>
 class CP_ALS : public CP<Tile, Policy>{
  public:
   using CP<Tile, Policy>::ndim;
   using CP<Tile, Policy>::cp_factors;
+
+  /// Default CP_ALS constructor
   CP_ALS() = default;
 
+  /// CP_ALS constructor function
+  /// takes, as a constant reference, the tensor to be decomposed
+  /// \param[in] tref A constant reference to the tensor to be decomposed.
   CP_ALS(const DistArray<Tile, Policy> & tref) :
             CP<Tile, Policy>(rank(tref)), reference(tref), world(tref.world()){
+    for(size_t i = 0; i < ndim; ++i){
+      ref_indices += std::to_string(i);
+      if(i + 1 != ndim )
+        ref_indices += ",";
+    }
+    std::cout << ref_indices << std::endl;
   }
 
  protected:
   const DistArray<Tile, Policy> & reference;
   madness::World & world;
+  std::string ref_indices;
 
+  /// This function constructs the initial CP facotr matrices
+  /// stores them in CP::cp_factors vector.
+  /// In general the initial guess is constructed using quasi-random numbers
+  /// generated between [-1, 1]
+  /// \param[in] rank rank of the CP approximation
+  /// \param[in] rank_trange TiledRange1 of the rank dimension.
   void build_guess(const size_t rank,
                    const TiledRange1 rank_trange) override{
     if(cp_factors.size() == 0) {
       for (auto i = 0; i < ndim; ++i) {
-        cp_factors.emplace_back(this->construct_random_factor(
-            world, rank, reference.trange().tiles_range().extent_data()[i],
-            rank_trange, reference.trange().data()[i]));
+        auto factor = this->construct_random_factor(
+                              world, rank, reference.trange().elements_range().extent(i),
+                              rank_trange, reference.trange().data()[i]);
+        std::cout << factor << std::endl;
+        cp_factors.emplace_back(factor);
+        this->normCol(factor, rank);
+        std::cout << factor << std::endl;
       }
     } else{
       TA_EXCEPTION("Currently no implementation to increase or change rank");
@@ -68,20 +100,16 @@ class CP_ALS : public CP<Tile, Policy>{
   }
 
   void update_factor(size_t mode){
-    std::string ref, contract, final;
-    ref.reserve(ndim); contract.reserve(2); final.reserve(ndim);
-    auto ptr_ref = ref.data(), ptr_cont = contract.data(),
-         ptr_final = final.data();
-    *ptr_final = char(ndim); *ptr_cont = char(ndim);
-    size_t first_contr = (mode == ndim - 1 ? 0 : ndim - 1);
-    *(ptr_cont + 1) = char(first_contr);
-    for(size_t i = 0; i < ndim; ++i){
-      *(ptr_ref + i) = char(i);
-      if(i == first_contr) continue;
-      *(ptr_cont) = char(i);
-    }
+    auto first_contr = (mode == ndim - 1 ? 0 : ndim - 1);
+    std::string contract(char(ndim) + "," + char(first_contr)),
+        final = ref_indices;
+    auto start_erase = 2 * first_contr,
+         end_erase = start_erase + 1;
+    final.erase(start_erase, end_erase);
+    final = std::to_string(ndim) + "," + final;
+
     DistArray<Tile, Policy> An;
-    An(final) = this->reference(ref) * cp_factors[first_contr](contract);
+    An(final) = this->reference(ref_indices) * cp_factors[first_contr](contract);
 
     
   }
