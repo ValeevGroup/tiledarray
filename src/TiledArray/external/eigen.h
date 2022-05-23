c6372243cc2d8801a6dfb95efc3ae2d7b065628c
kmp5VT <kmp5@vt.edu>
2022-05-23 11:54:18 -0400

    Manually update external/eigen.h to match master

--- src/TiledArray/external/eigen.h
+++ src/TiledArray/external/eigen.h
@@ -1,27 +1,27 @@
 /*
- *  This file is a part of TiledArray.
- *  Copyright (C) 2019  Virginia Tech
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Edward Valeev
- *  Department of Chemistry, Virginia Tech
- *
- *  eigen.h
- *  Oct 1, 2019
- *
- */
+*  This file is a part of TiledArray.
+*  Copyright (C) 2019  Virginia Tech
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*  Edward Valeev
+*  Department of Chemistry, Virginia Tech
+*
+*  eigen.h
+*  Oct 1, 2019
+*
+*/
 
 #ifndef TILEDARRAY_EXTERNAL_EIGEN_H__INCLUDED
 #define TILEDARRAY_EXTERNAL_EIGEN_H__INCLUDED
@@ -46,13 +46,15 @@ TILEDARRAY_PRAGMA_GCC(system_header)
 #endif
 
 #include <Eigen/Core>
+#include <unsupported/Eigen/CXX11/Tensor>
 
 #if defined(EIGEN_USE_LAPACKE) || defined(EIGEN_USE_LAPACKE_STRICT)
 #if !EIGEN_VERSION_AT_LEAST(3, 3, 7)
 #error "Eigen3 < 3.3.7 with LAPACKE enabled may give wrong eigenvalue results"
-#error "Either turn off EIGEN_USE_LAPACKE/EIGEN_USE_LAPACKE_STRICT or use Eigen3 3.3.7"
+#error \
+   "Either turn off EIGEN_USE_LAPACKE/EIGEN_USE_LAPACKE_STRICT or use Eigen3 3.3.7"
 #endif
-#endif // EIGEN_USE_LAPACKE || EIGEN_USE_LAPACKE_STRICT
+#endif  // EIGEN_USE_LAPACKE || EIGEN_USE_LAPACKE_STRICT
 
 TILEDARRAY_PRAGMA_GCC(diagnostic pop)
 
@@ -60,40 +62,72 @@ namespace madness {
 namespace archive {
 
 template <class Archive, typename Scalar, int RowsAtCompileTime,
-          int ColsAtCompileTime, int Options, int MaxRowsAtCompileTime,
-          int MaxColsAtCompileTime>
+         int ColsAtCompileTime, int Options, int MaxRowsAtCompileTime,
+         int MaxColsAtCompileTime>
 struct ArchiveStoreImpl<
-    Archive,
-    Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
-                  MaxRowsAtCompileTime, MaxColsAtCompileTime>> {
-  static inline void store(
-      const Archive& ar,
-      const Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
-                          MaxRowsAtCompileTime, MaxColsAtCompileTime>& t) {
-    ar& t.rows() & t.cols();
-    if (t.size()) ar& madness::archive::wrap(t.data(), t.size());
-  }
+   Archive,
+   Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
+                 MaxRowsAtCompileTime, MaxColsAtCompileTime>> {
+ static inline void store(
+     const Archive& ar,
+     const Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
+                         MaxRowsAtCompileTime, MaxColsAtCompileTime>& t) {
+   ar& t.rows() & t.cols();
+   if (t.size()) ar& madness::archive::wrap(t.data(), t.size());
+ }
 };
 
 template <class Archive, typename Scalar, int RowsAtCompileTime,
-          int ColsAtCompileTime, int Options, int MaxRowsAtCompileTime,
-          int MaxColsAtCompileTime>
+         int ColsAtCompileTime, int Options, int MaxRowsAtCompileTime,
+         int MaxColsAtCompileTime>
 struct ArchiveLoadImpl<
-    Archive,
-    Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
-                  MaxRowsAtCompileTime, MaxColsAtCompileTime>> {
-  static inline void load(
-      const Archive& ar,
-      Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
-                    MaxRowsAtCompileTime, MaxColsAtCompileTime>& t) {
-    typename Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime,
-                           Options, MaxRowsAtCompileTime,
-                           MaxColsAtCompileTime>::Index nrows(0),
-        ncols(0);
-    ar& nrows& ncols;
-    t.resize(nrows, ncols);
-    if (t.size()) ar& madness::archive::wrap(t.data(), t.size());
-  }
+   Archive,
+   Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
+                 MaxRowsAtCompileTime, MaxColsAtCompileTime>> {
+ static inline void load(
+     const Archive& ar,
+     Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime, Options,
+                   MaxRowsAtCompileTime, MaxColsAtCompileTime>& t) {
+   typename Eigen::Matrix<Scalar, RowsAtCompileTime, ColsAtCompileTime,
+                          Options, MaxRowsAtCompileTime,
+                          MaxColsAtCompileTime>::Index nrows(0),
+       ncols(0);
+   ar& nrows& ncols;
+   t.resize(nrows, ncols);
+   if (t.size()) ar& madness::archive::wrap(t.data(), t.size());
+ }
+};
+
+template <class Archive, typename Scalar_, int NumIndices_, int Options_,
+         typename IndexType_>
+struct ArchiveStoreImpl<
+   Archive, Eigen::Tensor<Scalar_, NumIndices_, Options_, IndexType_>> {
+ static inline void store(
+     const Archive& ar,
+     const Eigen::Tensor<Scalar_, NumIndices_, Options_, IndexType_>& t) {
+   using idx_t = typename Eigen::Tensor<Scalar_, NumIndices_, Options_,
+                                        IndexType_>::Index;
+   std::array<idx_t, NumIndices_> extents;
+   for (int d = 0; d != NumIndices_; ++d) extents[d] = t.dimension(d);
+   ar& extents;
+   if (t.size()) ar& madness::archive::wrap(t.data(), t.size());
+ }
+};
+
+template <class Archive, typename Scalar_, int NumIndices_, int Options_,
+         typename IndexType_>
+struct ArchiveLoadImpl<
+   Archive, Eigen::Tensor<Scalar_, NumIndices_, Options_, IndexType_>> {
+ static inline void load(
+     const Archive& ar,
+     Eigen::Tensor<Scalar_, NumIndices_, Options_, IndexType_>& t) {
+   using idx_t = typename Eigen::Tensor<Scalar_, NumIndices_, Options_,
+                                        IndexType_>::Index;
+   std::array<idx_t, NumIndices_> extents;
+   ar& extents;
+   t.resize(extents);
+   if (t.size()) ar& madness::archive::wrap(t.data(), t.size());
+ }
 };
 
 }  // namespace archive
