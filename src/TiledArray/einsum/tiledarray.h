722c6e7b67183e9712cb614cf80b65a91ba98095
asadchev <asadchev@gmail.com>
2022-08-31 15:47:55 -0400

    Einsum 3-term dot product

--- src/TiledArray/einsum/tiledarray.h
+++ src/TiledArray/einsum/tiledarray.h
@@ -293,6 +293,118 @@ auto einsum(
 
 }
 
+/// Specialized function to compute the einsum between two tensors
+/// then dot with a third
+template <typename Array_, typename... Indices>
+auto dot(
+  expressions::TsrExpr<Array_> A,
+  expressions::TsrExpr<Array_> B,
+  expressions::TsrExpr<Array_> C,
+  World &world)
+{
+  using Array = std::remove_cv_t<Array_>;
+  using Tensor = typename Array::value_type;
+  using Shape = typename Array::shape_type;
+
+  auto a = std::get<0>(Einsum::idx(A));
+  auto b = std::get<0>(Einsum::idx(B));
+  auto c = std::get<0>(Einsum::idx(C));
+
+  // these are "Hadamard" (fused) indices
+  auto h = a & b & c;
+  auto ab_e = (a ^ b);
+  auto ab_i = (a & b)-h;
+  TA_ASSERT(ab_e);
+
+  // no Hadamard indices => standard contraction
+  if (!h) {
+    Array AB;
+    AB(ab_e) = A*B;
+    return AB(ab_e).dot(C).get();
+  }
+
+  TA_ASSERT(sorted(c) == sorted(h + ab_e));
+
+  auto range_map = (
+    RangeMap(a, A.array().trange()) |
+    RangeMap(b, B.array().trange()) |
+    RangeMap(c, C.array().trange())
+  );
+
+  struct {
+    RangeProduct tiles;
+    std::vector<std::vector<size_t>> batch;
+  } H;
+
+  for (auto idx : h) {
+    H.tiles *= Range(range_map[idx].tiles_range());
+    H.batch.push_back({});
+    for (auto r : range_map[idx]) {
+      H.batch.back().push_back(Range{r}.size());
+    }
+  }
+
+  ArrayTerm<Array> terms[3] = {{A.array(), a}, {B.array(), b}, {C.array(), c}};
+
+  for (auto &term : terms) {
+    auto ei = (ab_e + ab_i & term.idx);
+    if (term.idx != h + ei) {
+      term.permutation = permutation(term.idx, h + ei);
+    }
+    term.expr = ei;
+    term.ei_tiled_range = TiledRange(range_map[ei]);
+    for (auto idx : ei) {
+      term.tiles *= Range(range_map[idx].tiles_range());
+    }
+  }
+
+  using Index = Einsum::Index<size_t>;
+  typename Tensor::value_type result = 0.0;
+
+  // iterates over tiles of hadamard indices
+  for (Index h : H.tiles) {
+    auto &[A, B, C] = terms;
+    size_t batch = 1;
+    for (size_t i = 0; i < h.size(); ++i) {
+      batch *= H.batch[i].at(h[i]);
+    }
+    for (auto &term : terms) {
+      term.local_tiles.clear();
+      const Permutation &P = term.permutation;
+
+      for (Index ei : term.tiles) {
+        auto idx = apply_inverse(P, h + ei);
+        if (!term.array.is_local(idx)) continue;
+        if (term.array.is_zero(idx)) continue;
+        // TODO no need for immediate evaluation
+        auto tile = term.array.find(idx).get();
+        if (P) tile = tile.permute(P);
+        auto shape = term.ei_tiled_range.tile(ei);
+        tile = tile.reshape(shape, batch);
+        term.local_tiles.push_back({ei, tile});
+      }
+      bool replicated = term.array.pmap()->is_replicated();
+      term.ei = TiledArray::make_array<Array>(
+          world, term.ei_tiled_range,
+          term.local_tiles.begin(),
+          term.local_tiles.end(),
+          replicated
+      );
+    }
+    result += (A.ei(A.expr) * B.ei(B.expr)).dot(C.ei(C.expr)).get();
+    for(auto & term : terms){
+      term.ei.defer_deleter_to_next_fence();
+      term.ei = Array();
+    }
+
+  }
+
+  world.gop.fence();
+
+  return result;
+
+}
+
 } // tiledarray::expressions
 
 namespace TiledArray::expressions {
@@ -329,6 +441,18 @@ auto einsum(
   return Einsum::einsum(E(A), E(B), Einsum::idx<T>(cs), world);
 }
 
+template <typename T, typename U, typename V>
+auto dot(expressions::TsrExpr<T> A,
+         expressions::TsrExpr<U> B,
+         expressions::TsrExpr<V> C,
+         World &world = get_default_world())
+{
+  static_assert(std::is_same<const T, const U>::value);
+  static_assert(std::is_same<const T, const V>::value);
+  using E = expressions::TsrExpr<const T>;
+  return Einsum::dot(E(A), E(B), E(C), world);
+}
+
 } // TiledArray::expressions
 
 namespace TiledArray {
@@ -350,6 +474,25 @@ auto einsum(
     A(string::join(a,",")),
     B(string::join(b,",")),
     string::join(rhs,","),
+    world
+  );
+}
+
+template<typename T, typename P>
+auto dot(
+  const std::string &expr,
+  const DistArray<T,P> &A,
+  const DistArray<T,P> &B,
+  const DistArray<T,P> &C,
+  World &world = get_default_world())
+{
+  namespace string = ::Einsum::string;
+  auto [a,bc] = string::split2(expr,",");
+  auto [b,c] = string::split2(bc,",");
+  return dot(
+    A(string::join(a,",")),
+    B(string::join(b,",")),
+    C(string::join(c,",")),
     world
   );
 }
