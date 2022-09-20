41f8b1f57f559d8daa6f719a0ad7ce9c74646b03
kmp5VT <kmp5@vt.edu>
2022-09-20 07:42:00 -0400

    Fix einsum impl

--- src/TiledArray/einsum/tiledarray.h
+++ src/TiledArray/einsum/tiledarray.h
@@ -4,13 +4,11 @@
 #include "TiledArray/fwd.h"
 #include "TiledArray/dist_array.h"
 #include "TiledArray/expressions/fwd.h"
-#include "TiledArray/fwd.h"
-
 #include "TiledArray/einsum/index.h"
 #include "TiledArray/einsum/range.h"
-#include "TiledArray/tiled_range.h"
 #include "TiledArray/tiled_range1.h"
-#include "TiledArray/util/function.h"
+#include "TiledArray/tiled_range.h"
+//#include "TiledArray/util/string.h"
 
 namespace TiledArray::expressions {
 
@@ -21,12 +19,12 @@ namespace TiledArray::expressions {
 /// @param[in] B second argument to the product
 /// @warning just as in the plain expression code, reductions are a special
 /// case; use Expr::reduce()
-template <typename T, typename U>
+template<typename T, typename U>
 auto einsum(expressions::TsrExpr<T> A, expressions::TsrExpr<U> B) {
-  // printf("einsum(A,B)\n");
+  //printf("einsum(A,B)\n");
   auto a = std::get<0>(idx(A));
   auto b = std::get<0>(idx(B));
-  return einsum(A, B, std::string(a ^ b));
+  return einsum(A, B, std::string(a^b));
 }
 
 /// einsum function with result indices explicitly specified
@@ -35,18 +33,26 @@ auto einsum(expressions::TsrExpr<T> A, expressions::TsrExpr<U> B) {
 /// @param[in] r result indices
 /// @warning just as in the plain expression code, reductions are a special
 /// case; use Expr::reduce()
-template <typename T, typename U, typename... Indices>
-auto einsum(expressions::TsrExpr<T> A, expressions::TsrExpr<U> B,
-            const std::string &cs, World &world = get_default_world()) {
+template<typename T, typename U, typename ... Indices>
+auto einsum(
+    expressions::TsrExpr<T> A,
+    expressions::TsrExpr<U> B,
+    const std::string &cs,
+    World &world = get_default_world())
+{
   static_assert(std::is_same<const T, const U>::value);
   using E = expressions::TsrExpr<const T>;
   return einsum(E(A), E(B), Einsum::idx<T>(cs), world);
 }
 
-template <typename Array_, typename... Indices>
-auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
-            std::tuple<Einsum::Index<std::string>, Indices...> cs,
-            World &world) {
+template<typename Array_, typename ... Indices>
+auto einsum(
+    expressions::TsrExpr<Array_> A,
+    expressions::TsrExpr<Array_> B,
+    std::tuple<Einsum::Index<std::string>,Indices...> cs,
+    World &world)
+{
+
   using Array = std::remove_cv_t<Array_>;
   using Tensor = typename Array::value_type;
   using Shape = typename Array::shape_type;
@@ -55,9 +61,7 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
   auto b = std::get<0>(Einsum::idx(B));
   Einsum::Index<std::string> c = std::get<0>(cs);
 
-  struct {
-    std::string a, b, c;
-  } inner;
+  struct { std::string a, b, c; } inner;
   if constexpr (std::tuple_size<decltype(cs)>::value == 2) {
     inner.a = ";" + (std::string)std::get<1>(Einsum::idx(A));
     inner.b = ";" + (std::string)std::get<1>(Einsum::idx(B));
@@ -71,7 +75,7 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
   // same a, b, and c => pure Hadamard
   if (!h || (!(a ^ b) && !(b ^ c))) {
     Array C;
-    C(std::string(c) + inner.c) = A * B;
+    C(std::string(c) + inner.c) = A*B;
     return C;
   }
 
@@ -85,12 +89,14 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
   using Range = Einsum::Range;
   using RangeProduct = Einsum::RangeProduct<Range, small_vector<size_t> >;
 
-  using RangeMap = Einsum::IndexMap<std::string, TiledRange1>;
-  auto range_map =
-      (RangeMap(a, A.array().trange()) | RangeMap(b, B.array().trange()));
+  using RangeMap = Einsum::IndexMap<std::string,TiledRange1>;
+  auto range_map = (
+      RangeMap(a, A.array().trange()) |
+      RangeMap(b, B.array().trange())
+  );
 
-  using Einsum::index::permutation;
   using TiledArray::Permutation;
+  using Einsum::index::permutation;
 
   struct Term {
     Array array;
@@ -103,29 +109,29 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
     std::vector< std::pair<Einsum::Index<size_t>,Tensor> > local_tiles;
     bool own(Einsum::Index<size_t> h) const {
       for (Einsum::Index<size_t> ei : tiles) {
-        auto idx = apply_inverse(permutation, h + ei);
+        auto idx = apply_inverse(permutation, h+ei);
         if (array.is_local(idx)) return true;
       }
       return false;
     }
   };
 
-  Term AB[2] = {{A.array(), a}, {B.array(), b}};
+  Term AB[2] = { { A.array(), a }, { B.array(), b } };
 
   for (auto &term : AB) {
-    auto ei = (e + i & term.idx);
-    if (term.idx != h + ei) {
-      term.permutation = permutation(term.idx, h + ei);
+    auto ei = (e+i & term.idx);
+    if (term.idx != h+ei) {
+      term.permutation = permutation(term.idx, h+ei);
     }
     term.expr = ei;
   }
 
-  Term C = {Array(world, TiledRange(range_map[c])), c};
+  Term C = { Array(world, TiledRange(range_map[c])), c };
   for (auto idx : e) {
     C.tiles *= Range(range_map[idx].tiles_range());
   }
-  if (C.idx != h + e) {
-    C.permutation = permutation(h + e, C.idx);
+  if (C.idx != h+e) {
+    C.permutation = permutation(h+e, C.idx);
   }
   C.expr = e;
 
@@ -135,7 +141,7 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
 
   struct {
     RangeProduct tiles;
-    std::vector<std::vector<size_t> > batch;
+    std::vector< std::vector<size_t> > batch;
   } H;
 
   for (auto idx : h) {
@@ -148,10 +154,11 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
 
   using Index = Einsum::Index<size_t>;
 
-  if constexpr (std::tuple_size<decltype(cs)>::value > 1) {
+  if constexpr(std::tuple_size<decltype(cs)>::value > 1) {
     TA_ASSERT(e);
-  } else if (!e) {  // hadamard reduction
-    auto &[A, B] = AB;
+  }
+  else if (!e) { // hadamard reduction
+    auto& [A,B] = AB;
     TiledRange trange(range_map[i]);
     RangeProduct tiles;
     for (auto idx : i) {
@@ -168,8 +175,8 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
       Tensor tile(TiledArray::Range{batch}, typename Tensor::value_type());
       for (Index i : tiles) {
         // skip this unless both input tiles exist
-        const auto pahi_inv = apply_inverse(pa, h + i);
-        const auto pbhi_inv = apply_inverse(pb, h + i);
+        const auto pahi_inv = apply_inverse(pa,h+i);
+        const auto pbhi_inv = apply_inverse(pb,h+i);
         if (A.array.is_zero(pahi_inv) || B.array.is_zero(pbhi_inv)) continue;
 
         auto ai = A.array.find(pahi_inv).get();
@@ -196,7 +203,7 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
   // generalized contraction
 
   for (auto &term : AB) {
-    auto ei = (e + i & term.idx);
+    auto ei = (e+i & term.idx);
     term.ei_tiled_range = TiledRange(range_map[ei]);
     for (auto idx : ei) {
       term.tiles *= Range(range_map[idx].tiles_range());
@@ -208,11 +215,10 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
 
   // iterates over tiles of hadamard indices
   for (Index h : H.tiles) {
-    auto &[A, B] = AB;
+    auto& [A,B] = AB;
     auto own = A.own(h) || B.own(h);
     auto comm = world.mpi.comm().Split(own, world.rank());
-    worlds.push_back(std::unique_ptr<World, decltype(fence_then_delete)>(
-        new World{comm}, fence_then_delete));
+    worlds.push_back(std::make_unique<World>(comm));
     auto &owners = worlds.back();
     if (!own) continue;
     size_t batch = 1;
@@ -222,8 +228,9 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
     for (auto &term : AB) {
       term.local_tiles.clear();
       const Permutation &P = term.permutation;
+
       for (Index ei : term.tiles) {
-        auto idx = apply_inverse(P, h + ei);
+        auto idx = apply_inverse(P, h+ei);
         if (!term.array.is_local(idx)) continue;
         if (term.array.is_zero(idx)) continue;
         // TODO no need for immediate evaluation
@@ -235,11 +242,11 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
       }
       bool replicated = term.array.pmap()->is_replicated();
       term.ei = TiledArray::make_array<Array>(
-        *owners,
-        term.ei_tiled_range,
-        term.local_tiles.begin(),
-        term.local_tiles.end(),
-        replicated
+          *owners,
+          term.ei_tiled_range,
+          term.local_tiles.begin(),
+          term.local_tiles.end(),
+          replicated
       );
     }
     C.ei(C.expr) = (A.ei(A.expr) * B.ei(B.expr)).set_world(*owners);
@@ -287,6 +294,7 @@ auto einsum(expressions::TsrExpr<Array_> A, expressions::TsrExpr<Array_> B,
   }
 
   return C.array;
+
 }
 
 }  // namespace TiledArray::expressions
@@ -295,16 +303,23 @@ namespace TiledArray {
 
 using expressions::einsum;
 
-template <typename T, typename P>
-auto einsum(const std::string &expr, const DistArray<T, P> &A,
-            const DistArray<T, P> &B, World &world = get_default_world()) {
+template<typename T, typename P>
+auto einsum(
+    const std::string &expr,
+    const DistArray<T,P> &A,
+    const DistArray<T,P> &B,
+    World &world = get_default_world())
+{
   namespace string = Einsum::string;
-  auto [lhs, rhs] = string::split2(expr, "->");
-  auto [a, b] = string::split2(lhs, ",");
-  return einsum(A(string::join(a, ",")), B(string::join(b, ",")),
-                string::join(rhs, ","));
+  auto [lhs,rhs] = string::split2(expr, "->");
+  auto [a,b] = string::split2(lhs,",");
+  return einsum(
+      A(string::join(a,",")),
+      B(string::join(b,",")),
+      string::join(rhs,",")
+  );
 }
 
-}  // namespace TiledArray
+}
 
 #endif /* TILEDARRAY_EINSUM_H__INCLUDED */
