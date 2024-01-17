3ccdc324ec70381f18a52e05c35c5702082cb3f4
Bimal Gaudel <bimalgaudel@gmail.com>
2024-01-16 19:49:59 -0500

    Amend previous commit on the basis of second thought :)

--- src/TiledArray/tensor/tensor.h
+++ src/TiledArray/tensor/tensor.h
@@ -1687,6 +1687,9 @@ class Tensor {
   template <typename Right,
             typename std::enable_if<is_tensor<Right>::value>::type* = nullptr>
   Tensor& add_to(const Right& right) {
+    if (empty()) {
+      *this = Tensor{right.range(), value_type{}};
+    }
     return inplace_binary(right, [](value_type& MADNESS_RESTRICT l,
                                     const value_t<Right> r) { l += r; });
   }
